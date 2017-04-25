d2dc1d0932a9eafbf57da6f0d051a4a094419191
foglio <foglio1024@gmail.com>
2017-04-25 18:25:25 +0200

    renamed some properties

--- TCC.UI/Parsing/Messages/S_SPAWN_NPC.cs
+++ TCC.UI/Parsing/Messages/S_SPAWN_NPC.cs
@@ -14,11 +14,11 @@ namespace TCC.Parsing.Messages
 
         ulong id;
         uint templateId;
-        short huntingZoneId;
+        ushort huntingZoneId;
 
         public ulong EntityId { get => id;}
         public uint TemplateId { get => templateId;  }
-        public short HuntingZoneId { get => huntingZoneId; }
+        public ushort HuntingZoneId { get => huntingZoneId; }
 
         public S_SPAWN_NPC(TeraMessageReader reader) : base(reader)
         {
@@ -26,7 +26,7 @@ namespace TCC.Parsing.Messages
             id = reader.ReadUInt64();
             reader.Skip(26);
             templateId = reader.ReadUInt32();
-            huntingZoneId = reader.ReadInt16();
+            huntingZoneId = reader.ReadUInt16();
 
            // Console.WriteLine("[S_SPAWN NPC] id:{0} name:{1}", id, MonsterDatabase.GetName(npc, (uint)type));
         }
