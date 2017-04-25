980114d922ea083cb0429e1f1cf62d64f2bd0831
foglio <foglio1024@gmail.com>
2017-04-25 18:24:28 +0200

    added packets to hide UI during loading screens

---
+++ TCC.UI/Parsing/Messages/S_LOAD_TOPO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tera.Game;
+using Tera.Game.Messages;
+
+namespace TCC.Parsing.Messages
+{
+    public class S_LOAD_TOPO : ParsedMessage
+    {
+        public S_LOAD_TOPO(TeraMessageReader reader) : base(reader)
+        {
+
+        }
+    }
+}
