1baaaf4dc9db93abca1bd4438008970910fbffe1
foglio <foglio1024@gmail.com>
2017-04-27 01:17:10 +0200

    added internal timer for each abnormality

---
+++ TCC.UI/UI elements/AbnormalityDuration.cs
@@ -0,0 +1,35 @@
+﻿using System.Timers;
+using TCC.Data;
+
+namespace TCC
+{
+    public class AbnormalityDuration
+    {
+        public ulong Target { get; set; }
+        public Abnormality Abnormality { get; set; }
+        public int Duration { get; set; }
+        public int Stacks { get; set; }
+        public Timer timer;
+        public int DurationLeft { get; set; }
+        public AbnormalityDuration(Abnormality b, int d, int s, ulong t)
+        {
+            Abnormality = b;
+            Duration = d;
+            Stacks = s;
+            Target = t;
+
+            DurationLeft = d;
+            timer = new Timer(1000);
+            timer.Elapsed += (se, ev) => DurationLeft = DurationLeft - 1000;
+            if (!Abnormality.Infinity)
+            {
+                timer.Start();
+            }
+        }
+        ~AbnormalityDuration()
+        {
+            timer.Stop();
+        }
+    }
+
+}
