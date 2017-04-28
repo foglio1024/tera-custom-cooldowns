38dd1f7809ffe6f6c7f1ecc2e8212474ec5a1fd5
foglio <foglio1024@gmail.com>
2017-04-28 12:06:15 +0200

    added default constructor

--- TCC.UI/Data/AbnormalityDuration.cs
+++ TCC.UI/Data/AbnormalityDuration.cs
@@ -26,6 +26,10 @@ namespace TCC
                 timer.Start();
             }
         }
+        public AbnormalityDuration()
+        {
+
+        }
         ~AbnormalityDuration()
         {
             timer.Stop();
