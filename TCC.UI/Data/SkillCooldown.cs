402dbaba9f1c1f490ef91ac862dd45b2ac8b38c9
foglio <foglio1024@gmail.com>
2017-05-11 13:30:03 +0200

    reworked SkillCooldown type

--- TCC.UI/Data/SkillCooldown.cs
+++ TCC.UI/Data/SkillCooldown.cs
@@ -1,32 +1,64 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Timers;
-using System.Threading.Tasks;
+using System.Windows.Threading;
+using TCC.ViewModels;
 
 namespace TCC
 {
-    public class SkillCooldownOld
+    public class SkillCooldown : TSPropertyChanged, IDisposable
     {
-        public uint Id { get; set; }
-        public uint Cooldown { get; set; }
+        public Skill Skill { get; set; }
+        public int Cooldown { get; set; }
+        public int OriginalCooldown { get; set; }
         public CooldownType Type { get; set; }
-        public Timer Timer { get; set; }
+        private Timer _timer;
 
-        public SkillCooldownOld(uint id, uint cd, CooldownType t)
+        public SkillCooldown(Skill sk, int cd, CooldownType t)
         {
-            Id = id;
+            _dispatcher = CooldownBarWindowManager.Instance.Dispatcher;
+
+
+            Skill = sk;
             Cooldown = cd;
-            if(t == CooldownType.Item)
+            OriginalCooldown = Cooldown;
+            if (t == CooldownType.Item)
             {
                 Cooldown = Cooldown * 1000;
             }
-            Type = t;
-            if(cd != 0)
-            {
-                Timer = new Timer(Cooldown);
-            }
-        }     
+
+            if (cd == 0) return;
+            _timer = new Timer(cd);
+            _timer.Elapsed += _timer_Elapsed;
+            _timer.Start();
+
+        }
+
+        private void _timer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            NotifyPropertyChanged("Ending");
+            _timer?.Stop();
+        }
+
+        public void Refresh()
+        {
+            NotifyPropertyChanged("Refresh");
+            if (_timer == null) return;
+            _timer.Stop();
+            _timer.Interval = Cooldown > 0 ? Cooldown : 1;
+            _timer.Start();
+        }
+
+        public void Dispose()
+        {
+            _timer?.Stop();
+            _timer?.Dispose();
+        }
     }
 }
+
+/*
+* C_START_SKILL            0xCBC5
+* S_START_COOLTIME_SKILL   0x7BA8
+* C_USE_ITEM               0x750F
+* S_START_COOLTIME_ITEM    0xAD63
+*/
