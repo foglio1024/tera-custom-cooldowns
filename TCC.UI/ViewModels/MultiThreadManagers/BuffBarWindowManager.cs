14cb55906182f8ab961048baab07b640b1149b48
foglio <foglio1024@gmail.com>
2017-05-11 14:01:53 +0200

    moved window managers

---
+++ TCC.UI/ViewModels/MultiThreadManagers/BuffBarWindowManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows;
+using TCC.Data;
+
+namespace TCC.ViewModels
+{
+    public class BuffBarWindowManager : DependencyObject
+    {
+        private static BuffBarWindowManager _instance;
+        public static BuffBarWindowManager Instance => _instance ?? (_instance = new BuffBarWindowManager());
+
+        private Player _player;
+        public Player Player
+        {
+            get { return _player; }
+            set
+            {
+                if (_player == value) return;
+                _player = value;
+            }
+        }
+    }
+}
---
+++ TCC.UI/ViewModels/MultiThreadManagers/CooldownBarWindowManager.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Threading;
+using TCC.Parsing.Messages;
+
+namespace TCC.ViewModels
+{
+
+    public class CooldownBarWindowManager : DependencyObject
+    {
+        private static CooldownBarWindowManager _instance;
+        public static CooldownBarWindowManager Instance => _instance ?? (_instance = new CooldownBarWindowManager());
+
+        private SynchronizedObservableCollection<SkillCooldown> _shortSkills;
+        public SynchronizedObservableCollection<SkillCooldown> ShortSkills
+        {
+            get { return _shortSkills; }
+            set
+            {
+                if (_shortSkills == value) return;
+                _shortSkills = value;
+            }
+        }
+        private SynchronizedObservableCollection<SkillCooldown> _longSkills;
+        public SynchronizedObservableCollection<SkillCooldown> LongSkills
+        {
+            get { return _longSkills; }
+            set
+            {
+                if (_longSkills == value) return;
+                _longSkills = value;
+            }
+        }
+
+        public void AddOrRefreshSkill(SkillCooldown sk)
+        {
+            if (sk.Cooldown < SkillManager.LongSkillTreshold)
+            {
+                var existing = _shortSkills.FirstOrDefault(x => x.Skill.Name == sk.Skill.Name);
+                if (existing == null)
+                {
+                    _shortSkills.Add(sk);
+                    return;
+                }
+                existing.Cooldown = sk.Cooldown;
+                existing.Refresh();
+            }
+            else
+            {
+                var existing = _longSkills.FirstOrDefault(x => x.Skill.Name == sk.Skill.Name);
+                if (existing == null)
+                {
+                    _longSkills.Add(sk);
+                    return;
+                }
+                existing.Cooldown = sk.Cooldown;
+                existing.Refresh();
+            }
+        }
+        public void RemoveSkill(Skill sk)
+        {
+            var shortSkill = _shortSkills.FirstOrDefault(x => x.Skill.Name == sk.Name);
+            if (shortSkill != null)
+            {
+                
+                _shortSkills.Remove(shortSkill);
+                shortSkill.Dispose();
+            }
+            var longSkill = _longSkills.FirstOrDefault(x => x.Skill.Name == sk.Name);
+            if (longSkill != null)
+            {
+                _longSkills.Remove(longSkill);
+                longSkill.Dispose();
+            }
+        }
+    }
+}
