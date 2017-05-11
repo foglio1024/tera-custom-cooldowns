3a1280d2005494c46febbb8c11faa2e65f846be0
foglio <foglio1024@gmail.com>
2017-05-11 13:36:35 +0200

    added base class for windows

---
+++ TCC.UI/Windows/TccWindow.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Interop;
+
+namespace TCC.Windows
+{
+    public class TccWindow : Window
+    {
+        protected IntPtr _handle;
+
+        private bool clickThru;
+
+        public bool ClickThru
+        {
+            get { return clickThru; }
+            set {
+                if (clickThru == value) return;
+                clickThru = value;
+
+                if (clickThru) FocusManager.MakeTransparent(_handle);
+                else FocusManager.UndoTransparent(_handle);
+
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("ClickThru"));
+            }
+        }
+
+
+        protected void InitWindow()
+        {
+            _handle = new WindowInteropHelper(this).Handle;
+            FocusManager.MakeUnfocusable(_handle);
+            FocusManager.HideFromToolBar(_handle);
+            Topmost = true;
+            ContextMenu = new ContextMenu();
+
+            var HideButton = new MenuItem() { Header = "Hide" };
+            HideButton.Click += (s, ev) =>
+            {
+                this.Visibility = Visibility.Hidden;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Visibility"));
+            };
+            ContextMenu.Items.Add(HideButton);
+
+            var ClickThruButton = new MenuItem() { Header = "Click through" };
+            ClickThruButton.Click += (s, ev) =>
+            {
+                SetClickThru(true);
+            };
+            ContextMenu.Items.Add(ClickThruButton);
+
+        }
+
+        public void SetClickThru(bool t)
+        {
+            ClickThru = t;
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+
+    }
+}
