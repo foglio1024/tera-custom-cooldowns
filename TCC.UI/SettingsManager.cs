eb541c19bd7f1bc141dadc846a5111b51ae413d6
foglio <foglio1024@gmail.com>
2017-05-11 14:03:57 +0200

    added settingsmanager class

---
+++ TCC.UI/SettingsManager.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace TCC
+{
+    public class WindowSettings
+    {
+        public double X;
+        public double Y;
+        public Visibility Visibility;
+        public bool ClickThru;
+    }
+    public static class SettingsManager
+    {
+        static Rectangle _screen =  System.Windows.Forms.Screen.PrimaryScreen.Bounds;
+
+        public static WindowSettings CooldownWindowSettings = new WindowSettings()
+        {
+            X = _screen.Width / 3,
+            Y = _screen.Height /1.5,
+            Visibility = Visibility.Visible,
+            ClickThru = false
+        };
+        public static WindowSettings BossGaugeWindowSettings = new WindowSettings()
+        {
+            X = _screen.Width / 2 - 200,
+            Y = 20,
+            Visibility = Visibility.Visible,
+            ClickThru = false
+        };
+        public static WindowSettings BuffBarWindowSettings = new WindowSettings()
+        {
+            X = _screen.Width -1000,
+            Y = _screen.Height/1.5,
+            Visibility = Visibility.Visible,
+            ClickThru = false
+        };
+        public static WindowSettings CharacterWindowSettings = new WindowSettings()
+        {
+            X = _screen.Width / 2 - 200,
+            Y = _screen.Height - 120,
+            Visibility = Visibility.Visible,
+            ClickThru = false
+        };
+    }
+}
