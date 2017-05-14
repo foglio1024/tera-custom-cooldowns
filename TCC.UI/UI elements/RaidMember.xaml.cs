c9923e13523ca510d800de1ce125d80ed0ccc849
foglio <foglio1024@gmail.com>
2017-05-14 17:22:51 +0200

    added converters

--- TCC.UI/UI elements/RaidMember.xaml.cs
+++ TCC.UI/UI elements/RaidMember.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,6 +15,7 @@ using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using TCC.ViewModels;
 
 namespace TCC.UI_elements
 {
@@ -25,7 +28,7 @@ namespace TCC.UI_elements
         {
             InitializeComponent();
         }
-
+        
         private void UserControl_Loaded(object sender, RoutedEventArgs e)
         {
         }
@@ -44,4 +47,114 @@ namespace TCC.UI_elements
             BeginAnimation(OpacityProperty, an);
         }
     }
+}
+namespace TCC.Converters
+{
+    public class AliveToOpacityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if ((bool)value)
+            {
+                return 0;
+            }
+            else
+            {
+                return 1;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+    public class BoolToOpacityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if ((bool)value)
+            {
+                return 1;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class ReadyToIconConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            ReadyStatus v = (ReadyStatus)value;
+            string img = "resources/images/Icon_Laurels/blank.png";
+            switch (v)
+            {
+                case ReadyStatus.NotReady:
+                    img = "resources/images/ic_close_white_24dp_2x.png";
+                    break;
+                case ReadyStatus.Ready:
+                    img = "resources/images/ic_done_white_24dp_2x.png";
+                    break;
+                case ReadyStatus.Undefined:
+                    img = "resources/images/Icon_Laurels/blank.png";
+                    break;
+            }
+            return img;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+    public class ReadyToOpacityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            ReadyStatus v = (ReadyStatus)value;
+            switch (v)
+            {
+                case ReadyStatus.NotReady:
+                    return .9;
+                case ReadyStatus.Ready:
+                    return .9;
+                default:
+                    return 0;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class ReadyToColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if ((ReadyStatus)value == ReadyStatus.Ready)
+            {
+                return new SolidColorBrush(Color.FromRgb(0x55, 0xff, 0x77));
+            }
+            else
+            {
+                return new SolidColorBrush(Color.FromRgb(0xff, 0x55, 0x55));
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
 }
