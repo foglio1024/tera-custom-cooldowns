8aa5512e6bb4bd435a254d604acbf1db9131e933
foglio <foglio1024@gmail.com>
2017-05-14 17:14:04 +0200

    added converters for dice rolls ui

---
+++ TCC.UI/Converters/RollToStringConverter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace TCC.Converters
+{
+    public class RollToStringConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if((int)value == -1)
+            {
+                return "x";
+            }
+            else if((int)value == 0)
+            {
+                return "-";
+            }
+            else
+            {
+                return ((int)value).ToString();
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
