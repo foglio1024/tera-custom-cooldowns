bf1a0e6a428fbf41bedb01e68796169c61d15437
foglio <foglio1024@gmail.com>
2017-05-14 17:18:53 +0200

    added more properties to User class

--- TCC.UI/Data/User.cs
+++ TCC.UI/Data/User.cs
@@ -1,4 +1,7 @@
-﻿using System.Windows.Threading;
+﻿using System;
+using System.Linq;
+using System.Windows.Threading;
+using TCC.ClassSpecific;
 
 namespace TCC.Data
 {
@@ -45,16 +48,14 @@ namespace TCC.Data
         {
             get
             {
-                System.Console.WriteLine("Get {0} online status ({1})", name, online);
-
                 return online;
             }
             set
             {
                 if (online == value) return;
                 online = value;
+                if (!online) { CurrentHP = 0; CurrentMP = 0; }
                 NotifyPropertyChanged("Online");
-                System.Console.WriteLine("{0} online status = {1}", name, online);
             }
         }
 
@@ -206,7 +207,6 @@ namespace TCC.Data
             }
         }
 
-
         private double mpFactor;
         public double MpFactor
         {
@@ -231,7 +231,19 @@ namespace TCC.Data
             }
         }
 
-        private bool alive;
+        private ReadyStatus ready = ReadyStatus.Undefined;
+        public ReadyStatus Ready
+        {
+            get { return ready; }
+            set
+            {
+                if (ready == value) return;
+                ready = value;
+                NotifyPropertyChanged("Ready");
+            }
+        }
+
+        private bool alive = true;
         public bool Alive
         {
             get { return alive; }
@@ -243,9 +255,152 @@ namespace TCC.Data
             }
         }
 
+        private int rollResult = 0;
+        public int RollResult
+        {
+            get { return rollResult; }
+            set
+            {
+                if (rollResult == value) return;
+                rollResult = value;
+                if (rollResult == -1) IsRolling = false;
+                NotifyPropertyChanged("RollResult");
+            }
+        }
+
+        private bool isRolling = false;
+        public bool IsRolling
+        {
+            get { return isRolling; }
+            set
+            {
+                if (isRolling == value) return;
+                isRolling = value;
+                NotifyPropertyChanged("IsRolling");
+            }
+        }
+
+        private bool isWinning = false;
+        public bool IsWinning
+        {
+            get { return isWinning; }
+            set
+            {
+                if (isWinning == value) return;
+                isWinning = value;
+                NotifyPropertyChanged("IsWinning");
+            }
+        }
+
+        private bool isLeader = false;
+        public bool IsLeader
+        {
+            get
+            {
+                return isLeader;
+            }
+            set
+            {
+                if (isLeader == value) return;
+                isLeader = value;
+                NotifyPropertyChanged("IsLeader");
+            }
+        }
+
+        private SynchronizedObservableCollection<AbnormalityDuration> _buffs;
+        public SynchronizedObservableCollection<AbnormalityDuration> Buffs
+        {
+            get { return _buffs; }
+            set
+            {
+                if (_buffs == value) return;
+                _buffs = value;
+            }
+        }
+        private SynchronizedObservableCollection<AbnormalityDuration> _debuffs;
+        public SynchronizedObservableCollection<AbnormalityDuration> Debuffs
+        {
+            get { return _debuffs; }
+            set
+            {
+                if (_debuffs == value) return;
+                _debuffs = value;
+            }
+        }
+
+        public void AddOrRefreshBuff(AbnormalityDuration ab)
+        {
+            if (SettingsManager.IgnoreAllBuffsInGroupWindow) return; 
+            switch (SessionManager.CurrentPlayer.Class)
+            {
+                case Class.Priest:
+                    if (FilterPriest(ab)) return;
+                    break;
+                case Class.Elementalist:
+                    if (FilterMystic(ab)) return;
+                    break;
+                default:
+                    return;
+            }
+            var existing = Buffs.FirstOrDefault(x => x.Abnormality.Id == ab.Abnormality.Id);
+            if (existing == null)
+            {
+                Buffs.Add(ab);
+                return;
+            }
+            existing.Duration = ab.Duration;
+            existing.DurationLeft = ab.DurationLeft;
+            existing.Stacks = ab.Stacks;
+            existing.Refresh();
+
+        }
+
+        private bool FilterPriest(AbnormalityDuration ab)
+        {
+            if (!Priest.CommonBuffs.Any(x => x == ab.Abnormality.Id)) return true;
+            else return false;
+        }
+
+        private bool FilterMystic(AbnormalityDuration ab)
+        {
+            if (!Mystic.CommonBuffs.Any(x=> x == ab.Abnormality.Id)) return true;
+            else return false;
+        }
+
+        public void AddOrRefreshDebuff(AbnormalityDuration ab)
+        {
+            var existing = Debuffs.FirstOrDefault(x => x.Abnormality.Id == ab.Abnormality.Id);
+            if (existing == null)
+            {
+                Debuffs.Add(ab);
+                return;
+            }
+            existing.Duration = ab.Duration;
+            existing.DurationLeft = ab.DurationLeft;
+            existing.Stacks = ab.Stacks;
+            existing.Refresh();
+        }
+
+        public void RemoveBuff(Abnormality ab)
+        {
+            var buff = Buffs.FirstOrDefault(x => x.Abnormality.Id == ab.Id);
+            if (buff == null) return;
+            Buffs.Remove(buff);
+            buff.Dispose();
+        }
+        public void RemoveDebuff(Abnormality ab)
+        {
+            var buff = Debuffs.FirstOrDefault(x => x.Abnormality.Id == ab.Id);
+            if (buff == null) return;
+            Debuffs.Remove(buff);
+            buff.Dispose();
+        }
+
         public User(Dispatcher d)
         {
             _dispatcher = d;
+            _debuffs = new SynchronizedObservableCollection<AbnormalityDuration>(d);
+            _buffs = new SynchronizedObservableCollection<AbnormalityDuration>(d);
         }
     }
 }
