d3316351bc83dc116348d19ddef067b18c9467d1
foglio <foglio1024@gmail.com>
2017-05-20 21:45:39 +0200

    moved counters to own files

---
+++ TCC.Core/Windows/ClassSpecificBars/Managers/Counter.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Windows.Threading;
+
+namespace TCC.ViewModels
+{
+    public class Counter : TSPropertyChanged
+    {
+        private int val = 0;
+        public int Val
+        {
+            get => val;
+            set
+            {
+                if (val == value) return;
+                val = value;
+                if (val == _max)
+                {
+                    NotifyPropertyChanged("Maxed");
+                }
+                RefreshTimer();
+                NotifyPropertyChanged("Val");
+            }
+        }
+
+        DispatcherTimer _expire;
+        int _max;
+        bool _autoexpire;
+        void RefreshTimer()
+        {
+            _expire.Stop();
+            if (val == 0) return;
+            if (!_autoexpire) return;
+            _expire.Start();
+        }
+        public Counter(int max, bool autoexpire)
+        {
+            _dispatcher = Dispatcher.CurrentDispatcher;
+            _max = max;
+            _autoexpire = autoexpire;
+            _expire = new DispatcherTimer() { Interval = TimeSpan.FromMilliseconds(8000) };
+            _expire.Tick += (s, ev) => Val = 0;
+        }
+    }
+}
---
+++ TCC.Core/Windows/ClassSpecificBars/Managers/IntTracker.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows.Threading;
+
+namespace TCC.ViewModels
+{
+    public class IntTracker : TSPropertyChanged
+    {
+        private int val = 0;
+        public int Val
+        {
+            get { return val; }
+            set
+            {
+                if (val == value) return;
+                val = value;
+
+                NotifyPropertyChanged("Val");
+            }
+        }
+        public IntTracker()
+        {
+            _dispatcher = Dispatcher.CurrentDispatcher;
+        }
+    }
+}
