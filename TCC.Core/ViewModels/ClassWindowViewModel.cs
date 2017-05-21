c479e62a8d5ac1b0fbf0f2251145d3ca3805ab3b
foglio <foglio1024@gmail.com>
2017-05-21 17:49:53 +0200

    moved class manager to own file

--- TCC.Core/ViewModels/ClassWindowViewModel.cs
+++ TCC.Core/ViewModels/ClassWindowViewModel.cs
@@ -5,8 +5,6 @@ using System.Linq;
 using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
-using System.Windows;
-using TCC.Data;
 
 namespace TCC.ViewModels
 {
@@ -61,6 +59,9 @@ namespace TCC.ViewModels
                         break;
                     case Class.Glaiver:
                         CurrentManager = ValkyrieBarManager.Instance;
+                        break;
+                    case Class.Archer:
+                        CurrentManager = ArcherBarManager.Instance;
                         break;
                     default:
                         CurrentManager = null;
@@ -157,93 +158,6 @@ namespace TCC.ViewModels
                 CurrentManager.OtherSkills.Clear();
             });
         }
-    }
-
-    public abstract class ClassManager : DependencyObject
-    {
-
-        public static ClassManager CurrentClassManager;
-
-
-        private SynchronizedObservableCollection<FixedSkillCooldown> mainSkills;
-        public SynchronizedObservableCollection<FixedSkillCooldown> MainSkills
-        {
-            get
-            {
-                return mainSkills; ;
-            }
-            set
-            {
-                if (mainSkills == value) return;
-                mainSkills = value;
-            }
-        }
-
-        private SynchronizedObservableCollection<FixedSkillCooldown> secondarySkills;
-        public SynchronizedObservableCollection<FixedSkillCooldown> SecondarySkills
-        {
-            get
-            {
-                return secondarySkills; ;
-            }
-            set
-            {
-                if (secondarySkills == value) return;
-                secondarySkills = value;
-            }
-        }
-
-        private SynchronizedObservableCollection<SkillCooldown> otherSkills;
-        public SynchronizedObservableCollection<SkillCooldown> OtherSkills
-        {
-            get
-            {
-                return otherSkills;
-            }
-            set
-            {
-                if (otherSkills == value) return;
-                otherSkills = value;
-            }
-        }
-
-        public abstract void StartCooldown(SkillCooldown sk);
-        public abstract void ResetCooldown(SkillCooldown sk);
-        public abstract void RemoveSkill(Skill sk);
-
-        protected abstract void LoadSkills(string filename, Class c);
-
-        public IntTracker HP { get; set; }
-        public IntTracker MP { get; set; }
-        public IntTracker ST { get; set; }
-
-        public static void SetHP(int hp)
-        {
-            if (CurrentClassManager == null) return;
-            WindowManager.ClassWindow.Dispatcher.Invoke(() => { CurrentClassManager.HP.Val = hp; });
-
-        }
-        public static void SetMP(int mp)
-        {
-            if (CurrentClassManager == null) return;
-            WindowManager.ClassWindow.Dispatcher.Invoke(() => { CurrentClassManager.MP.Val = mp; });
-        }
-        public static void SetST(int currentStamina)
-        {
-            if (CurrentClassManager == null) return;
-            WindowManager.ClassWindow.Dispatcher.Invoke(() => { CurrentClassManager.ST.Val = currentStamina; });
-        }
-
-        public ClassManager()
-        {
-            SecondarySkills = new SynchronizedObservableCollection<FixedSkillCooldown>(WindowManager.ClassWindow.Dispatcher);
-            MainSkills = new SynchronizedObservableCollection<FixedSkillCooldown>(WindowManager.ClassWindow.Dispatcher);
-            OtherSkills = new SynchronizedObservableCollection<SkillCooldown>(WindowManager.ClassWindow.Dispatcher);
-            HP = new IntTracker();
-            MP = new IntTracker();
-            ST = new IntTracker();
-        }
-
     }
 }
 
---
+++ TCC.Core/ViewModels/MultiThreadManagers/ClassManager.cs
@@ -0,0 +1,114 @@
+﻿using System.Windows;
+using TCC.Data;
+
+namespace TCC.ViewModels
+{
+    public abstract class ClassManager : DependencyObject
+    {
+        
+        public static ClassManager CurrentClassManager;
+
+
+        private SynchronizedObservableCollection<FixedSkillCooldown> mainSkills;
+        public SynchronizedObservableCollection<FixedSkillCooldown> MainSkills
+        {
+            get
+            {
+                return mainSkills; ;
+            }
+            set
+            {
+                if (mainSkills == value) return;
+                mainSkills = value;
+            }
+        }
+
+        private SynchronizedObservableCollection<FixedSkillCooldown> secondarySkills;
+        public SynchronizedObservableCollection<FixedSkillCooldown> SecondarySkills
+        {
+            get
+            {
+                return secondarySkills; ;
+            }
+            set
+            {
+                if (secondarySkills == value) return;
+                secondarySkills = value;
+            }
+        }
+
+        private SynchronizedObservableCollection<SkillCooldown> otherSkills;
+        public SynchronizedObservableCollection<SkillCooldown> OtherSkills
+        {
+            get
+            {
+                return otherSkills;
+            }
+            set
+            {
+                if (otherSkills == value) return;
+                otherSkills = value;
+            }
+        }
+
+        public abstract void StartCooldown(SkillCooldown sk);
+        public abstract void ResetCooldown(SkillCooldown sk);
+        public abstract void RemoveSkill(Skill sk);
+
+        protected abstract void LoadSkills(string filename, Class c);
+
+        public IntTracker HP { get; set; }
+        public IntTracker MP { get; set; }
+        public IntTracker ST { get; set; }
+
+        public static void SetHP(int hp)
+        {
+            if (CurrentClassManager == null) return;
+            WindowManager.ClassWindow.Dispatcher.Invoke(() => { CurrentClassManager.HP.Val = hp; });
+
+        }
+        public static void SetMP(int mp)
+        {
+            if (CurrentClassManager == null) return;
+            WindowManager.ClassWindow.Dispatcher.Invoke(() => { CurrentClassManager.MP.Val = mp; });
+        }
+        public static void SetST(int currentStamina)
+        {
+            if (CurrentClassManager == null) return;
+            WindowManager.ClassWindow.Dispatcher.Invoke(() => { CurrentClassManager.ST.Val = currentStamina; });
+        }
+
+        public ClassManager()
+        {
+            SecondarySkills = new SynchronizedObservableCollection<FixedSkillCooldown>(WindowManager.ClassWindow.Dispatcher);
+            MainSkills = new SynchronizedObservableCollection<FixedSkillCooldown>(WindowManager.ClassWindow.Dispatcher);
+            OtherSkills = new SynchronizedObservableCollection<SkillCooldown>(WindowManager.ClassWindow.Dispatcher);
+            HP = new IntTracker();
+            MP = new IntTracker();
+            ST = new IntTracker();
+        }
+        protected void AddOrRefreshSkill(SkillCooldown sk)
+        {
+            sk.SetDispatcher(this.Dispatcher);
+
+            try
+            {
+                var existing = OtherSkills.FirstOrDefault(x => x.Skill.Name == sk.Skill.Name);
+                if (existing == null)
+                {
+                    OtherSkills.Add(sk);
+                    return;
+                }
+                existing.Refresh(sk.Cooldown);
+            }
+            catch
+            {
+
+            }
+        }
+
+    }
+}
+
+
+
