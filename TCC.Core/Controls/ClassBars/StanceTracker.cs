47a9a66951ffc25f89d76abeb6ee0d5a6c5f47ab
foglio <foglio1024@gmail.com>
2017-05-24 19:09:51 +0200

    added stancetracker

---
+++ TCC.Core/Controls/ClassBars/StanceTracker.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Threading;
+
+namespace TCC.Data
+{
+    public enum ArcherStance
+    {
+        None, SniperEye
+    }
+    public enum WarriorStance
+    {
+        None, Assault, Defensive
+    }
+    public class StanceTracker<T> : TSPropertyChanged where T : IComparable
+    {
+        T _currentStance;
+        public T CurrentStance
+        {
+            get => _currentStance;
+            set
+            {
+                if (_currentStance.CompareTo(value) == 0) return;
+                _currentStance = value;
+                NotifyPropertyChanged("CurrentStance");
+            }
+        }
+        public StanceTracker()
+        {
+            _dispatcher = Dispatcher.CurrentDispatcher;
+        }
+    }
+}
