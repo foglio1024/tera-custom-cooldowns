c4c5f32a1bfe464ac387b517e42752f59b178737
foglio <foglio1024@gmail.com>
2017-05-26 22:36:54 +0200

    class specific abnormalities handlers

--- TCC.Core/ClassSpecific/Mystic.cs
+++ TCC.Core/ClassSpecific/Mystic.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using TCC.Parsing.Messages;
+using TCC.ViewModels;
 
 namespace TCC
 {
@@ -23,6 +24,11 @@ namespace TCC
             700730, 700731          //aura swift
         };
 
+        static uint[] critAuraIDs = { 700600, 700601, 700602, 700603 };
+        static uint[] manaAuraIDs = { 700300 };
+        static uint[] critResAuraIDs = { 700200, 700201, 700202, 700203 };
+        static uint[] swiftAuraIDs = { 700700, 700701 };
+
         public static void CheckHurricane(S_ABNORMALITY_BEGIN msg)
         {
             if(msg.Id == HURRICANE_ID) Console.WriteLine("Checking hurricane; id={0} caster={1} player={2}", msg.Id, msg.CasterId, SessionManager.CurrentPlayer.EntityId);
@@ -32,6 +38,63 @@ namespace TCC
                 SkillManager.AddSkillDirectly(hurricane, HURRICANE_DURATION);
             }
 
+        }
+        public static void CheckAura(S_ABNORMALITY_BEGIN p)
+        {
+            if(critAuraIDs.Contains(p.Id) && p.TargetId == SessionManager.CurrentPlayer.EntityId)
+            {
+                ((MysticBarManager)ClassManager.CurrentClassManager).Auras.CritAura = true;
+            }
+            else if (manaAuraIDs.Contains(p.Id) && p.TargetId == SessionManager.CurrentPlayer.EntityId)
+            {
+                ((MysticBarManager)ClassManager.CurrentClassManager).Auras.ManaAura = true;
+            }
+            else if (critResAuraIDs.Contains(p.Id) && p.TargetId == SessionManager.CurrentPlayer.EntityId)
+            {
+                ((MysticBarManager)ClassManager.CurrentClassManager).Auras.CritResAura = true;
+            }
+            else if (swiftAuraIDs.Contains(p.Id) && p.TargetId == SessionManager.CurrentPlayer.EntityId)
+            {
+                ((MysticBarManager)ClassManager.CurrentClassManager).Auras.SwiftAura = true;
+            }
+        }
+        public static void CheckAura(S_ABNORMALITY_REFRESH p)
+        {
+            if (critAuraIDs.Contains(p.AbnormalityId) && p.TargetId == SessionManager.CurrentPlayer.EntityId)
+            {
+                ((MysticBarManager)ClassManager.CurrentClassManager).Auras.CritAura = true;
+            }
+            else if (manaAuraIDs.Contains(p.AbnormalityId) && p.TargetId == SessionManager.CurrentPlayer.EntityId)
+            {
+                ((MysticBarManager)ClassManager.CurrentClassManager).Auras.ManaAura = true;
+            }
+            else if (critResAuraIDs.Contains(p.AbnormalityId) && p.TargetId == SessionManager.CurrentPlayer.EntityId)
+            {
+                ((MysticBarManager)ClassManager.CurrentClassManager).Auras.CritResAura = true;
+            }
+            else if (swiftAuraIDs.Contains(p.AbnormalityId) && p.TargetId == SessionManager.CurrentPlayer.EntityId)
+            {
+                ((MysticBarManager)ClassManager.CurrentClassManager).Auras.SwiftAura = true;
+            }
+        }
+        public static void CheckAuraEnd(S_ABNORMALITY_END p)
+        {
+            if (critAuraIDs.Contains(p.Id) && p.Target == SessionManager.CurrentPlayer.EntityId)
+            {
+                ((MysticBarManager)ClassManager.CurrentClassManager).Auras.CritAura = false;
+            }
+            else if (manaAuraIDs.Contains(p.Id) && p.Target == SessionManager.CurrentPlayer.EntityId)
+            {
+                ((MysticBarManager)ClassManager.CurrentClassManager).Auras.ManaAura = false;
+            }
+            else if (critResAuraIDs.Contains(p.Id) && p.Target == SessionManager.CurrentPlayer.EntityId)
+            {
+                ((MysticBarManager)ClassManager.CurrentClassManager).Auras.CritResAura = false;
+            }
+            else if (swiftAuraIDs.Contains(p.Id) && p.Target == SessionManager.CurrentPlayer.EntityId)
+            {
+                ((MysticBarManager)ClassManager.CurrentClassManager).Auras.SwiftAura = false;
+            }
         }
     }
 }
--- TCC.Core/ClassSpecific/Priest.cs
+++ TCC.Core/ClassSpecific/Priest.cs
@@ -3,8 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TCC.Parsing.Messages;
+using TCC.ViewModels;
 
-namespace TCC.ClassSpecific
+namespace TCC
 {
     public static class Priest
     {
@@ -15,5 +17,22 @@ namespace TCC.ClassSpecific
             800300, 800301, 800302,800303, 800304
         };
 
+        static readonly uint[] energyStarsIDs = { 801500, 801501 , 801502 , 801503, 98000107 };
+        
+        internal static void CheckBuff(S_ABNORMALITY_BEGIN p)
+        {
+            if(p.TargetId == SessionManager.CurrentPlayer.EntityId && energyStarsIDs.Contains(p.Id))
+            {
+                ((PriestBarManager)ClassManager.CurrentClassManager).EnergyStars.Buff.Start(p.Duration);
+            }
+        }
+
+        internal static void CheckBuff(S_ABNORMALITY_REFRESH p)
+        {
+            if (p.TargetId == SessionManager.CurrentPlayer.EntityId && energyStarsIDs.Contains(p.AbnormalityId))
+            {
+                ((PriestBarManager)ClassManager.CurrentClassManager).EnergyStars.Buff.Refresh(p.Duration);
+            }
+        }
     }
 }
