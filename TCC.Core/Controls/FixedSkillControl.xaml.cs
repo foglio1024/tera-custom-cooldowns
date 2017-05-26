007dc1bd5a87fe89974b510f11422a4e7913489e
foglio <foglio1024@gmail.com>
2017-05-26 22:42:47 +0200

    removed commented code - added different anim types (unused for now)

--- TCC.Core/Controls/FixedSkillControl.xaml.cs
+++ TCC.Core/Controls/FixedSkillControl.xaml.cs
@@ -29,22 +29,9 @@ namespace TCC.Controls
             InitializeComponent();
         }
 
-        //DispatcherTimer _numberTimer;
         FixedSkillCooldown _context;
 
-        //private double currentCD;
-        //public double CurrentCD
-        //{
-        //    get { return currentCD; }
-        //    set
-        //    {
-        //        if (currentCD != value)
-        //        {
-        //            currentCD = value;
-        //            NotifyPropertyChanged("CurrentCD");
-        //        }
-        //    }
-        //}
+
 
         private bool isRunning = false;
         public bool IsRunning
@@ -69,10 +56,6 @@ namespace TCC.Controls
             if (DesignerProperties.GetIsInDesignMode(this)) return;
             _context = (FixedSkillCooldown)DataContext;
             _context.PropertyChanged += _context_PropertyChanged;
-            //_numberTimer = new DispatcherTimer() { Interval = TimeSpan.FromMilliseconds(1000) };
-            //_numberTimer.Tick += (s, o) => {
-            //    CurrentCD = CurrentCD - 1 > 0 ? CurrentCD - 1 : 0;
-            //};
         }
 
         void _context_PropertyChanged(object sender, PropertyChangedEventArgs e)
@@ -80,38 +63,49 @@ namespace TCC.Controls
             if (e.PropertyName == "Refresh")
             {
                 if (_context.Cooldown == _context.OriginalCooldown) return;
-                //_numberTimer.Stop();
-                //CurrentCD = (double)_context.Cooldown / 1000;
-                double newAngle = (double)_context.Cooldown / (double)_context.OriginalCooldown;
-                if (_context.Cooldown == 0) newAngle = 0;
-                if (newAngle > 1) newAngle = 1;
+                double newVal = (double)_context.Cooldown / (double)_context.OriginalCooldown;
+                if (_context.Cooldown == 0) newVal = 0;
+                if (newVal > 1) newVal = 1;
 
-                AnimateCooldown(newAngle);
+                AnimateArcAngle(newVal);
             }
             else if (e.PropertyName == "Start")
             {
                 IsRunning = true;
-                //CurrentCD = (double)_context.Cooldown / 1000;
-                //_numberTimer.IsEnabled = false;
-                AnimateCooldown();
-
+                AnimateArcAngle();
             }
         }
-        void AnimateCooldown(double angle = 1)
+
+        void AnimateArcAngle(double val = 1)
         {
-            var an = new DoubleAnimation(angle * 359.9, 0, TimeSpan.FromMilliseconds(_context.Cooldown));
+            var an = new DoubleAnimation(val * 359.9, 0, TimeSpan.FromMilliseconds(_context.Cooldown));
             an.Completed += An_Completed;
             int fps = _context.Cooldown > 80000 ? 1 : 30;
             DoubleAnimation.SetDesiredFrameRate(an, fps);
             arc.BeginAnimation(Arc.EndAngleProperty, an);
-            //_numberTimer.IsEnabled = true;
-
+        }
+        void AnimateArcThickness(double val = 1)
+        {
+            var an = new DoubleAnimation(val * 25, 0, TimeSpan.FromMilliseconds(_context.Cooldown));
+            an.Completed += An_Completed;
+            int fps = _context.Cooldown > 80000 ? 1 : 30;
+            DoubleAnimation.SetDesiredFrameRate(an, fps);
+            arc.BeginAnimation(Arc.StrokeThicknessProperty, an);
+        }
+        void AnimateEllipseSize(double val = 0)
+        {
+            if (val == 0) val = 1;
+            var an = new DoubleAnimation(val, 0, TimeSpan.FromMilliseconds(_context.Cooldown));
+            an.Completed += An_Completed;
+            int fps = _context.Cooldown > 80000 ? 1 : 30;
+            DoubleAnimation.SetDesiredFrameRate(an, fps);
+            growEllipse.LayoutTransform.BeginAnimation(ScaleTransform.ScaleXProperty, an);
+            growEllipse.LayoutTransform.BeginAnimation(ScaleTransform.ScaleYProperty, an);
         }
 
         private void An_Completed(object sender, EventArgs e)
         {
             IsRunning = false;
-            //_numberTimer.IsEnabled = false;
             var an = new DoubleAnimation(1, 0, TimeSpan.FromMilliseconds(200)) { EasingFunction = new QuadraticEase() };
             glow.BeginAnimation(OpacityProperty, an);
         }
