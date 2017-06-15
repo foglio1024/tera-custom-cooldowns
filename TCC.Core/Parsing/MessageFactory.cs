849655423d9bc40fe62c4988e16c81a277530fb2
foglio <foglio1024@gmail.com>
2017-06-15 12:47:36 +0200

    added S_CHAT to factory

--- TCC.Core/Parsing/MessageFactory.cs
+++ TCC.Core/Parsing/MessageFactory.cs
@@ -66,6 +66,7 @@ namespace TCC.Parsing
             {"S_CHANGE_PARTY_MANAGER", Contructor<Func<TeraMessageReader, S_CHANGE_PARTY_MANAGER>>() },
             {"S_WEAK_POINT", Contructor<Func<TeraMessageReader, S_WEAK_POINT>>() },
             {"S_EACH_SKILL_RESULT", Contructor<Func<TeraMessageReader, S_EACH_SKILL_RESULT>>() },
+            {"S_CHAT", Contructor<Func<TeraMessageReader, S_CHAT>>() },
 
         };
 
@@ -125,6 +126,7 @@ namespace TCC.Parsing
             {typeof(S_CHANGE_PARTY_MANAGER), new Action<S_CHANGE_PARTY_MANAGER>(x => PacketProcessor.HandleChangeLeader(x)) },
             {typeof(S_WEAK_POINT), new Action<S_WEAK_POINT>(x => PacketProcessor.HandleRunemark(x)) },
             {typeof(S_EACH_SKILL_RESULT), new Action<S_EACH_SKILL_RESULT>(x => PacketProcessor.HandleSkillResult(x)) },
+            {typeof(S_CHAT), new Action<S_CHAT>(x => PacketProcessor.HandleChat(x)) },
 
         };
 
