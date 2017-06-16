534792a9d3c66ff5dd7be5968e6b5b50befdf05c
foglio <foglio1024@gmail.com>
2017-06-17 00:42:03 +0200

    added chat channels enum

--- TCC.Core/Parsing/Messages/S_CHAT.cs
+++ TCC.Core/Parsing/Messages/S_CHAT.cs
@@ -8,30 +8,6 @@ using Tera.Game.Messages;
 
 namespace TCC.Parsing.Messages
 {
-    public enum ChatChannel
-    {
-        Say = 0,
-        Party = 1,
-        Guild = 2,
-        Area = 3,
-        Trade = 4,
-        Greet = 9,
-        Private1 = 11,
-        Private2 = 12,
-        Private3 = 13,
-        Private4 = 14,
-        Private5 = 15,
-        Private6 = 16,
-        Private7 = 17,
-        Private8 = 18,
-        PartyNotice = 21,
-        RaidNotice = 25,
-        Emote = 26,
-        Global = 27,
-        Raid = 32,
-        Megaphone = 213,
-        GuildAdvertising = 214
-    }
     public class S_CHAT : ParsedMessage
     {
         ushort authorNameOffset, messageOffset;
