484c023052caa2270ff480be91cdaef9bebc1bb5
foglio <foglio1024@gmail.com>
2017-06-17 00:42:53 +0200

    moved chatchannels to enums

--- TCC.Core/Data/Enums.cs
+++ TCC.Core/Data/Enums.cs
@@ -6,6 +6,31 @@ using System.Threading.Tasks;
 
 namespace TCC
 {
+    public enum ChatChannel
+    {
+        Say = 0,
+        Party = 1,
+        Guild = 2,
+        Area = 3,
+        Trade = 4,
+        Greet = 9,
+        Private1 = 11,
+        Private2 = 12,
+        Private3 = 13,
+        Private4 = 14,
+        Private5 = 15,
+        Private6 = 16,
+        Private7 = 17,
+        Private8 = 18,
+        PartyNotice = 21,
+        RaidNotice = 25,
+        Emote = 26,
+        Global = 27,
+        Raid = 32,
+        Megaphone = 213,
+        GuildAdvertising = 214
+    }
+
     public enum Class
     {
         Warrior = 0,
