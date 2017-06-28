378daa02f2cab3b6cfdd2b07e65c477a37ea51f0
foglio <foglio1024@gmail.com>
2017-06-28 12:01:22 +0200

    added more messages

--- TCC.Core/ProxyInterop.cs
+++ TCC.Core/ProxyInterop.cs
@@ -7,6 +7,7 @@ using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 using System.Timers;
+using TCC.ViewModels;
 
 namespace TCC
 {
@@ -18,9 +19,9 @@ namespace TCC
         {
             try
             {
+                if (client.Client != null && client.Connected) return;
                 Debug.WriteLine("Connecting...");
-                if (client.Client == null) client = new TcpClient();
-                if (client.Connected) return;
+                client = new TcpClient();
                 client.Connect("127.0.0.50", 9550);
                 Debug.WriteLine("Connected");
             }
@@ -30,9 +31,10 @@ namespace TCC
                 Debug.WriteLine(e.Message);
                 Task.Delay(2000).ContinueWith(t =>
                 {
-                    if (retries == 0)
+                    if (retries <= 0)
                     {
                         Debug.WriteLine("Maximum retries exceeded...");
+                        retries = 2;
                         return;
                     }
                     ConnectToProxy();
@@ -52,10 +54,12 @@ namespace TCC
             catch (Exception e)
             {
                 Debug.WriteLine(e.Message);
+                retries = 2;
+                ConnectToProxy();
             }
         }
 
-        public static void SendExTooltipMessage(ulong itemUid, string ownerName)
+        public static void SendExTooltipMessage(long itemUid, string ownerName)
         {
             var sb = new StringBuilder("ex_tooltip");
             sb.Append("&uid=");
@@ -74,9 +78,81 @@ namespace TCC
             SendData(sb.ToString());
         }
 
-        internal static void CloseConnection()
+        public static void SendFriendRequestMessage(string name, string message)
         {
-            client?.Client.Close();
+            var sb = new StringBuilder("friend_req");
+            sb.Append("&name=");
+            sb.Append(name);
+            sb.Append("&msg=");
+            sb.Append(message);
+
+            SendData(sb.ToString());
+        }
+        public static void SendUnfriendUserMessage(string name)
+        {
+            var sb = new StringBuilder("unfriend");
+            sb.Append("&name=");
+            sb.Append(name);
+
+            SendData(sb.ToString());
+        }
+
+        public static void SendBlockUserMessage(string name)
+        {
+            var sb = new StringBuilder("block");
+            sb.Append("&name=");
+            sb.Append(name);
+
+            SendData(sb.ToString());
+        }
+        public static void SendUnblockUserMessage(string name)
+        {
+            var sb = new StringBuilder("unblock");
+            sb.Append("&name=");
+            sb.Append(name);
+
+            SendData(sb.ToString());
+        }
+        public static void SendAskInteractiveMessage(uint srvId, string name)
+        {
+            var sb = new StringBuilder("ask_int");
+            sb.Append("&srvId=");
+            sb.Append(srvId);
+            sb.Append("&name=");
+            sb.Append(name);
+
+            SendData(sb.ToString());
+        }
+        public static void SendInspect(string name)
+        {
+            var sb = new StringBuilder("inspect");
+            sb.Append("&name=");
+            sb.Append(name);
+
+            SendData(sb.ToString());
+        }
+        public static void SendPartyInvite(string name)
+        {
+            var sb = new StringBuilder("inv_party");
+            sb.Append("&name=");
+            sb.Append(name);
+            sb.Append("&raid=");
+            sb.Append(GroupWindowManager.Instance.Raid ? 1 : 0);
+
+            SendData(sb.ToString());
+        }
+        public static void SendGuildInvite(string name)
+        {
+            var sb = new StringBuilder("inv_guild");
+            sb.Append("&name=");
+            sb.Append(name);
+
+            SendData(sb.ToString());
+        }
+
+        public static void CloseConnection()
+        {
+            client?.Client?.Close();
             client?.Close();
         }
     }
