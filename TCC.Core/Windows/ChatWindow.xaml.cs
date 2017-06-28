4f9305fe4c606d34314c2a477b322584ae5f4f6c
foglio <foglio1024@gmail.com>
2017-06-28 12:43:27 +0200

    chat window WIP

--- TCC.Core/Windows/ChatWindow.xaml.cs
+++ TCC.Core/Windows/ChatWindow.xaml.cs
@@ -1,284 +1,230 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
+using System.Windows.Controls.Primitives;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 using System.Windows.Threading;
+using TCC.Controls;
+using TCC.Controls.ChatControls;
 using TCC.Data;
 using TCC.ViewModels;
 
 namespace TCC.Windows
 {
-    /// <summary>
-    /// Logica di interazione per ChatWindow.xaml
-    /// </summary>
+
     public partial class ChatWindow : TccWindow
     {
-        public ChatWindow()
-        {
-            InitializeComponent();
-        }
-        ScrollViewer _scroller;
-        bool _bottom = true;
         DispatcherTimer testTimer;
+        bool _bottom = true;
+        int _testCounter = 0;
+
+        public object CurrentSender { get; internal set; }
+
         private void TccWindow_Loaded(object sender, RoutedEventArgs e)
         {
             //InitWindow(SettingsManager.ChatWindowSettings);
-            itemsControl.ItemsSource = ChatWindowViewModel.Instance.ChatMessages;
+
             ChatWindowViewModel.Instance.PropertyChanged += Instance_PropertyChanged;
-            _scroller = (ScrollViewer)VisualTreeHelper.GetChild(itemsControl, 0);
-            _scroller.ScrollChanged += Scroller_ScrollChanged;
+            //_scroller = (ScrollViewer)VisualTreeHelper.GetChild(itemsControl, 0);
+            //_scroller.ScrollChanged += Scroller_ScrollChanged;
             testTimer = new DispatcherTimer();
-            testTimer.Interval = TimeSpan.FromMilliseconds(1000);
+            testTimer.Interval = TimeSpan.FromMilliseconds(100);
             testTimer.Tick += (s, ev) =>
             {
-                ChatWindowViewModel.Instance.AddChatMessage(new ChatMessage(ChatChannel.Say, "Author", "<FONT>SayMessage " + i + "</FONT>"));
-                ChatWindowViewModel.Instance.AddChatMessage(new ChatMessage(ChatChannel.ReceivedWhisper, "Author", "<FONT>ReceivedWhisperLooooooooooooooooooo ooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo oooooooooooongMessage " + i + "</FONT>"));
-                ChatWindowViewModel.Instance.AddChatMessage(new ChatMessage(ChatChannel.Megaphone, "Author", "<FONT>SentWhisperMessage " + i + "</FONT>"));
-                i++;
+                ChatWindowViewModel.Instance.AddChatMessage(new ChatMessage(ChatChannel.Say, "Author", "<FONT>SayMessage " + _testCounter + "</FONT>"));
+                ChatWindowViewModel.Instance.AddChatMessage(new ChatMessage(ChatChannel.Party, "Author2", "<FONT>SayMessage " + _testCounter + "</FONT>"));
+                ChatWindowViewModel.Instance.AddChatMessage(new ChatMessage(ChatChannel.Guild, "Author3", "<FONT>SayMessage " + _testCounter + "</FONT>"));
+                _testCounter++;
             };
             //testTimer.Start();
+            //for (int i = 0; i < 100; i++)
+            //{
+            //    ChatWindowViewModel.Instance.AddChatMessage(new ChatMessage(ChatChannel.Guild, "Author" + i, "<FONT>GuildMessage " + i + "</FONT>"));
 
+            //}
+            //ChatWindowViewModel.Instance.AddChatMessage(new ChatMessage(ChatChannel.Party, "Author", "<FONT>PartyMessage eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee" + "</FONT>"));
+            //for (int i = 0; i < 10; i++)
+            //{
+            //    ChatWindowViewModel.Instance.AddChatMessage(new ChatMessage(ChatChannel.System, "System", "<FONT>SysMessage " + i + "</FONT>"));
+
+            //}
         }
-        int i = 0;
-        private void Scroller_ScrollChanged(object sender, ScrollChangedEventArgs e)
-        {
-            if(_scroller.VerticalOffset == _scroller.ScrollableHeight)
-            {
-                _bottom = true;
-            }
-            else
-            {
-                _bottom = false;
-            }
-        }
+
 
         private void Instance_PropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
         {
-            if(e.PropertyName == "NewItem")
+            if (e.PropertyName == "NewItem")
             {
-                if(_bottom) _scroller.ScrollToBottom();
+                if (_bottom)
+                {
+                    var t = tabControl.SelectedContent as ItemsControl;
+                    var b = (Border)VisualTreeHelper.GetChild(t, 0);
+                    var s = (ScrollViewer)VisualTreeHelper.GetChild(b, 0);
+
+                    s.ScrollToBottom();
+                }
             }
+        }
+        bool _locked;
+        internal void LockTooltip(bool locked)
+        {
+            _locked = locked;
         }
 
         private void TccWindow_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
             DragMove();
         }
-
         private void SWPreviewMouseWheel(object sender, MouseWheelEventArgs e)
         {
             ScrollViewer s = (ScrollViewer)sender;
-            s.ScrollToVerticalOffset(s.VerticalOffset - e.Delta/2);
-        }
-    }
-    public class ChannelLabelDataTemplateSelector : DataTemplateSelector
-    {
-        public DataTemplate NormalChannelDataTemplate { get; set; }
-        public DataTemplate WhisperChannelDataTemplate { get; set; }
-        public DataTemplate MegaphoneChannelDataTemplate { get; set; }
-        public override DataTemplate SelectTemplate(object item, DependencyObject container)
-        {
-            ChatMessage m = item as ChatMessage;
-            switch (m.Channel)
+            if (s.VerticalOffset == s.ScrollableHeight)
             {
-                case ChatChannel.SentWhisper:
-                    return WhisperChannelDataTemplate;
-                case ChatChannel.ReceivedWhisper:
-                    return WhisperChannelDataTemplate;
-                case ChatChannel.Megaphone:
-                    return MegaphoneChannelDataTemplate;
-                default:
-                    return NormalChannelDataTemplate;
-            }
-        }
-    }
-
-    #region Converters
-
-    public class ChatColorConverter : IValueConverter
-    {
-        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
-        {
-            var ch = (ChatChannel)value;
-            Color col;
-            switch (ch)
-            {
-                case ChatChannel.Say:
-                    col = Colors.White;
-                    break;
-                case ChatChannel.Party:
-                    col = Color.FromRgb(0, 113, 187);
-                    break;
-                case ChatChannel.Guild:
-                    col = Color.FromRgb(64, 251, 64);
-                    break;
-                case ChatChannel.Area:
-                    col = Color.FromRgb(186, 130, 242);
-                    break;
-                case ChatChannel.Trade:
-                    col = Color.FromRgb(192, 122, 129);
-                    break;
-                case ChatChannel.Greet:
-                    col = Color.FromRgb(220, 220, 220);
-                    break;
-                case ChatChannel.PartyNotice:
-                    col = Color.FromRgb(142, 255, 255);
-                    break;
-                case ChatChannel.RaidNotice:
-                    col = Color.FromRgb(242, 135, 48);
-                    break;
-                case ChatChannel.Emote:
-                    col = Color.FromRgb(255, 185, 185);
-                    break;
-                case ChatChannel.Global:
-                    col = Color.FromRgb(240, 255, 137);
-                    break;
-                case ChatChannel.Raid:
-                    col = Color.FromRgb(255, 255, 0);
-                    break;
-                case ChatChannel.Megaphone:
-                    col = Color.FromRgb(0, 216, 255);
-                    break;
-                case ChatChannel.GuildAdvertising:
-                    col = Color.FromRgb(112, 196, 1);
-                    break;
-                case ChatChannel.Private1:
-                    col = Color.FromRgb(255, 95, 56);
-                    break;
-                case ChatChannel.Private2:
-                    col = Color.FromRgb(255, 95, 56);
-                    break;
-                case ChatChannel.Private3:
-                    col = Color.FromRgb(255, 95, 56);
-                    break;
-                case ChatChannel.Private4:
-                    col = Color.FromRgb(255, 95, 56);
-                    break;
-                case ChatChannel.Private5:
-                    col = Color.FromRgb(255, 95, 56);
-                    break;
-                case ChatChannel.Private6:
-                    col = Color.FromRgb(255, 95, 56);
-                    break;
-                case ChatChannel.Private7:
-                    col = Color.FromRgb(255, 95, 56);
-                    break;
-                case ChatChannel.Private8:
-                    col = Color.FromRgb(255, 95, 56);
-                    break;
-                case ChatChannel.SentWhisper:
-                    col = Color.FromRgb(244, 121, 244);
-                    break;
-                case ChatChannel.ReceivedWhisper:
-                    col = Color.FromRgb(244, 121, 244);
-                    break;
-                default:
-                    col = Color.FromRgb(220, 220, 220);
-                    break;
-            }
-            return new SolidColorBrush(col);
-        }
-
-        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
-        {
-            throw new NotImplementedException();
-        }
-    }
-    public class ChatChannelNameConverter : IValueConverter
-    {
-        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
-        {
-            var ch = (ChatChannel)value;
-            switch (ch)
-            {
-                case ChatChannel.PartyNotice:
-                    return "Notice";
-                case ChatChannel.RaidNotice:
-                    return "Notice";
-                case ChatChannel.GuildAdvertising:
-                    return "G. Ad";
-                case ChatChannel.Megaphone:
-                    return "Shout";
-                case ChatChannel.Private1:
-                    return ChatMessage.PrivateChannels[0].Name;
-                case ChatChannel.Private2:
-                    return ChatMessage.PrivateChannels[1].Name;
-                case ChatChannel.Private3:
-                    return ChatMessage.PrivateChannels[2].Name;
-                case ChatChannel.Private4:
-                    return ChatMessage.PrivateChannels[3].Name;
-                case ChatChannel.Private5:
-                    return ChatMessage.PrivateChannels[4].Name;
-                case ChatChannel.Private6:
-                    return ChatMessage.PrivateChannels[5].Name;
-                case ChatChannel.Private7:
-                    return ChatMessage.PrivateChannels[6].Name;
-                case ChatChannel.Private8:
-                    return ChatMessage.PrivateChannels[7].Name;
-                case ChatChannel.ReceivedWhisper:
-                    return "W <";
-                case ChatChannel.SentWhisper:
-                    return "W >";
-                default:
-                    return ch.ToString();
-            }
-        }
-
-        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
-        {
-            throw new NotImplementedException();
-        }
-    }
-    public class MentionToColorConverter : IValueConverter
-    {
-        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
-        {
-            if ((bool)value)
-            {
-                return Brushes.Orange; 
+                _bottom = true;
             }
             else
             {
-                return Brushes.Black;
+                _bottom = false;
+
             }
         }
-
-        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        public void OpenTooltip()
         {
-            throw new NotImplementedException();
-        }
-    }
-    public class SentReceivedToAngleConverter : IValueConverter
-    {
-        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
-        {
-            var c = (ChatChannel)value;
-
-            switch (c)
+            Dispatcher.Invoke(() =>
             {
-                case ChatChannel.SentWhisper:
-                    return 1;
-                case ChatChannel.ReceivedWhisper:
-                    return -1;
-                default:
-                    return 0;
+                if (playerInfo.IsOpen) CloseTooltip();
+                ChatWindowViewModel.Instance.TooltipInfo.Refresh();
+                playerInfo.IsOpen = true;
+                ((PlayerTooltip)playerInfo.Child).AnimateOpening();
+            });
+        }
+        public void CloseTooltip()
+        {
+            Dispatcher.Invoke(() =>
+            {
+                if (_locked) return;
+                playerInfo.IsOpen = false;
+            });
+        }
+        public ChatWindow()
+        {
+            InitializeComponent();
+
+        }
+
+        private void playerInfo_MouseLeave(object sender, MouseEventArgs e)
+        {
+            CloseTooltip();
+        }
+
+        private void TabClicked(object sender, MouseButtonEventArgs e)
+        {
+
+            var s = sender as FrameworkElement;
+            var w = s.ActualWidth;
+            Point r = s.TranslatePoint(new Point(0, 0), (UIElement)s.Parent);
+            var sizeAn = new DoubleAnimation(w, TimeSpan.FromMilliseconds(250)) { EasingFunction = new QuadraticEase() };
+            var posAn = new DoubleAnimation(r.X, TimeSpan.FromMilliseconds(250)) { EasingFunction = new QuadraticEase() };
+
+            selectionRect.BeginAnimation(WidthProperty, sizeAn);
+            selectionRect.RenderTransform.BeginAnimation(TranslateTransform.XProperty, posAn);
+
+            var t = tabControl.SelectedContent as ItemsControl;
+            var b = (Border)VisualTreeHelper.GetChild(t, 0);
+            var sw = (ScrollViewer)VisualTreeHelper.GetChild(b, 0);
+
+            sw.ScrollToBottom();
+
+
+        }
+
+        private void ScrollViewer_ScrollChanged(object sender, ScrollChangedEventArgs e)
+        {
+            var t = tabControl.SelectedContent as ItemsControl;
+            var sp = GetInnerStackPanel(t);
+            if (sp.Children.Count > 2)
+            {
+                for (int i = 0; i < sp.Children.Count; i++)
+                {
+                    var el = (sp.Children[i] as FrameworkElement);
+                    if(i <= 2)
+                    {
+                        if (el != null && el.ActualHeight > 22)
+                        {
+                            // set some property in (ChatMessage)DataContext to indicate that the message is collapsed
+                            //el.Height = 22;
+                        }
+                    }
+                    else
+                    {
+                        el.Height = Double.NaN;
+                    }
+                }
+
             }
         }
 
-        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        private VirtualizingStackPanel GetInnerStackPanel(FrameworkElement element)
         {
-            throw new NotImplementedException();
+            for (int i = 0; i < VisualTreeHelper.GetChildrenCount(element); i++)
+            {
+                var child = VisualTreeHelper.GetChild(element, i) as FrameworkElement;
+
+                if (child == null) continue;
+
+
+                if (child is VirtualizingStackPanel) return child as VirtualizingStackPanel;
+
+                var panel = GetInnerStackPanel(child);
+
+                if (panel != null)
+                    return panel;
+            }
+
+            return null;
+
         }
     }
-    #endregion
+
+}
+namespace TCC
+{
+    public static class ChatWindowBrushes
+    {
+        public static SolidColorBrush Say = new SolidColorBrush(Colors.White);
+        public static SolidColorBrush Party = new SolidColorBrush(Color.FromRgb(0, 113, 187));
+        public static SolidColorBrush Guild = new SolidColorBrush(Color.FromRgb(64, 251, 64));
+        public static SolidColorBrush Area = new SolidColorBrush(Color.FromRgb(186, 130, 242));
+        public static SolidColorBrush Trade = new SolidColorBrush(Color.FromRgb(192, 122, 129));
+        public static SolidColorBrush Greet = new SolidColorBrush(Color.FromRgb(220, 220, 220));
+        public static SolidColorBrush PartyNotice = new SolidColorBrush(Color.FromRgb(142, 255, 255));
+        public static SolidColorBrush RaidNotice = new SolidColorBrush(Color.FromRgb(242, 135, 48));
+        public static SolidColorBrush Emote = new SolidColorBrush(Color.FromRgb(255, 185, 185));
+        public static SolidColorBrush Global = new SolidColorBrush(Color.FromRgb(240, 255, 137));
+        public static SolidColorBrush Raid = new SolidColorBrush(Color.FromRgb(255, 255, 0));
+        public static SolidColorBrush Megaphone = new SolidColorBrush(Color.FromRgb(0, 216, 255));
+        public static SolidColorBrush GuildAd = new SolidColorBrush(Color.FromRgb(112, 196, 1));
+        public static SolidColorBrush Private = new SolidColorBrush(Color.FromRgb(255, 95, 56));
+        public static SolidColorBrush Whisper = new SolidColorBrush(Color.FromRgb(244, 121, 244));
+        public static SolidColorBrush SystemGeneric = new SolidColorBrush(Color.FromRgb(0xc8, 0xc8, 0xb6));
+        public static SolidColorBrush SystemNotify = new SolidColorBrush(Color.FromRgb(0xc8, 0xc8, 0xb6));
+        public static SolidColorBrush SystemEvent = new SolidColorBrush(Color.FromRgb(0xc8, 0xc8, 0xb6));
+        public static SolidColorBrush SystemError = new SolidColorBrush(Colors.Red);
+        public static SolidColorBrush SystemGroup = new SolidColorBrush(Color.FromRgb(0xd9, 0xd9, 0xd9));
+        public static SolidColorBrush Deathmatch = new SolidColorBrush(Color.FromRgb(0xd9, 0xd9, 0xd9));
+        public static SolidColorBrush ContractAlert = new SolidColorBrush(Color.FromRgb(0xd9, 0xd9, 0xd9));
+        public static SolidColorBrush GroupAlert = new SolidColorBrush(Color.FromRgb(0xd9, 0xd9, 0xd9));
+        public static SolidColorBrush Loot = new SolidColorBrush(Color.FromRgb(0xd9, 0xd9, 0xd9));
+        public static SolidColorBrush Exp = new SolidColorBrush(Color.FromRgb(0xd9, 0xd9, 0xd9));
+        public static SolidColorBrush Money = new SolidColorBrush(Color.FromRgb(0xd9, 0xd9, 0xd9));
+    }
 }
