285e8bf8f1380faca18cfc19b04bc16bc6acb3bb
foglio <foglio1024@gmail.com>
2017-06-28 12:18:51 +0200

    moved template selectors to own folder

---
+++ TCC.Core/TemplateSelectors/MessagePieceDataTemplateSelector.cs
@@ -0,0 +1,36 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using TCC.Data;
+
+namespace TCC.TemplateSelectors
+{
+    public class MessagePieceDataTemplateSelector : DataTemplateSelector
+    {
+        public DataTemplate SimpleTemplate { get; set; }
+        public DataTemplate ItemTemplate { get; set; }
+        public DataTemplate QuestTemplate { get; set; }
+        public DataTemplate PointOfInterestTemplate { get; set; }
+        public DataTemplate MoneyTemplate { get; set; }
+
+        public override DataTemplate SelectTemplate(object item, DependencyObject container)
+        {
+            MessagePiece m = item as MessagePiece;
+            if (m == null) return null;
+            switch (m.Type)
+            {
+                case MessagePieceType.Simple:
+                    return SimpleTemplate;
+                case MessagePieceType.Item:
+                    return ItemTemplate;
+                case MessagePieceType.Quest:
+                    return QuestTemplate;
+                case MessagePieceType.Point_of_interest:
+                    return PointOfInterestTemplate;
+                case MessagePieceType.Money:
+                    return MoneyTemplate;
+                default:
+                    return SimpleTemplate;
+            }
+        }
+    }
+}
