4f5ee6f45d96cfaf39b0892ed8736472f11691a1
foglio <foglio1024@gmail.com>
2017-07-16 16:40:45 +0200

    added visible/hidden converter

---
+++ TCC.Core/Converters/BoolToVisibleHidden.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace TCC.Converters
+{
+    public class BoolToVisibleHidden : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if ((bool)value) return Visibility.Visible;
+            else return Visibility.Hidden;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
