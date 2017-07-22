5ead9e0385fa9b850d7781fbd8edceeca0a59de6
foglio <foglio1024@gmail.com>
2017-07-22 12:35:27 +0200

    removed old users list, reset aggro on boss despawn

--- TCC.Core/EntitiesManager.cs
+++ TCC.Core/EntitiesManager.cs
@@ -11,7 +11,6 @@ namespace TCC
 {
     public static class EntitiesManager
     {
-        public static ObservableCollection<Player> CurrentUsers = new ObservableCollection<Player>();
         public static MonsterDatabase CurrentDatabase;
         private static ulong currentEncounter;
         public static void SpawnNPC(ushort zoneId, uint templateId, ulong entityId, Visibility v)
@@ -42,7 +41,11 @@ namespace TCC
         public static void DespawnNPC(ulong target)
         {
             BossGageWindowViewModel.Instance.RemoveBoss(target);
-            if (BossGageWindowViewModel.Instance.VisibleBossesCount == 0) SessionManager.Encounter = false;
+            if (BossGageWindowViewModel.Instance.VisibleBossesCount == 0)
+            {
+                SessionManager.Encounter = false;
+                GroupWindowViewModel.Instance.ResetAggro();
+            }
         }
         public static void SetNPCStatus(ulong entityId, bool enraged)
         {
@@ -79,13 +82,6 @@ namespace TCC
         public static void ClearNPC()
         {
             BossGageWindowViewModel.Instance.ClearBosses();
-        }
-        public static void ClearUsers()
-        {
-            App.Current.Dispatcher.Invoke(() =>
-            {
-                CurrentUsers.Clear();
-            });
         }
         public static void CheckHarrowholdMode(ushort zoneId, uint templateId)
         {
@@ -140,115 +136,6 @@ namespace TCC
                 }
             }
             return d;
-        }
-        static void SetDragonsContexts(uint templateId)
-        {
-            //if (templateId == 1100)
-            //{
-            //    WindowManager.BossGauge.Dispatcher.Invoke(() =>
-            //    {
-            //        WindowManager.BossGauge.HHBosses.ignidrax.Reset();
-            //        WindowManager.BossGauge.HHBosses.ignidrax.DataContext = CurrentBosses.FirstOrDefault(x => x.Name == "Ignidrax");
-            //    });
-            //}
-            //if (templateId == 1101)
-            //{
-            //    WindowManager.BossGauge.Dispatcher.Invoke(() =>
-            //    {
-            //        WindowManager.BossGauge.HHBosses.terradrax.Reset();
-            //        WindowManager.BossGauge.HHBosses.terradrax.DataContext = CurrentBosses.FirstOrDefault(x => x.Name == "Terradrax");
-            //    });
-            //}
-            //if (templateId == 1102)
-            //{
-            //    WindowManager.BossGauge.Dispatcher.Invoke(() =>
-            //    {
-            //        WindowManager.BossGauge.HHBosses.umbradrax.Reset();
-            //        WindowManager.BossGauge.HHBosses.umbradrax.DataContext = CurrentBosses.FirstOrDefault(x => x.Name == "Umbradrax");
-            //    });
-            //}
-            //if (templateId == 1103)
-            //{
-            //    WindowManager.BossGauge.Dispatcher.Invoke(() =>
-            //    {
-            //        WindowManager.BossGauge.HHBosses.aquadrax.Reset();
-            //        WindowManager.BossGauge.HHBosses.aquadrax.DataContext = CurrentBosses.FirstOrDefault(x => x.Name == "Aquadrax");
-            //    });
-            //}
-
-        }
-
-
-        public static void SpawnUser(ulong entityId, string name)
-        {
-            App.Current.Dispatcher.Invoke(() =>
-            {
-                CurrentUsers.Add(new Player(entityId, name));
-            });
-        }
-        public static void DespawnUser(ulong entityId)
-        {
-            if (TryGetUserById(entityId, out Player p))
-            {
-                App.Current.Dispatcher.Invoke(() =>
-                {
-                    CurrentUsers.Remove(p);
-                });
-            }
-        }
-
-        static void UnsetDragonsContexts(ulong target)
-        {
-            //WindowManager.BossGauge.Dispatcher.Invoke(() =>
-            //{
-            //    if (WindowManager.BossGauge.HHBosses.ignidrax.EntityId == target)
-            //    {
-            //        WindowManager.BossGauge.HHBosses.ignidrax.DataContext = null;
-            //        WindowManager.BossGauge.HHBosses.ignidrax.ForceEnrageOff();
-            //        WindowManager.BossGauge.HHBosses.abnormalities.DataContext = null;
-            //        WindowManager.BossGauge.HHBosses.abnormalities.ItemsSource = null;
-            //    }
-            //    if (WindowManager.BossGauge.HHBosses.aquadrax.EntityId == target)
-            //    {
-            //        WindowManager.BossGauge.HHBosses.aquadrax.DataContext = null;
-            //        WindowManager.BossGauge.HHBosses.aquadrax.ForceEnrageOff();
-            //        WindowManager.BossGauge.HHBosses.abnormalities.DataContext = null;
-            //        WindowManager.BossGauge.HHBosses.abnormalities.ItemsSource = null;
-
-            //    }
-            //    if (WindowManager.BossGauge.HHBosses.terradrax.EntityId == target)
-            //    {
-            //        WindowManager.BossGauge.HHBosses.terradrax.DataContext = null;
-            //        WindowManager.BossGauge.HHBosses.terradrax.ForceEnrageOff();
-            //        WindowManager.BossGauge.HHBosses.abnormalities.DataContext = null;
-            //        WindowManager.BossGauge.HHBosses.abnormalities.ItemsSource = null;
-
-            //    }
-            //    if (WindowManager.BossGauge.HHBosses.umbradrax.EntityId == target)
-            //    {
-            //        WindowManager.BossGauge.HHBosses.umbradrax.DataContext = null;
-            //        WindowManager.BossGauge.HHBosses.umbradrax.ForceEnrageOff();
-            //        WindowManager.BossGauge.HHBosses.abnormalities.DataContext = null;
-            //        WindowManager.BossGauge.HHBosses.abnormalities.ItemsSource = null;
-
-
-            //    }
-            //});
-        }
-
-        public static bool TryGetUserById(ulong id, out Player p)
-        {
-            p = CurrentUsers.FirstOrDefault(x => x.EntityId == id);
-            if (p == null)
-            {
-                p = new Player();
-                return false;
-            }
-            else
-            {
-                return true;
-            }
-
         }
     }
 }
