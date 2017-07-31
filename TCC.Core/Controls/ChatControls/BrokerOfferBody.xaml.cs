cbd0cec3b48a3a04b3c16d4298594f1e817731d0
foglio <foglio1024@gmail.com>
2017-07-31 12:30:41 +0200

    changed broker offer behaviour

--- TCC.Core/Controls/ChatControls/BrokerOfferBody.xaml.cs
+++ TCC.Core/Controls/ChatControls/BrokerOfferBody.xaml.cs
@@ -28,9 +28,7 @@ namespace TCC.Controls.ChatControls
         private void Accept(object sender, MouseButtonEventArgs e)
         {
             var dc = (BrokerChatMessage)DataContext;
-            if (dc.Handled) return;
             ProxyInterop.SendTradeBrokerAccept(dc.PlayerId, dc.ListingId);
-            OnHandled();
         }
 
         private void Decline(object sender, MouseButtonEventArgs e)
@@ -39,15 +37,14 @@ namespace TCC.Controls.ChatControls
             if (dc.Handled) return;
             ProxyInterop.SendTradeBrokerDecline(dc.PlayerId, dc.ListingId);
             OnHandled();
-
         }
 
         private void OnHandled()
         {
             var dc = (BrokerChatMessage)DataContext;
             dc.Handled = true;
-            declineButton.Visibility = Visibility.Collapsed;
-            acceptButton.Visibility = Visibility.Collapsed;
+            //declineButton.Visibility = Visibility.Collapsed;
+            //acceptButton.Visibility = Visibility.Collapsed;
         }
     }
 }
