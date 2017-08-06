5ca53008874b0cc6c0784143acd1daf2528f26fc
foglio <foglio1024@gmail.com>
2017-08-06 18:57:29 +0200

    load animation at control loading

--- TCC.Core/Controls/DungeonInfoControl.xaml.cs
+++ TCC.Core/Controls/DungeonInfoControl.xaml.cs
@@ -27,6 +27,8 @@ namespace TCC.Controls
         DoubleAnimation moveUp;
         DoubleAnimation scaleDown;
         DoubleAnimation moveDown;
+        DoubleAnimation bubbleScale;
+        DoubleAnimation fadeIn;
 
         public DungeonInfoControl()
         {
@@ -50,16 +52,17 @@ namespace TCC.Controls
             moveUp = new DoubleAnimation(10, growDuration) { EasingFunction = new QuadraticEase() };
             scaleDown = new DoubleAnimation(0, growDuration) { EasingFunction = new QuadraticEase() };
             moveDown = new DoubleAnimation(4, growDuration) { EasingFunction = new QuadraticEase() };
-
+            bubbleScale = new DoubleAnimation(.9, 1, TimeSpan.FromMilliseconds(1000)) { EasingFunction = new ElasticEase() };
+            fadeIn = new DoubleAnimation(0, 1, TimeSpan.FromMilliseconds(200));
         }
         public void AnimateIn()
         {
 
             Dispatcher.Invoke(() =>
             {
-                entriesBubble.RenderTransform.BeginAnimation(ScaleTransform.ScaleXProperty, new DoubleAnimation(.9, 1, TimeSpan.FromMilliseconds(1000)) { EasingFunction = new ElasticEase() });
-                entriesBubble.RenderTransform.BeginAnimation(ScaleTransform.ScaleYProperty, new DoubleAnimation(.9, 1, TimeSpan.FromMilliseconds(1000)) { EasingFunction = new ElasticEase() });
-                entriesBubble.Child.BeginAnimation(OpacityProperty, new DoubleAnimation(0, 1, TimeSpan.FromMilliseconds(200)));
+                entriesBubble.RenderTransform.BeginAnimation(ScaleTransform.ScaleXProperty, bubbleScale);
+                entriesBubble.RenderTransform.BeginAnimation(ScaleTransform.ScaleYProperty, bubbleScale);
+                entriesBubble.Child.BeginAnimation(OpacityProperty, fadeIn);
             });
 
         }
