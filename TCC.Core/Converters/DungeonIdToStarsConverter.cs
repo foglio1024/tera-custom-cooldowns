f9691b9aba1a78a6b63e8707124e7e0f10e3e089
foglio <foglio1024@gmail.com>
2017-08-06 14:48:05 +0200

    added new converters

---
+++ TCC.Core/Converters/DungeonIdToStarsConverter.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using TCC.Data.Databases;
+
+namespace TCC.Converters
+{
+    class DungeonIdToStarsConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var id = (uint)value;
+            var t =  DungeonDatabase.Instance.DungeonDefinitions[id].Tier;
+            switch (t)
+            {
+                case DungeonTier.Tier2:
+                    return "2";
+                case DungeonTier.Tier3:
+                    return "3";
+                case DungeonTier.Tier4:
+                    return "4";
+                case DungeonTier.Tier5:
+                    return "5";
+                default:
+                    return "-";
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ TCC.Core/Converters/ValueToFactorConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace TCC.Converters
+{
+    class ValueToFactorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var val = System.Convert.ToDouble(value);
+            var max = System.Convert.ToDouble(parameter);
+
+            if (max != 0) return val / max;
+            else return 0;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
