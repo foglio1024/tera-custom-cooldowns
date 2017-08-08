789faffaa6d2143cd9381ccbd4e31c52c8f79d16
foglio <foglio1024@gmail.com>
2017-08-08 09:25:00 +0200

    resharper

--- TCC.Core/TimeManager.cs
+++ TCC.Core/TimeManager.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Threading;
 using TCC.ViewModels;
 
@@ -10,90 +7,109 @@ namespace TCC
 {
     public class TimeManager : TSPropertyChanged
     {
-        const string EU_TIMEZONE = "Central Europe Standard Time";
-        const string NA_TIMEZONE = "Central Standard Time";
-        const string RU_TIMEZONE = "Russian Standard Time";
-        const string TW_TIMEZONE = "China Standard Time";
-        const string JP_TIMEZONE = "Japan Standard Time";
-        const string KR_TIMEZONE = "Korea Standard Time";
+        private const string EuTimezone = "Central Europe Standard Time";
+        private const string NaTimezone = "Central Standard Time";
+        private const string RuTimezone = "Russian Standard Time";
+        private const string TwTimezone = "China Standard Time";
+        private const string JpTimezone = "Japan Standard Time";
+        private const string KrTimezone = "Korea Standard Time";
 
-        const int EU_RESET_HOUR = 6;
-        const int NA_RESET_HOUR = 6;
-        const int RU_RESET_HOUR = 6;
-        const int TW_RESET_HOUR = 6;
-        const int JP_RESET_HOUR = 6;
-        const int KR_RESET_HOUR = 6;
+        private const int EuResetHour = 6;
+        private const int NaResetHour = 6;
+        private const int RuResetHour = 6;
+        private const int TwResetHour = 6;
+        private const int JpResetHour = 6;
+        private const int KrResetHour = 6;
 
-        const DayOfWeek EU_RESET_DAY = DayOfWeek.Wednesday;
-        const DayOfWeek NA_RESET_DAY = DayOfWeek.Wednesday;
-        const DayOfWeek RU_RESET_DAY = DayOfWeek.Wednesday;
-        const DayOfWeek TW_RESET_DAY = DayOfWeek.Wednesday;
-        const DayOfWeek JP_RESET_DAY = DayOfWeek.Wednesday;
-        const DayOfWeek KR_RESET_DAY = DayOfWeek.Wednesday;
+        private const DayOfWeek EuResetDay = DayOfWeek.Wednesday;
+        private const DayOfWeek NaResetDay = DayOfWeek.Wednesday;
+        private const DayOfWeek RuResetDay = DayOfWeek.Wednesday;
+        private const DayOfWeek TwResetDay = DayOfWeek.Wednesday;
+        private const DayOfWeek JpResetDay = DayOfWeek.Wednesday;
+        private const DayOfWeek KrResetDay = DayOfWeek.Wednesday;
 
-        static TimeManager _instance;
-        DayOfWeek resetDay;
-        int resetHour;
-        int serverHourOffset;
+        public const double SecondsInDay = 60 * 60 * 24;
+
+        private static TimeManager _instance;
+        private DayOfWeek _resetDay;
+        public int ResetHour;
         public static TimeManager Instance => _instance ?? (_instance = new TimeManager());
-
+        private readonly DispatcherTimer _t;
+        private string _currentRegion;
+        public int ServerHourOffset { get; set; }
 
         public TimeManager()
         {
             _dispatcher = Dispatcher.CurrentDispatcher;
+            _t = new DispatcherTimer();
+            _t.Interval = TimeSpan.FromMilliseconds(1000);
+            _t.Tick += CheckNewDay;
         }
+
+        private void CheckNewDay(object sender, EventArgs e)
+        {
+            if(DateTime.Now.Hour == 0 && DateTime.Now.Minute == 0)
+            {
+                InfoWindowViewModel.Instance.LoadEvents(DateTime.Now.DayOfWeek, _currentRegion);
+            }
+        }
+
         public void SetServerTimeZone(string region)
         {
+            _currentRegion = region;
             TimeZoneInfo timezone = TimeZoneInfo.Local;
             if (region.StartsWith("EU"))
             {
-                timezone = TimeZoneInfo.GetSystemTimeZones().FirstOrDefault(x => x.Id == EU_TIMEZONE);
-                resetHour = EU_RESET_HOUR;
-                resetDay = EU_RESET_DAY;
+                timezone = TimeZoneInfo.GetSystemTimeZones().FirstOrDefault(x => x.Id == EuTimezone);
+                ResetHour = EuResetHour;
+                _resetDay = EuResetDay;
             }
-            else if (region == "NA")
+            else switch (region)
             {
-                timezone = TimeZoneInfo.GetSystemTimeZones().FirstOrDefault(x => x.Id == NA_TIMEZONE);
-                resetHour = NA_RESET_HOUR;
-                resetDay = NA_RESET_DAY;
-
-            }
-            else if (region == "RU")
-            {
-                timezone = TimeZoneInfo.GetSystemTimeZones().FirstOrDefault(x => x.Id == RU_TIMEZONE);
-                resetHour = RU_RESET_HOUR;
-                resetDay = RU_RESET_DAY;
-
-            }
-            else if (region == "TW")
-            {
-                timezone = TimeZoneInfo.GetSystemTimeZones().FirstOrDefault(x => x.Id == TW_TIMEZONE);
-                resetHour = TW_RESET_HOUR;
-                resetDay = TW_RESET_DAY;
-
-            }
-            else if (region == "JP")
-            {
-                timezone = TimeZoneInfo.GetSystemTimeZones().FirstOrDefault(x => x.Id == JP_TIMEZONE);
-                resetHour = JP_RESET_HOUR;
-                resetDay = JP_RESET_DAY;
-
-            }
-            else if (region == "KR")
-            {
-                timezone = TimeZoneInfo.GetSystemTimeZones().FirstOrDefault(x => x.Id == KR_TIMEZONE);
-                resetHour = KR_RESET_HOUR;
-                resetDay = KR_RESET_DAY;
-
+                case "NA":
+                    timezone = TimeZoneInfo.GetSystemTimeZones().FirstOrDefault(x => x.Id == NaTimezone);
+                    ResetHour = NaResetHour;
+                    _resetDay = NaResetDay;
+                    break;
+                case "RU":
+                    timezone = TimeZoneInfo.GetSystemTimeZones().FirstOrDefault(x => x.Id == RuTimezone);
+                    ResetHour = RuResetHour;
+                    _resetDay = RuResetDay;
+                    break;
+                case "TW":
+                    timezone = TimeZoneInfo.GetSystemTimeZones().FirstOrDefault(x => x.Id == TwTimezone);
+                    ResetHour = TwResetHour;
+                    _resetDay = TwResetDay;
+                    break;
+                case "JP":
+                    timezone = TimeZoneInfo.GetSystemTimeZones().FirstOrDefault(x => x.Id == JpTimezone);
+                    ResetHour = JpResetHour;
+                    _resetDay = JpResetDay;
+                    break;
+                case "KR":
+                    timezone = TimeZoneInfo.GetSystemTimeZones().FirstOrDefault(x => x.Id == KrTimezone);
+                    ResetHour = KrResetHour;
+                    _resetDay = KrResetDay;
+                    break;
             }
             var serverUtcOffset = timezone.IsDaylightSavingTime(DateTime.UtcNow + timezone.BaseUtcOffset) ? 
                 timezone.BaseUtcOffset.Hours + 1 : 
                 timezone.BaseUtcOffset.Hours;
-            serverHourOffset = TimeZone.CurrentTimeZone.GetUtcOffset(DateTime.Now).Hours - serverUtcOffset;
+            ServerHourOffset = TimeZone.CurrentTimeZone.GetUtcOffset(DateTime.Now).Hours - serverUtcOffset;
+            if (ServerHourOffset != 0)
+            {
+                if (InfoWindowViewModel.Instance.Markers.FirstOrDefault(x => x.Name.Equals(region + " server time")) != null) return;
+                InfoWindowViewModel.Instance.Markers.Add(new TimeMarker(DateTime.Now.AddHours(-ServerHourOffset), region + " server time", "fff5c6"));
+            }
+            var sg = new EventGroup("Special events");
+            sg.AddEvent(new DailyEvent("Reset", TimeManager.Instance.ResetHour, 0, "ff0000"));
+            InfoWindowViewModel.Instance.AddEventGroup(sg);
+            InfoWindowViewModel.Instance.LoadEvents(DateTime.Now.DayOfWeek, region);
+
         }
         public bool CheckReset()
         {
-            if (SettingsManager.LastRun.Hour < resetHour + serverHourOffset && DateTime.Now.Hour > resetHour + serverHourOffset)
+            if (SettingsManager.LastRun.Hour < ResetHour + ServerHourOffset && DateTime.Now.Hour > ResetHour + ServerHourOffset)
             {
                 foreach (var ch in InfoWindowViewModel.Instance.Characters)
                 {
@@ -108,12 +124,13 @@ namespace TCC
                     }
                     ch.DailiesDone = 0;
 
-                    if(DateTime.Now.DayOfWeek == resetDay)
+                    if(DateTime.Now.DayOfWeek == _resetDay)
                     {
                         ch.WeekliesDone = 0;
                     }
                 }
                 SettingsManager.LastRun = DateTime.Now;
+                InfoWindowViewModel.Instance.SaveToFile();
                 SettingsManager.SaveSettings();
                 return true;
             }
