31b911cf695acbe56778d6d301d0b62f6eea10ae
foglio <foglio1024@gmail.com>
2017-08-17 16:10:00 +0200

    added arc animations and numeric indicator for ninja, gunner, brawler

--- TCC.Core/Controls/ClassBars/GunnerBar.xaml.cs
+++ TCC.Core/Controls/ClassBars/GunnerBar.xaml.cs
@@ -9,9 +9,11 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using TCC.ViewModels;
 
 namespace TCC.Controls
 {
@@ -23,6 +25,25 @@ namespace TCC.Controls
         public GunnerBar()
         {
             InitializeComponent();
+        }
+
+        private GunnerBarManager _dc;
+        private DoubleAnimation _an;
+        private void GunnerBar_OnLoaded(object sender, RoutedEventArgs e)
+        {
+            _dc = (GunnerBarManager) DataContext;
+            _an = new DoubleAnimation(_dc.ST.Factor * 359.99, TimeSpan.FromMilliseconds(150));
+            _dc.ST.PropertyChanged += ST_PropertyChanged;
+
+        }
+
+        private void ST_PropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
+        {
+            if (e.PropertyName == nameof(_dc.ST.Factor))
+            {
+                _an.To = _dc.ST.Factor*359.99;
+                MainReArc.BeginAnimation(Arc.EndAngleProperty, _an);
+            }
         }
     }
 }
--- TCC.Core/Controls/ClassBars/NinjaBar.xaml.cs
+++ TCC.Core/Controls/ClassBars/NinjaBar.xaml.cs
@@ -9,9 +9,11 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using TCC.ViewModels;
 
 namespace TCC.Controls
 {
@@ -24,5 +26,24 @@ namespace TCC.Controls
         {
             InitializeComponent();
         }
+        private NinjaBarManager _dc;
+        private DoubleAnimation _an;
+        private void NinjaBar_OnLoaded(object sender, RoutedEventArgs e)
+        {
+            _dc = (NinjaBarManager)DataContext;
+            _an = new DoubleAnimation(_dc.ST.Factor * 359.99, TimeSpan.FromMilliseconds(150));
+
+            _dc.ST.PropertyChanged += ST_PropertyChanged;
+        }
+
+        private void ST_PropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
+        {
+            if (e.PropertyName == nameof(_dc.ST.Factor))
+            {
+                _an.To = _dc.ST.Factor * 359.99;
+                MainReArc.BeginAnimation(Arc.EndAngleProperty, _an);
+            }
+        }
+
     }
 }
