6ada38ab5b2e3b254234eb2805b8ff19081494a8
foglio <foglio1024@gmail.com>
2017-08-17 16:09:17 +0200

    added time period support for event groups

--- TCC.Core/Data/EventGroup.cs
+++ TCC.Core/Data/EventGroup.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows.Threading;
+﻿using System;
+using System.Security.Cryptography;
+using System.Windows.Threading;
 
 namespace TCC.ViewModels
 {
@@ -7,12 +9,27 @@ namespace TCC.ViewModels
         public SynchronizedObservableCollection<DailyEvent> Events { get; }
         public string Name { get; }
         public bool RemoteCheck { get; }
-        public EventGroup(string name)
+        private DateTime _start;
+        private DateTime _end;
+        public string TimeSpanTooltip
+        {
+            get
+            {
+                if(_start == DateTime.MinValue && _end == DateTime.MaxValue) return $"{Name} (permanent)";
+                var startTime = _start.Hour == 0 &&_start.Minute == 0? _start.ToShortDateString() : _start.ToString();
+                var endTime = _end.Hour == 0 && _end.Minute == 0 ? _end.ToShortDateString() : $"{_end}";
+                return $"{Name}: from {startTime} to {endTime}";
+            }
+        }
+
+        public EventGroup(string name, DateTime start, DateTime end, bool rc)
         {
             _dispatcher = Dispatcher.CurrentDispatcher;
             Events = new SynchronizedObservableCollection<DailyEvent>(_dispatcher);
             Name = name;
-            RemoteCheck = Name == "gbam";
+            RemoteCheck = rc;
+            _start = start;
+            _end = end;
         }
         public void AddEvent(DailyEvent ev)
         {
