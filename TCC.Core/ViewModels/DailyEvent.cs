19147f155db3dd3b92843ca3bba11fdd3c2992e1
foglio <foglio1024@gmail.com>
2017-08-21 15:19:51 +0200

    moved types to own files

---
+++ TCC.Core/ViewModels/DailyEvent.cs
@@ -0,0 +1,68 @@
+using System;
+using System.Windows.Threading;
+
+namespace TCC.ViewModels
+{
+    public class DailyEvent : TSPropertyChanged
+    {
+        private DateTime Start { get; set; }
+        private TimeSpan Duration { get; set; }
+        private readonly TimeSpan _realDuration;
+        public double StartFactor => 60 * (Start.Hour * 60 + Start.Minute) / TimeManager.SecondsInDay;
+        public double DurationFactor => Duration.TotalSeconds / TimeManager.SecondsInDay;
+        private bool _happened = false;
+        public bool IsClose
+        {
+            get
+            {
+                var ts = Start - TimeManager.Instance.CurrentServerTime;
+                return ts.TotalMinutes > 0 && ts.TotalMinutes <= 15;
+            }
+        }
+        public string Name { get; set; }
+        public string ToolTip
+        {
+            get
+            {
+                var d = Duration > TimeSpan.FromHours(0) ? " to " + Start.Add(_realDuration).ToShortTimeString() : "";
+                return Name + " " + Start.ToShortTimeString() + d;
+            }
+        }
+        public string Color { get; }
+        public DailyEvent(string name, double startHour, double durationOrEndHour, string color = "30afff", bool isDuration = true)
+        {
+            _dispatcher = Dispatcher.CurrentDispatcher;
+            Start = DateTime.Parse(startHour + ":00");
+            var d = isDuration ? durationOrEndHour : durationOrEndHour - startHour;
+            Duration = TimeSpan.FromHours(d);
+            _realDuration = Duration;
+            var dayend = DateTime.Parse("00:00").AddDays(1);
+            if (Start.Add(Duration) > dayend)
+            {
+                Duration = dayend - Start;
+            }
+            Name = name;
+            Color = color;
+        }
+
+        public void UpdateFromServer(bool force = false)
+        {
+            if (TimeManager.Instance.CurrentServerTime >= Start && !_happened)
+            {
+                var time = force
+                    ? TimeManager.Instance.CurrentServerTime
+                    : TimeManager.Instance.RetrieveGuildBamDateTime().AddHours(TimeManager.Instance.ServerHourOffsetFromUtc);
+
+                if (time >= Start)
+                {
+                    Start = time;
+                    Duration = TimeSpan.Zero;
+                    _happened = true;
+                    NotifyPropertyChanged(nameof(StartFactor));
+                    NotifyPropertyChanged(nameof(DurationFactor));
+                    NotifyPropertyChanged(nameof(ToolTip));
+                }
+            }
+        }
+    }
+}
