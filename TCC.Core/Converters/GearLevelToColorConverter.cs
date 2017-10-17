60cdc71e4fbed46c124db24a6d6a50a9e9e8a2d9
foglio <foglio1024@gmail.com>
2017-10-17 15:58:13 +0200

    added new converters for gear

---
+++ TCC.Core/Converters/GearLevelToColorConverter.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using TCC.Data;
+
+namespace TCC.Converters
+{
+    class GearLevelToColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var val = (GearTier) value;
+            switch (val)
+            {
+                case GearTier.Low:
+                    return App.Current.FindResource("Colors.DungeonTier.2");
+                case GearTier.Mid:
+                    return App.Current.FindResource("Colors.DungeonTier.3");
+                case GearTier.High:
+                    return App.Current.FindResource("Colors.DungeonTier.4");
+                case GearTier.Top:
+                    return App.Current.FindResource("Colors.DungeonTier.5");
+                default:
+                    return App.Current.FindResource("Colors.DungeonTier.Solo");
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ TCC.Core/Converters/PieceToPathConverter.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace TCC.Converters
+{
+    class PieceToPathConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var p = (GearPiece) value;
+            switch (p)
+            {
+                case GearPiece.Weapon:
+                    return App.Current.FindResource("SVG.Weapon");
+                case GearPiece.Armor:
+                    return App.Current.FindResource("SVG.Armor");
+                case GearPiece.Hands:
+                    return App.Current.FindResource("SVG.Hands");
+                case GearPiece.Feet:
+                    return App.Current.FindResource("SVG.Feet");
+                case GearPiece.CritNecklace:
+                    return App.Current.FindResource("SVG.Necklace");
+                case GearPiece.CritEarring:
+                    return App.Current.FindResource("SVG.Earring");
+                case GearPiece.CritRing:
+                    return App.Current.FindResource("SVG.Ring");
+                case GearPiece.PowerNecklace:
+                    return App.Current.FindResource("SVG.Necklace");
+                case GearPiece.PowerEarring:
+                    return App.Current.FindResource("SVG.Earring");
+                case GearPiece.PowerRing:
+                    return App.Current.FindResource("SVG.Ring");
+                case GearPiece.Circlet:
+                    return App.Current.FindResource("SVG.Circlet");
+                case GearPiece.Belt:
+                    return App.Current.FindResource("SVG.Belt");
+            }
+            return App.Current.FindResource("SVG.Close");
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
