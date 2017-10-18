a7c4a361631c154333272a116cc1ad5a2131e524
foglio <foglio1024@gmail.com>
2017-10-18 19:11:55 +0200

    added IsOpen parsing to avoid using sInven when not actually open

--- TCC.Core/Parsing/Messages/S_INVEN.cs
+++ TCC.Core/Parsing/Messages/S_INVEN.cs
@@ -16,13 +16,16 @@ namespace TCC.Parsing.Messages
         public static List<Tuple<uint, int, uint>> Items { get; private set; }
         public bool More { get; private set; }
         public bool First { get; private set; }
+        public bool IsOpen { get; private set; }
         public S_INVEN(TeraMessageReader reader) : base(reader)
         {
             //TODO
             if(BuffBarWindowViewModel.Instance.Player.InfBuffs.Any(x => AbnormalityDatabase.NoctIds.Contains(x.Abnormality.Id))) return;
             var count = reader.ReadUInt16();
             var invOffset = reader.ReadUInt16();
-            reader.Skip(8+8+1);
+            reader.Skip(8+8);
+            IsOpen = reader.ReadBoolean();
+            if(!IsOpen) return;
             First = reader.ReadBoolean();
             if(First) Items = new List<Tuple<uint, int, uint>>();
             More = reader.ReadBoolean();
@@ -37,12 +40,19 @@ namespace TCC.Parsing.Messages
                     var next = reader.ReadUInt16();
                     reader.Skip(6);
                     var itemId = reader.ReadUInt32();
-                    reader.Skip(8 +8+ 4+4+4);
+                    reader.Skip(8 + 8);
+                    var slot = reader.ReadUInt32();
+                    if (slot > 39)
+                    {
+                        reader.BaseStream.Position = next - 4;
+                        continue;
+                    }
+                    reader.Skip(4+4);
                     var enchant = reader.ReadInt32();
                     reader.Skip(4+1+4+4+4+4+4+4+4+4+4+4+4+4+8+1+4+4+4+4+4+4+1+4+4+4+4+1+1);
                     var exp = reader.ReadUInt32();
                     Items.Add(new Tuple<uint, int, uint>(itemId, enchant, exp));
-                    Console.WriteLine(itemId + " " + enchant);
+                    Console.WriteLine(itemId + " " + enchant + " ["+slot+"]");
                     reader.BaseStream.Position = next -4;
                 }
                 
