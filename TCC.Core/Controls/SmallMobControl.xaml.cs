b2a4f3c2608efbe5f88d7365cd636c5ae1e6bc29
foglio <foglio1024@gmail.com>
2017-10-26 20:59:20 +0200

    fixed crash on some mob deletion

--- TCC.Core/Controls/SmallMobControl.xaml.cs
+++ TCC.Core/Controls/SmallMobControl.xaml.cs
@@ -1,5 +1,9 @@
-﻿using System.Windows;
+﻿using System;
+using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Threading;
 using TCC.Data;
 using TCC.ViewModels;
 
@@ -10,6 +14,7 @@ namespace TCC.Controls
     /// </summary>
     public partial class SmallMobControl : UserControl
     {
+        private DispatcherTimer t;
         public SmallMobControl()
         {
             InitializeComponent();
@@ -19,8 +24,20 @@ namespace TCC.Controls
         {
             var dc = (Boss) DataContext;
             dc.DeleteEvent += Dc_DeleteEvent;
+            t = new DispatcherTimer {Interval = TimeSpan.FromSeconds(4700)};
+            t.Tick += (s,ev) => RootGrid.LayoutTransform.BeginAnimation(ScaleTransform.ScaleYProperty, new DoubleAnimation(1,0,TimeSpan.FromMilliseconds(200)));
+            RootGrid.LayoutTransform.BeginAnimation(ScaleTransform.ScaleYProperty, new DoubleAnimation(0,1,TimeSpan.FromMilliseconds(200)));
         }
 
-        private void Dc_DeleteEvent() => Dispatcher.Invoke(() => BossGageWindowViewModel.Instance.RemoveMe((Boss)DataContext));
+        private void Dc_DeleteEvent() => Dispatcher.Invoke(() =>
+        {
+            t.Start();
+            try
+            {
+                BossGageWindowViewModel.Instance.RemoveMe((Boss) DataContext);
+            }
+            catch { }
+        });
+
     }
 }
