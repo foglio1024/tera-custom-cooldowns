5abb051905f481472df4b8fcf4786eb0a86e584b
foglio <foglio1024@gmail.com>
2017-10-31 12:42:33 +0100

    added glow effect on mouse over

--- TCC.Core/Controls/ChatControls/ChatMessageHeader.xaml.cs
+++ TCC.Core/Controls/ChatControls/ChatMessageHeader.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System.Windows.Controls;
 using System.Windows.Input;
+using System.Windows.Media.Effects;
 using TCC.Data;
 using TCC.Parsing;
 
@@ -20,12 +21,28 @@ namespace TCC.Controls
             var context = (ChatMessage)DataContext;
             if (context.Author == "System" || context.Channel == ChatChannel.Twitch) return;
             WindowManager.ChatWindow.CurrentSender = sender;
-            ProxyInterop.SendAskInteractiveMessage(PacketProcessor.ServerId, context.Author);
+            Proxy.AskInteractive(PacketProcessor.ServerId, context.Author);
         }
 
         private void UserControl_MouseEnter(object sender, MouseEventArgs e)
         {
             WindowManager.ChatWindow.CloseTooltip();
+        }
+
+        private void UIElement_OnMouseEnter(object sender, MouseEventArgs e)
+        {
+            var s = sender as ContentControl;
+            var eff = s.Effect as DropShadowEffect;
+            eff.Opacity = .7;
+
+        }
+
+        private void UIElement_OnMouseLeave(object sender, MouseEventArgs e)
+        {
+            var s = sender as ContentControl;
+            var eff = s.Effect as DropShadowEffect;
+            eff.Opacity = 0;
+
         }
     }
 }
