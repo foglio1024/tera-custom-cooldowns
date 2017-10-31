6ce83ada31daa2dc11db8dd765dc30a5e37db9f2
foglio <foglio1024@gmail.com>
2017-10-31 12:38:36 +0100

    added more fields to parse

--- TCC.Core/Parsing/Messages/S_DESPAWN_NPC.cs
+++ TCC.Core/Parsing/Messages/S_DESPAWN_NPC.cs
@@ -6,10 +6,13 @@ namespace TCC.Parsing.Messages
     public class S_DESPAWN_NPC : ParsedMessage
     {
         public ulong Target { get; private set; }
+        public DespawnType Type { get; private set; }
 
         public S_DESPAWN_NPC(TeraMessageReader reader) : base(reader)
         {
             Target = reader.ReadUInt64();
+            reader.Skip(3*4);
+            Type = (DespawnType) reader.ReadUInt32();
         }
     }
 }
--- TCC.Core/Parsing/Messages/S_GUILD_TOWER_INFO.cs
+++ TCC.Core/Parsing/Messages/S_GUILD_TOWER_INFO.cs
@@ -7,12 +7,15 @@ namespace TCC.Parsing.Messages
     {
         public ulong TowerId { get; set; }
         public string GuildName { get; set; }
-
+        public uint GuildId { get; set; }
+        public string LogoInfo { get; set; }
         public S_GUILD_TOWER_INFO(TeraMessageReader reader) : base(reader)
         {
             reader.Skip(2);
             var nameOffset = reader.ReadUInt16();
             TowerId = reader.ReadUInt64();
+            GuildId = reader.ReadUInt32();
+            LogoInfo = reader.ReadTeraString();
             reader.BaseStream.Position = nameOffset - 4;
             GuildName = reader.ReadTeraString();
         }
