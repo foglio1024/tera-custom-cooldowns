dbe940865f986f714c08ae25b0ce06cf51132d21
foglio <foglio1024@gmail.com>
2017-10-31 12:33:03 +0100

    added converters for guild logos

---
+++ TCC.Core/Converters/ClassToFillConverter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace TCC.Converters
+{
+    public class ClassToFillConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var c = (Class)value;
+            switch (c)
+            {
+                case Class.Lancer:
+                    return App.Current.FindResource("Colors.GroupWindow.Tank");
+                case Class.Fighter:
+                    return App.Current.FindResource("Colors.GroupWindow.Tank");
+                case Class.Priest:
+                    return App.Current.FindResource("Colors.GroupWindow.Healer");
+                case Class.Elementalist:
+                    return App.Current.FindResource("Colors.GroupWindow.Healer");
+                default:
+                    return App.Current.FindResource("Colors.GroupWindow.Dps");
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ TCC.Core/Converters/LogoInfoToImageConverter.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Media.Imaging;
+using TCC.Parsing.Messages;
+
+namespace TCC.Converters
+{
+    class LogoInfoToImageConverter : IValueConverter
+    {
+        [DllImport("gdi32")]
+        static extern int DeleteObject(IntPtr o);
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var guildId = (uint) value;
+            if (!S_IMAGE_DATA.Database.ContainsKey(guildId))
+            {
+                return App.Current.FindResource("DefaultGuildLogo");
+
+            }
+
+            IntPtr ip = S_IMAGE_DATA.Database[guildId].GetHbitmap();
+            BitmapSource bs = null;
+            try
+            {
+                bs = System.Windows.Interop.Imaging.CreateBitmapSourceFromHBitmap(ip,
+                    IntPtr.Zero, Int32Rect.Empty,
+                    System.Windows.Media.Imaging.BitmapSizeOptions.FromEmptyOptions());
+            }
+            finally
+            {
+                DeleteObject(ip);
+            }
+
+            return bs;
+
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
