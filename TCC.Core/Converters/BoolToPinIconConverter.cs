4baa1eda0feebc351b4333f3793224fcfde28b5e
Foglio1024 <foglio1024@gmail.com>
2018-04-08 19:29:20 +0200

    converter for custom toggle

---
+++ TCC.Core/Converters/BoolToPinIconConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace TCC.Converters
+{
+    class BoolToSvgSwitchConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var p = new string[2];
+            if (parameter != null) p = (parameter as string).Split(':');
+            if ((bool)value) return App.Current.FindResource(p[0]) as Geometry;
+            else return App.Current.FindResource(p[1]) as Geometry;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
