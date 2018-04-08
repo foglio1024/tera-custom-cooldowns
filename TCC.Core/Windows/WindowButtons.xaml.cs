eab60005e6c48dcfd6a5e52db705f9331d2a5904
Foglio1024 <foglio1024@gmail.com>
2018-04-08 19:32:57 +0200

    added window buttons control

---
+++ TCC.Core/Windows/WindowButtons.xaml.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TCC.Windows
+{
+    /// <summary>
+    /// Logica di interazione per WindowButtons.xaml
+    /// </summary>
+    public partial class WindowButtons : UserControl, INotifyPropertyChanged
+    {
+        WindowSettings _dc;
+        public WindowButtons()
+        {
+            InitializeComponent();
+        }
+
+
+        public string WindowName
+        {
+            get { return (string)GetValue(WindowNameProperty); }
+            set { SetValue(WindowNameProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for WindowName.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty WindowNameProperty =
+            DependencyProperty.Register("WindowName", typeof(string), typeof(WindowButtons));
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void Hide(object sender, RoutedEventArgs e)
+        {
+            _dc.Visible = false;
+        }
+
+        private void Pin(object sender, RoutedEventArgs e)
+        {
+            _dc.ShowAlways = !_dc.ShowAlways;
+            //PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(_dc.ShowAlways)));
+        }
+
+
+        private void Close(object sender, RoutedEventArgs e)
+        {
+            _dc.Enabled = false;
+        }
+
+        private void UserControl_Loaded_1(object sender, RoutedEventArgs e)
+        {
+            _dc = DataContext as WindowSettings;
+        }
+
+        private void UserControl_DataContextChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            if (DataContext == null) {Console.WriteLine($"Data context is null"); return; }
+            var t = DataContext.GetType().Name;
+            Console.WriteLine($"Data context is {t}");
+        }
+
+        private void AutoDim(object sender, RoutedEventArgs e)
+        {
+            _dc.AutoDim = !_dc.AutoDim;
+        }
+    }
+}
