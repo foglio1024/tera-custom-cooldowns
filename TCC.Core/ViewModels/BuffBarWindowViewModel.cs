566738c8d8590343e7e0007863f7533606f8c64e
Foglio1024 <foglio1024@gmail.com>
2018-04-08 19:31:19 +0200

    moved player init to view model ctor

--- TCC.Core/ViewModels/BuffBarWindowViewModel.cs
+++ TCC.Core/ViewModels/BuffBarWindowViewModel.cs
@@ -15,6 +15,7 @@ namespace TCC.ViewModels
         {
             _dispatcher = Dispatcher.CurrentDispatcher;
             _scale = SettingsManager.BuffWindowSettings.Scale;
+            Player = new Player();
             WindowManager.TccVisibilityChanged += (s, ev) =>
             {
                 //RaisePropertyChanged("IsTeraOnTop");
