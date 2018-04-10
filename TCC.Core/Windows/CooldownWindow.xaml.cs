3a9fe3011f3ed701014bb1165a8c1882aa08d2ce
Foglio1024 <foglio1024@gmail.com>
2018-04-10 18:32:18 +0200

    fixed event handlers

--- TCC.Core/Windows/CooldownWindow.xaml.cs
+++ TCC.Core/Windows/CooldownWindow.xaml.cs
@@ -10,32 +10,14 @@ namespace TCC
 {
     public partial class CooldownWindow : TccWindow
     {
-        DispatcherTimer _t;
         public CooldownWindow()
         {
             InitializeComponent();
-            _t = new DispatcherTimer();
-            _t.Interval = TimeSpan.FromMilliseconds(1000);
-            _t.Tick += _t_Tick;
+            _b = buttons;
             InitWindow(SettingsManager.CooldownWindowSettings, ignoreSize: true);
 
         }
 
-        private void _t_Tick(object sender, EventArgs e)
-        {
-            if (buttons.IsMouseOver) return;
-            _t.Stop();
-            //buttons.Visibility = Visibility.Collapsed;
-            buttons.BeginAnimation(OpacityProperty, new DoubleAnimation(0, TimeSpan.FromMilliseconds(1000)));
-        }
-
-        private void Window_Loaded(object sender, RoutedEventArgs e)
-        {
-
-            //SwitchMode();
-
-            //((FrameworkElement)controlContainer.Content).DataContext = CooldownWindowViewModel.Instance;
-        }
 
         public void SwitchMode()
         {
@@ -53,20 +35,6 @@ namespace TCC
                 ((FrameworkElement)controlContainer.Content).DataContext = CooldownWindowViewModel.Instance;
 
             }, DispatcherPriority.Normal);
-        }
-
-        private void Grid_MouseEnter(object sender, System.Windows.Input.MouseEventArgs e)
-        {
-            //buttons.Visibility = Visibility.Visible;
-            _t.Stop();
-            buttons.BeginAnimation(OpacityProperty, new DoubleAnimation(1, TimeSpan.FromMilliseconds(250)));
-
-        }
-
-        private void Grid_MouseLeave(object sender, System.Windows.Input.MouseEventArgs e)
-        {
-            //buttons.Visibility = Visibility.Collapsed;
-            _t.Start();
         }
     }
 }
