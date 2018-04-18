ad7e33ee4294217e4500f02385f3e5962f455e75
Foglio1024 <foglio1024@gmail.com>
2018-04-18 13:32:03 +0200

    first vanguard info control rework

--- TCC.Core/Controls/VanguardInfoControl.xaml.cs
+++ TCC.Core/Controls/VanguardInfoControl.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
@@ -29,15 +30,24 @@ namespace TCC.Controls
         }
 
         private void UserControl_MouseEnter(object sender, MouseEventArgs e)
-        {
+         {
             rootBorder.RenderTransform.BeginAnimation(TranslateTransform.YProperty, scaleUp);
             rootBorder.Effect.BeginAnimation(DropShadowEffect.BlurRadiusProperty, moveUp);
         }
 
         private void UserControl_MouseLeave(object sender, MouseEventArgs e)
         {
-            rootBorder.RenderTransform.BeginAnimation(TranslateTransform.YProperty, scaleDown);
-            rootBorder.Effect.BeginAnimation(DropShadowEffect.BlurRadiusProperty, moveDown);
+            if (_animDown)
+            {
+
+                rootBorder.RenderTransform.BeginAnimation(TranslateTransform.YProperty, scaleDown);
+                rootBorder.Effect.BeginAnimation(DropShadowEffect.BlurRadiusProperty, moveDown);
+            }
+            else
+            {
+                rootBorder.RenderTransform = new TranslateTransform(0, 0);
+                (rootBorder.Effect as DropShadowEffect).BlurRadius = 3;
+            }
 
         }
 
@@ -61,17 +71,21 @@ namespace TCC.Controls
             InfoWindowViewModel.Instance.Characters.Remove((Character) DataContext);
         }
 
-
+        bool _animDown = true;
         private void UserControl_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
+            _animDown = false;
             var scaleTrans = (ripple.RenderTransform as TransformGroup).Children[0];
             (ripple.RenderTransform as TransformGroup).Children[1] = new TranslateTransform(e.MouseDevice.GetPosition(this).X - ripple.Width / 2, e.MouseDevice.GetPosition(this).Y - ripple.Height / 2);
 
-            scaleTrans.BeginAnimation(ScaleTransform.ScaleXProperty, scaleRipple);
-            scaleTrans.BeginAnimation(ScaleTransform.ScaleYProperty, scaleRipple);
-            ripple.BeginAnimation(OpacityProperty, fadeRipple);
+            //scaleTrans.BeginAnimation(ScaleTransform.ScaleXProperty, scaleRipple);
+            //scaleTrans.BeginAnimation(ScaleTransform.ScaleYProperty, scaleRipple);
+            //ripple.BeginAnimation(OpacityProperty, fadeRipple);
+            Point relativePoint = rootBorder.TransformToAncestor(WindowManager.InfoWindow)
+                              .Transform(new Point(0, 0));
+            WindowManager.InfoWindow.ExpandCharacter(relativePoint, rootBorder.ActualWidth, this.ActualHeight);
             InfoWindowViewModel.Instance.SelectCharacter((Character)DataContext);
-
+            Task.Delay(300).ContinueWith(t => _animDown = true);
         }
         
         private void RootBorder_OnPreviewMouseRightButtonDown(object sender, MouseButtonEventArgs e)
