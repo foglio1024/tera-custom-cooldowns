e31c9b917d726ca8d48e05a67068343761e25842
Foglio1024 <foglio1024@gmail.com>
2018-04-23 13:29:45 +0200

    moved tab type to own file

---
+++ TCC.Core/ViewModels/Tab.cs
@@ -0,0 +1,126 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Windows.Data;
+using System.Windows.Threading;
+using TCC.Data;
+
+namespace TCC.ViewModels
+{
+    public class Tab : TSPropertyChanged
+    {
+        public List<ChatChannelOnOff> AllChannels => Utils.GetEnabledChannelsList();
+        private ICollectionView _messages;
+        private string _tabName;
+
+        public string TabName
+        {
+            get => _tabName;
+            set
+            {
+                if (_tabName == value) return;
+                _tabName = value;
+                NPC(nameof(TabName));
+            }
+        }
+        private bool _attention;
+
+        public bool Attention
+        {
+            get => _attention;
+            set
+            {
+                if (_attention == value) return;
+                _attention = value;
+                NPC(nameof(Attention));
+            }
+        }
+
+        public SynchronizedObservableCollection<string> Authors { get; set; }
+        public SynchronizedObservableCollection<string> ExcludedAuthors { get; set; }
+        public SynchronizedObservableCollection<ChatChannel> Channels { get; set; }
+        public SynchronizedObservableCollection<ChatChannel> ExcludedChannels { get; set; }
+        public ICollectionView Messages
+        {
+            get => _messages;
+            set
+            {
+                if (_messages == value) return;
+                _messages = value;
+                NPC(nameof(Messages));
+            }
+        }
+
+        public void Refresh()
+        {
+            Messages.Refresh();
+        }
+
+        public Tab(string n, ChatChannel[] ch, ChatChannel[] ex, string[] a, string[] exa)
+        {
+            _dispatcher = Dispatcher.CurrentDispatcher;
+            TabName = n;
+            Messages = new ListCollectionView(ChatWindowManager.Instance.ChatMessages);
+            Authors = new SynchronizedObservableCollection<string>(_dispatcher);
+            ExcludedAuthors = new SynchronizedObservableCollection<string>(_dispatcher);
+            Channels = new SynchronizedObservableCollection<ChatChannel>(_dispatcher);
+            ExcludedChannels = new SynchronizedObservableCollection<ChatChannel>(_dispatcher);
+            foreach (var auth in a)
+            {
+                Authors.Add(auth);
+            }
+            foreach (var auth in exa)
+            {
+                ExcludedAuthors.Add(auth);
+            }
+            foreach (var chan in ch)
+            {
+                Channels.Add(chan);
+            }
+            foreach (var chan in ex)
+            {
+                ExcludedChannels.Add(chan);
+            }
+            if (Channels.Count == 0 && Authors.Count == 0 && ExcludedChannels.Count == 0 && ExcludedAuthors.Count == 0)
+            {
+                Messages.Filter = null;
+                return;
+            }
+            ApplyFilter();
+        }
+
+        public bool Filter(ChatMessage m)
+        {
+            return (Authors.Count == 0 || Authors.Any(x => x == m.Author)) &&
+                   (Channels.Count == 0 || Channels.Any(x => x == m.Channel)) &&
+                   (ExcludedChannels.Count == 0 || ExcludedChannels.All(x => x != m.Channel)) &&
+                   (ExcludedAuthors.Count == 0 || ExcludedAuthors.All(x => x != m.Author));
+
+        }
+        public void ApplyFilter()
+        {
+            Messages.Filter = f =>
+            {
+                var m = f as ChatMessage;
+                return Filter(m);
+                //if (Authors.Count == 0 && Channels.Count != 0)
+                //{
+                //    if (ExcludedChannels.Count != 0)
+                //    {
+                //        return Channels.Contains(m.Channel) && !ExcludedChannels.Contains(m.Channel);
+                //    }
+                //    return Channels.Contains(m.Channel);
+                //}
+                //if (Channels.Count == 0 && Authors.Count != 0)
+                //{
+                //    if (ExcludedChannels.Count != 0)
+                //    {
+                //        return Authors.Contains(m.Author) && !ExcludedChannels.Contains(m.Channel);
+                //    }
+                //    return Authors.Contains(m.Author);
+                //}
+
+            };
+        }
+    }
+}
