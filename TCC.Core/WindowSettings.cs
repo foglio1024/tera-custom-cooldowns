8b46dcfc1c42aee7d403dc5967b46a15c967ea63
Foglio1024 <foglio1024@gmail.com>
2018-04-23 13:37:56 +0200

    added ChatWindowSettings extended class

--- TCC.Core/WindowSettings.cs
+++ TCC.Core/WindowSettings.cs
@@ -1,8 +1,10 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Windows;
 using System.Windows.Threading;
 using System.Xml.Linq;
 using TCC.Parsing;
+using TCC.ViewModels;
 
 namespace TCC
 {
@@ -29,60 +31,54 @@ namespace TCC
             set
             {
                 x = value;
-                NotifyPropertyChanged(nameof(X));
+                NPC(nameof(X));
             }
         }
-
         public double Y
         {
             get => y;
             set
             {
                 y = value;
-                NotifyPropertyChanged(nameof(Y));
+                NPC(nameof(Y));
             }
         }
-
         public double W
         {
             get => w;
             set
             {
                 w = value;
-                NotifyPropertyChanged(nameof(W));
+                NPC(nameof(W));
             }
         }
-
         public double H
         {
             get => h;
             set
             {
                 h = value;
-                NotifyPropertyChanged(nameof(H));
+                NPC(nameof(H));
             }
         }
-
         public bool Visible
         {
             get => visible;
             set
             {
                 visible = value;
-                NotifyPropertyChanged(nameof(Visible));
+                NPC(nameof(Visible));
             }
         }
-
         public ClickThruMode ClickThruMode
         {
             get => clickThruMode;
             set
             {
                 clickThruMode = value;
-                NotifyPropertyChanged(nameof(ClickThruMode));
+                NPC(nameof(ClickThruMode));
             }
         }
-
         public double Scale
         {
             get => scale;
@@ -90,20 +86,18 @@ namespace TCC
             {
                 if (scale == value) return;
                 scale = value;
-                NotifyPropertyChanged(nameof(Scale));
+                NPC(nameof(Scale));
             }
         }
-
         public bool AutoDim
         {
             get => autoDim;
             set
             {
                 autoDim = value;
-                NotifyPropertyChanged(nameof(AutoDim));
+                NPC(nameof(AutoDim));
             }
         }
-
         public double DimOpacity
         {
             get => dimOpacity;
@@ -116,31 +110,28 @@ namespace TCC
                 //    WindowManager.SkillsEnded = false;
                 //    WindowManager.SkillsEnded = true;
                 //}
-                NotifyPropertyChanged(nameof(DimOpacity));
+                NPC(nameof(DimOpacity));
                 WindowManager.RefreshDim();
             }
         }
-
         public bool ShowAlways
         {
             get => showAlways;
             set
             {
                 showAlways = value;
-                NotifyPropertyChanged(nameof(ShowAlways));
+                NPC(nameof(ShowAlways));
             }
         }
-
         public bool AllowTransparency
         {
             get => allowTransparency;
             set
             {
                 allowTransparency = value;
-                NotifyPropertyChanged(nameof(AllowTransparency));
+                NPC(nameof(AllowTransparency));
             }
         }
-
         public bool Enabled
         {
             get => enabled;
@@ -169,10 +160,9 @@ namespace TCC
                     App.Restart();
                 }
                 MessageFactory.Update();
-                NotifyPropertyChanged(nameof(Enabled));
+                NPC(nameof(Enabled));
             }
         }
-
 
         public WindowSettings(double _x, double _y, double _h, double _w, bool _visible, ClickThruMode _ctm, double _scale, bool _autoDim, double _dimOpacity, bool _showAlways, bool _allowTransparency, bool _enabled)
         {
@@ -191,7 +181,7 @@ namespace TCC
             enabled = _enabled;
         }
 
-        public XElement ToXElement(string name)
+        public virtual XElement ToXElement(string name)
         {
             var xe = new XElement("WindowSetting");
             xe.Add(new XAttribute("Name", name));
@@ -208,6 +198,35 @@ namespace TCC
             xe.Add(new XAttribute(nameof(AllowTransparency), AllowTransparency));
             xe.Add(new XAttribute(nameof(Enabled), Enabled));
             return xe;
+        }
+    }
+
+    public class ChatWindowSettings : WindowSettings
+    {
+        private bool lfgOn;
+
+        public List<Tab> Tabs { get; set; }
+        public bool LfgOn
+        {
+            get { return lfgOn; }
+            set
+            {
+                if (lfgOn == value) return;
+                lfgOn = value;
+                NPC();
+            }
+        }
+
+        public ChatWindowSettings(double _x, double _y, double _h, double _w, bool _visible, ClickThruMode _ctm, double _scale, bool _autoDim, double _dimOpacity, bool _showAlways, bool _allowTransparency, bool _enabled) : base(_x, _y, _h, _w, _visible, _ctm, _scale, _autoDim, _dimOpacity, _showAlways, _allowTransparency, _enabled)
+        {
+            Tabs = new List<Tab>();
+        }
+        public override XElement ToXElement(string name)
+        {
+            var b = base.ToXElement(name);
+            b.Add(SettingsManager.BuildChatTabsXElement(Tabs));
+            b.Add(new XElement(nameof(LfgOn), LfgOn));
+            return b;
         }
     }
 }
