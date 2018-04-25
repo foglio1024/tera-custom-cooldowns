79627b60c46d9e048bb71f4a619684b2f0769147
Foglio1024 <foglio1024@gmail.com>
2018-04-25 19:01:58 +0200

    - moved tab settings to own window - added tooltip for quick settings

---
+++ TCC.Core/Windows/ChatSettingsWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using TCC.ViewModels;
+
+namespace TCC.Windows
+{
+    /// <summary>
+    /// Logica di interazione per ChatSettingsWindow.xaml
+    /// </summary>
+    public partial class ChatSettingsWindow : Window
+    {
+        public ChatSettingsWindow(Tab dataContext)
+        {
+            InitializeComponent();
+            DataContext = dataContext;
+            Opacity = 0;
+        }
+
+        private void CloseChannelSettings(object sender, RoutedEventArgs e)
+        {
+            var an = new DoubleAnimation(0, TimeSpan.FromMilliseconds(200));
+            an.Completed += (s,ev) => this.Close();
+            BeginAnimation(OpacityProperty, an);
+        }
+
+        private void Grid_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            DragMove();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            BeginAnimation(OpacityProperty, new DoubleAnimation(1, TimeSpan.FromMilliseconds(500)));
+        }
+    }
+}
