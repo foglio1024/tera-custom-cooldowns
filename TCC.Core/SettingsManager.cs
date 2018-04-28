ee2aee537e02e7a1830baab47f9ee0261b261418
Foglio1024 <foglio1024@gmail.com>
2018-04-28 14:27:32 +0200

    added flight energy window

--- TCC.Core/SettingsManager.cs
+++ TCC.Core/SettingsManager.cs
@@ -65,6 +65,7 @@ namespace TCC
         public static bool AnimateChatMessages { get; set; } = false;
         public static bool ChatEnabled { get; set; } = true;
         public static bool StatSent { get; set; } = false;
+        public static bool ShowFlightEnergy { get; set; } = true;
 
         public static void LoadWindowSettings()
         {
@@ -326,6 +327,16 @@ namespace TCC
                     StatSent = Boolean.Parse(b.Attribute(nameof(StatSent)).Value);
                 }
                 catch (Exception) { }
+                try
+                {
+                    StatSent = Boolean.Parse(b.Attribute(nameof(StatSent)).Value);
+                }
+                catch (Exception) { }
+                try
+                {
+                    ShowFlightEnergy = Boolean.Parse(b.Attribute(nameof(ShowFlightEnergy)).Value);
+                }
+                catch (Exception) { }
                 //add settings here
 
                 try
@@ -350,7 +361,6 @@ namespace TCC
                 }
             }
         }
-
 
         public static List<Tab> ParseTabsSettings(XElement elem)
         {
@@ -435,7 +445,8 @@ namespace TCC
                 new XAttribute(nameof(ShowMembersLaurels), ShowMembersLaurels),
                 new XAttribute(nameof(AnimateChatMessages), AnimateChatMessages),
                 new XAttribute(nameof(ShowItemsCooldown), ShowItemsCooldown),
-                new XAttribute(nameof(StatSent), StatSent)
+                new XAttribute(nameof(StatSent), StatSent),
+                new XAttribute(nameof(ShowFlightEnergy), ShowFlightEnergy)
                 //add setting here
                 ),
                 BuildChannelsXElement(),
