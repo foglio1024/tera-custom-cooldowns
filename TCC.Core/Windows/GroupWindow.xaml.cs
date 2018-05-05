40ef8af7bb26b6ad3aff7f2164fefb87afadc112
Foglio1024 <foglio1024@gmail.com>
2018-05-05 15:44:10 +0200

    - added buttons for leader commands:   - disband   - loot settings   - reset instance - added leave party button

--- TCC.Core/Windows/GroupWindow.xaml.cs
+++ TCC.Core/Windows/GroupWindow.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Windows;
+﻿using System;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Media.Animation;
 
 namespace TCC.Windows
 {
@@ -20,5 +23,29 @@ namespace TCC.Windows
             Proxy.LootSettings();
         }
 
+        private void DisbandButtonClicked(object sender, RoutedEventArgs e)
+        {
+            Proxy.DisbandParty();
+        }
+
+        private void ResetButtonClicked(object sender, RoutedEventArgs e)
+        {
+            Proxy.ResetInstance();
+        }
+
+        private void GroupWindow_OnMouseEnter(object sender, MouseEventArgs e)
+        {
+            GroupButtons.BeginAnimation(OpacityProperty, new DoubleAnimation(1, TimeSpan.FromMilliseconds(300)));
+        }
+
+        private void GroupWindow_OnMouseLeave(object sender, MouseEventArgs e)
+        {
+            GroupButtons.BeginAnimation(OpacityProperty, new DoubleAnimation(0, TimeSpan.FromMilliseconds(300)){BeginTime = TimeSpan.FromMilliseconds(500)});
+        }
+
+        private void LeaveParty(object sender, RoutedEventArgs e)
+        {
+            Proxy.LeaveParty();
+        }
     }
 }
