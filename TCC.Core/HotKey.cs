f2be5c51bf4fcafa5470c9620b29bda98749b42c
Foglio1024 <foglio1024@gmail.com>
2018-05-16 19:57:58 +0200

    moved hotkey to own file

---
+++ TCC.Core/HotKey.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows.Forms;
+using TCC.Tera.Data;
+
+namespace TCC
+{
+    public struct HotKey
+    {
+        public HotKey(Keys k, HotkeysData.ModifierKeys m) : this()
+        {
+            Key = k;
+            Modifier = m;
+        }
+
+        public Keys Key { get; }
+        public HotkeysData.ModifierKeys Modifier { get; }
+    }
+}
