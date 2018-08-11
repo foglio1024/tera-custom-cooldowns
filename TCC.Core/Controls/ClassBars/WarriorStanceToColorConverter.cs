763bb4ded558447f95a2648624bc830d6c981f91
foglio <foglio1024@gmail.com>
2018-08-11 16:18:21 +0200

    moved converters to own files

---
+++ TCC.Core/Controls/ClassBars/WarriorStanceToColorConverter.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Media;
+using TCC.Data;
+
+namespace TCC.Controls.ClassBars
+{
+    public class WarriorStanceToColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value != null)
+            {
+                var s = (WarriorStance)value;
+                switch (s)
+                {
+                    default:
+                        return Application.Current.FindResource("KrBorderColor");
+                    case WarriorStance.Assault:
+                        return Application.Current.FindResource("AssaultStanceColor");
+                    case WarriorStance.Defensive:
+                        return Application.Current.FindResource("DefensiveStanceColor");
+                }
+            }
+            else return Brushes.Transparent;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
