b97fc67e305f4e3155def17a95d696635e038d15
Foglio1024 <foglio1024@gmail.com>
2018-08-27 16:36:03 +0200

    remove writelines

--- TCC.Core/ProxyPacketSplitter.cs
+++ TCC.Core/ProxyPacketSplitter.cs
@@ -27,22 +27,22 @@ namespace TCC
         public void Append(string data)
         {
             sb.Append(data);
-            Console.WriteLine($"Appending {data}");
-            Console.WriteLine($"StringBuilder: {sb}");
+            //Console.WriteLine($"Appending {data}");
+            //Console.WriteLine($"StringBuilder: {sb}");
             var bufferSplit = sb.ToString().Split(new[] {"\v:start:\v"}, StringSplitOptions.RemoveEmptyEntries);
 
             foreach (var b in bufferSplit)
             {
                 if (!b.Contains(EndToken))
                 {
-                    Console.WriteLine($"[Splitter] {b} -- is incomplete, continue");
+                    //Console.WriteLine($"[Splitter] {b} -- is incomplete, continue");
                     continue;
                 }
                 Packets.Enqueue(b.Replace(EndToken, ""));
                 sb = sb.Replace(StartToken + b, "");
             }
 
-            Console.WriteLine($"Left: {sb}");
+            //Console.WriteLine($"Left: {sb}");
         }
     }
 }
