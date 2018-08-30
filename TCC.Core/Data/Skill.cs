60683f32f8233a9a52db58ac9763905d8511c138
Foglio1024 <foglio1024@gmail.com>
2018-08-30 12:37:14 +0200

    added details to skill class

--- TCC.Core/Data/Skill.cs
+++ TCC.Core/Data/Skill.cs
@@ -37,6 +37,8 @@ namespace TCC.Data
                 return Name;
             }
         }
+
+        public string Detail { get; set; }
         //public ImageBrush IconBrush { get
         //    {
 
