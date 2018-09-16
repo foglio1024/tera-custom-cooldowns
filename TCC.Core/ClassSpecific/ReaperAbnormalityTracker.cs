0b00c67340a0be7c0713ec0bb978aa8efd88b3cc
Foglio1024 <foglio1024@gmail.com>
2018-09-16 18:01:16 +0200

    add shadow step and death spiral precooldown

--- TCC.Core/ClassSpecific/ReaperAbnormalityTracker.cs
+++ TCC.Core/ClassSpecific/ReaperAbnormalityTracker.cs
@@ -1,4 +1,5 @@
-﻿using TCC.Parsing.Messages;
+﻿using TCC.Data;
+using TCC.Parsing.Messages;
 using TCC.ViewModels;
 
 namespace TCC.ClassSpecific
@@ -6,12 +7,31 @@ namespace TCC.ClassSpecific
     public class ReaperAbnormalityTracker : ClassAbnormalityTracker
     {
         private const int ShadowReapingId = 10151010;
+        private const int ShadowStepId = 10151000;
+        private const int DeathSpiralId = 10151131;
 
         public override void CheckAbnormality(S_ABNORMALITY_BEGIN p)
         {
             if (p.TargetId != SessionManager.CurrentPlayer.EntityId) return;
             CheckShadowReaping(p);
+            CheckShadowStep(p);
+            CheckDeathSpiral(p);
         }
+
+        private void CheckDeathSpiral(S_ABNORMALITY_BEGIN p)
+        {
+            if (p.AbnormalityId != DeathSpiralId) return;
+            if (!SessionManager.SkillsDatabase.TryGetSkillByIconName("icon_skills.chainbrandish_tex", SessionManager.CurrentPlayer.Class, out var sk)) return;
+            CooldownWindowViewModel.Instance.AddOrRefresh(new SkillCooldown(sk, p.Duration, CooldownType.Skill, CooldownWindowViewModel.Instance.GetDispatcher(), true, true));
+        }
+
+        private void CheckShadowStep(S_ABNORMALITY_BEGIN p)
+        {
+            if (p.AbnormalityId != ShadowStepId) return;
+            if (!SessionManager.SkillsDatabase.TryGetSkillByIconName("icon_skills.instantleap_tex", SessionManager.CurrentPlayer.Class, out var sk)) return;
+            CooldownWindowViewModel.Instance.AddOrRefresh(new SkillCooldown(sk, p.Duration, CooldownType.Skill, CooldownWindowViewModel.Instance.GetDispatcher(), true, true));
+        }
+
         public override void CheckAbnormality(S_ABNORMALITY_REFRESH p)
         {
             if (p.TargetId != SessionManager.CurrentPlayer.EntityId) return;
