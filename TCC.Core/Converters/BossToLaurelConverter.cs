b7ef7cd115edcd4e3cc9727f210643a26853eb0e
Foglio1024 <foglio1024@gmail.com>
2018-09-23 22:14:38 +0200

    fix gg laurel

--- TCC.Core/Converters/BossToLaurelConverter.cs
+++ TCC.Core/Converters/BossToLaurelConverter.cs
@@ -19,28 +19,28 @@ namespace TCC.Converters
             switch (npc.ZoneId)
             {
                 case 620: //Balder's Refuge guardian
-                         if (npc.TemplateId == 1000) laurel = "gold";
+                         if (npc.TemplateId == 1000) laurel = "silver";
                     else if (npc.TemplateId == 1001) laurel = "bronze";
                     else if (npc.TemplateId == 1004) laurel = "bronze";
                     else if (npc.TemplateId == 1005) laurel = "bronze";
                     break;
                 case 621: //Crack the crustacean guardian
-                    if (npc.TemplateId == 3001) laurel = "gold";
+                    if (npc.TemplateId == 3001) laurel = "silver";
                     break;
                 case 622: //Dreadreaper guardian
-                    if (npc.TemplateId == 1000) laurel = "gold";
+                    if (npc.TemplateId == 1000) laurel = "silver";
                     break;
                 case 713: //Ghillieglade
                 case 813: //Ghillieglade
-                         if (npc.TemplateId == 81301) laurel = "silver";
-                    else if (npc.TemplateId == 81398) laurel = "bronze";
-                    else if (npc.TemplateId == 81399) laurel = "bronze";
+                         if (npc.TemplateId == 81301) laurel = "gold";
+                    else if (npc.TemplateId == 81398) laurel = "silver";
+                    else if (npc.TemplateId == 81399) laurel = "silver";
 
                     break;
                 case 628: //Superior guardian
-                    if (npc.TemplateId == 1000) laurel = "champion";
-                    else if (npc.TemplateId == 3000) laurel = "diamond";
-                    else if (npc.TemplateId == 3001) laurel = "diamond";
+                    if (npc.TemplateId == 1000) laurel = "gold";
+                    else if (npc.TemplateId == 3000) laurel = "silver";
+                    else if (npc.TemplateId == 3001) laurel = "silver";
                     break;
                 case 434: //Dreadspire
                     if (npc.TemplateId == 1000 || npc.TemplateId == 2000) laurel = "bronze";
