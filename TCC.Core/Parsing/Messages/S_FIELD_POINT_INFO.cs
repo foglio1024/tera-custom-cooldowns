087aa1587e02d0bc273eed2632195bd365ae9b6e
Foglio1024 <foglio1024@gmail.com>
2018-10-07 14:37:13 +0200

    parse cleared guardians too

--- TCC.Core/Parsing/Messages/S_FIELD_POINT_INFO.cs
+++ TCC.Core/Parsing/Messages/S_FIELD_POINT_INFO.cs
@@ -9,12 +9,14 @@ namespace TCC.Parsing.Messages
         public uint Points { get; }
         public uint MaxPoints { get; set; }
         public int Claimed { get; set; }
+        public int Cleared { get; set; }
 
         public S_FIELD_POINT_INFO(TeraMessageReader reader) : base(reader)
         {
             Points = reader.ReadUInt32();
             MaxPoints = reader.ReadUInt32();
             Claimed = reader.ReadInt32() + 1;
+            Cleared = reader.ReadInt32();
         }
     }
 }
