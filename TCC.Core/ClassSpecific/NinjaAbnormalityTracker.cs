f516252f416d6b30fbc3db2400bf755f06f73713
Seyuna <Seyuna.Tera@gmail.com>
2018-10-11 21:54:43 +0200

    Added ninja awk classwindow

--- TCC.Core/ClassSpecific/NinjaAbnormalityTracker.cs
+++ TCC.Core/ClassSpecific/NinjaAbnormalityTracker.cs
@@ -6,17 +6,26 @@ namespace TCC.ClassSpecific
     public class NinjaAbnormalityTracker :ClassAbnormalityTracker
     {
         private const int FocusId = 10154030;
+        private const int InnerHarmonyBuffId = 10154480;
 
         public override void CheckAbnormality(S_ABNORMALITY_BEGIN p)
         {
             if (!p.TargetId.IsMe()) return;
             CheckFocus(p);
+            CheckInnerHarmony(p);
+        }
+
+        public override void CheckAbnormality(S_ABNORMALITY_REFRESH p)
+        {
+            if (!p.TargetId.IsMe()) return;
+            CheckInnerHarmony(p);
         }
 
         public override void CheckAbnormality(S_ABNORMALITY_END p)
         {
             if (!p.TargetId.IsMe()) return;
             CheckFocus(p);
+            CheckInnerHarmony(p);
         }
 
         private static void CheckFocus(S_ABNORMALITY_BEGIN p)
@@ -28,6 +37,23 @@ namespace TCC.ClassSpecific
         {
             if (p.AbnormalityId != FocusId) return;
             ((NinjaBarManager)ClassWindowViewModel.Instance.CurrentManager).FocusOn = false;
+        }
+
+        private static void CheckInnerHarmony(S_ABNORMALITY_BEGIN p)
+        {
+            if (p.AbnormalityId != InnerHarmonyBuffId) return;
+            ((NinjaBarManager)ClassWindowViewModel.Instance.CurrentManager).InnerHarmony.Buff.Start(p.Duration);
+
+        }
+        private static void CheckInnerHarmony(S_ABNORMALITY_REFRESH p)
+        {
+            if (p.AbnormalityId != InnerHarmonyBuffId) return;
+            ((NinjaBarManager)ClassWindowViewModel.Instance.CurrentManager).InnerHarmony.Buff.Refresh(p.Duration);
+        }
+        private static void CheckInnerHarmony(S_ABNORMALITY_END p)
+        {
+            if (p.AbnormalityId != InnerHarmonyBuffId) return;
+            ((NinjaBarManager)ClassWindowViewModel.Instance.CurrentManager).InnerHarmony.Buff.Refresh(0);
         }
     }
 }
--- TCC.Core/ViewModels/ClassManagers/NinjaBarManager.cs
+++ TCC.Core/ViewModels/ClassManagers/NinjaBarManager.cs
@@ -9,6 +9,7 @@ namespace TCC.ViewModels
 
         public FixedSkillCooldown BurningHeart { get; set; }
         public FixedSkillCooldown FireAvalanche { get; set; }
+        public DurationCooldownIndicator InnerHarmony { get; set; }
 
         public bool FocusOn
         {
@@ -40,8 +41,14 @@ namespace TCC.ViewModels
         {
             SessionManager.SkillsDatabase.TryGetSkill(150700, Class.Ninja, out var bh);
             SessionManager.SkillsDatabase.TryGetSkill(80200, Class.Ninja, out var fa);
+            SessionManager.SkillsDatabase.TryGetSkill(230100, Class.Ninja, out var ih);
             BurningHeart = new FixedSkillCooldown(bh,  false);
             FireAvalanche = new FixedSkillCooldown(fa,  false);
+
+            InnerHarmony = new DurationCooldownIndicator(Dispatcher);
+            InnerHarmony.Cooldown = new FixedSkillCooldown(ih, true);
+            InnerHarmony.Buff = new FixedSkillCooldown(ih, false);
+
             StaminaTracker.PropertyChanged += FlashOnMaxSt;
 
         }
@@ -56,6 +63,11 @@ namespace TCC.ViewModels
             if (sk.Skill.IconName == BurningHeart.Skill.IconName)
             {
                 BurningHeart.Start(sk.Cooldown);
+                return true;
+            }
+            if (sk.Skill.IconName == InnerHarmony.Cooldown.Skill.IconName)
+            {
+                InnerHarmony.Cooldown.Start(sk.Cooldown);
                 return true;
             }
             return false;
