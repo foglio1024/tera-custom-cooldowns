8df02bdee17bb4365dcfab77108c9d931c053770
Foglio1024 <foglio1024@gmail.com>
2018-10-13 18:22:25 +0200

    added modular system to gunner class window

--- TCC.Core/ClassSpecific/GunnerAbnormalityTracker.cs
+++ TCC.Core/ClassSpecific/GunnerAbnormalityTracker.cs
@@ -1,4 +1,5 @@
-﻿using TCC.Data;
+﻿using System.Collections.Generic;
+using TCC.Data;
 using TCC.Parsing.Messages;
 using TCC.ViewModels;
 
@@ -7,10 +8,13 @@ namespace TCC.ClassSpecific
     public class GunnerAbnormalityTracker : ClassAbnormalityTracker
     {
         private static readonly uint DashingReloadId = 10152354;
+        private static readonly List<uint> LaserTargetingIDs = new List<uint> { 10152340, 10152342,10152345 };
+
         public override void CheckAbnormality(S_ABNORMALITY_BEGIN p)
         {
             if (!p.TargetId.IsMe()) return;
             CheckDashingReload(p);
+            CheckLaserTargeting(p);
         }
 
         private static void CheckDashingReload(S_ABNORMALITY_BEGIN p)
@@ -21,6 +25,22 @@ namespace TCC.ClassSpecific
             if (!SessionManager.SkillsDatabase.TryGetSkillByIconName("icon_skills.ambushrolling_tex", SessionManager.CurrentPlayer.Class, out var sk1)) return;
             CooldownWindowViewModel.Instance.AddOrRefresh(new SkillCooldown(sk, p.Duration, CooldownType.Skill, CooldownWindowViewModel.Instance.GetDispatcher(), true, true));
             CooldownWindowViewModel.Instance.AddOrRefresh(new SkillCooldown(sk1, p.Duration, CooldownType.Skill, CooldownWindowViewModel.Instance.GetDispatcher(), true, true));
+        }
+
+        private static void CheckLaserTargeting(S_ABNORMALITY_BEGIN p)
+        {
+            if (!LaserTargetingIDs.Contains(p.AbnormalityId)) return;
+            ((GunnerBarManager)ClassWindowViewModel.Instance.CurrentManager).ModularSystem.Buff.Start(p.Duration);
+        }
+        private static void CheckLaserTargeting(S_ABNORMALITY_REFRESH p)
+        {
+            if (!LaserTargetingIDs.Contains(p.AbnormalityId)) return;
+            ((GunnerBarManager)ClassWindowViewModel.Instance.CurrentManager).ModularSystem.Buff.Refresh(p.Duration);
+        }
+        private static void CheckLaserTargeting(S_ABNORMALITY_END p)
+        {
+            if (!LaserTargetingIDs.Contains(p.AbnormalityId)) return;
+            ((GunnerBarManager)ClassWindowViewModel.Instance.CurrentManager).ModularSystem.Buff.Refresh(0);
         }
     }
 }
