dc51a35ab64c9bda4ee5ff96572b6281d13c4841
Foglio1024 <foglio1024@gmail.com>
2018-10-19 00:09:09 +0200

    added godsfall to valk class window

--- TCC.Core/ViewModels/ClassManagers/ValkyrieBarManager.cs
+++ TCC.Core/ViewModels/ClassManagers/ValkyrieBarManager.cs
@@ -6,6 +6,7 @@ namespace TCC.ViewModels
     {
         public Counter RunemarksCounter { get; set; }
         public DurationCooldownIndicator Ragnarok { get; private set; }
+        public DurationCooldownIndicator Godsfall { get; private set; }
 
         public ValkyrieBarManager()
         {
@@ -15,10 +16,18 @@ namespace TCC.ViewModels
         public override void LoadSpecialSkills()
         {
             //Ragnarok
-            Ragnarok = new DurationCooldownIndicator(Dispatcher);
             SessionManager.SkillsDatabase.TryGetSkill(120100, Class.Valkyrie, out var rag);
-            Ragnarok.Cooldown = new FixedSkillCooldown(rag, true);
-            Ragnarok.Buff = new FixedSkillCooldown(rag, false);
+            SessionManager.SkillsDatabase.TryGetSkill(250100, Class.Valkyrie, out var gf);
+            Ragnarok = new DurationCooldownIndicator(Dispatcher)
+            {
+                Cooldown = new FixedSkillCooldown(rag, true),
+                Buff = new FixedSkillCooldown(rag, false)
+            };
+            Godsfall = new DurationCooldownIndicator(Dispatcher)
+            {
+                Cooldown = new FixedSkillCooldown(gf, true),
+                Buff = new FixedSkillCooldown(gf, false)
+            };
         }
         public override bool StartSpecialSkill(SkillCooldown sk)
         {
@@ -26,6 +35,11 @@ namespace TCC.ViewModels
             if (sk.Skill.IconName == Ragnarok.Cooldown.Skill.IconName)
             {
                 Ragnarok.Cooldown.Start(sk.Cooldown);
+                return true;
+            }
+            if (sk.Skill.IconName == Godsfall.Cooldown.Skill.IconName)
+            {
+                Godsfall.Cooldown.Start(sk.Cooldown);
                 return true;
             }
             return false;
