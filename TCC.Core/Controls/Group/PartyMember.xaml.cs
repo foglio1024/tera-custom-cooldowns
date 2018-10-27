f5a4027af0a0ca98a22345ab17802aa7d2a5cfac
Foglio1024 <foglio1024@gmail.com>
2018-10-27 11:36:18 +0200

    moved group controls to appropriate namespace

--- TCC.Core/Controls/Group/PartyMember.xaml.cs
+++ TCC.Core/Controls/Group/PartyMember.xaml.cs
@@ -6,53 +6,52 @@ using System.Windows.Media.Animation;
 using TCC.Data;
 using TCC.ViewModels;
 
-namespace TCC.Controls
+namespace TCC.Controls.Group
 {
-    /// <summary>
-    /// Logica di interazione per RaidMember.xaml
-    /// </summary>
-    public partial class RaidMember //TODO: make base class for this when???
+    public partial class PartyMember //TODO: make base class for this when???
     {
-        public RaidMember()
+        public PartyMember()
         {
             InitializeComponent();
             Unloaded += (_, __) => { SettingsWindowViewModel.AbnormalityShapeChanged -= OnAbnormalityShapeChanged; };
         }
 
-        private User _dc;
         private void UserControl_Loaded(object sender, RoutedEventArgs e)
         {
-            _dc = (User)DataContext;
             UpdateSettings();
-
             AnimateIn();
             GroupWindowViewModel.Instance.SettingsUpdated += UpdateSettings;
             SettingsWindowViewModel.AbnormalityShapeChanged += OnAbnormalityShapeChanged;
         }
+
+        private void OnAbnormalityShapeChanged()
+        {
+            Buffs.ItemTemplateSelector = null;
+            Buffs.ItemTemplateSelector = Application.Current.FindResource("PartyAbnormalityTemplateSelector") as DataTemplateSelector;
+            Debuffs.ItemTemplateSelector = null;
+            Debuffs.ItemTemplateSelector = Application.Current.FindResource("PartyAbnormalityTemplateSelector") as DataTemplateSelector;
+            
+        }
+
         private void UpdateSettings()
         {
-            SetMP();
-            SetHP();
+            SetMp();
+            SetHp();
             SetBuffs();
             SetDebuffs();
             SetLaurels();
             SetAwakenIcon();
-
         }
-        private void OnAbnormalityShapeChanged()
-        {
-            Buffs.ItemTemplateSelector = null;
-            Buffs.ItemTemplateSelector = Application.Current.FindResource("RaidAbnormalityTemplateSelector") as DataTemplateSelector;
-            Debuffs.ItemTemplateSelector = null;
-            Debuffs.ItemTemplateSelector = Application.Current.FindResource("RaidAbnormalityTemplateSelector") as DataTemplateSelector;
 
-        }
 
         private void SetAwakenIcon()
         {
             Dispatcher.Invoke(() => {
                 if (!(DataContext is User user)) return;
-                AwakenIcon.Visibility = Settings.ShowAwakenIcon ? (user.Awakened ? Visibility.Visible : Visibility.Collapsed) : Visibility.Collapsed;
+                AwakenIcon.Visibility = TCC.Settings.ShowAwakenIcon ? (user.Awakened ? Visibility.Visible : Visibility.Collapsed) : 
+                    Visibility.Collapsed;
+                AwakenBorder.Visibility = TCC.Settings.ShowAwakenIcon ? (user.Awakened ? Visibility.Visible : Visibility.Collapsed) : 
+                    Visibility.Collapsed;
             });
         }
 
@@ -62,7 +61,7 @@ namespace TCC.Controls
             {
                 Dispatcher.Invoke(() =>
                 {
-                    LaurelImage.Visibility = Settings.ShowMembersLaurels ? Visibility.Visible : Visibility.Hidden;
+                    LaurelImage.Visibility = TCC.Settings.ShowMembersLaurels ? Visibility.Visible : Visibility.Collapsed;
                 });
             }
             catch
@@ -77,14 +76,11 @@ namespace TCC.Controls
                 Dispatcher.Invoke(() =>
                 {
                     if (!(DataContext is User user)) return;
-
-                    Buffs.ItemsSource = Settings.IgnoreGroupBuffs ? null : user.Buffs;
-                    BuffGrid.Visibility = Settings.IgnoreGroupBuffs
+                    Buffs.ItemsSource = TCC.Settings.IgnoreGroupBuffs ? null : user.Buffs;
+                    BuffGrid.Visibility = TCC.Settings.IgnoreGroupBuffs
                         ? Visibility.Collapsed
                         : Visibility.Visible;
-
                 });
-
             }
             catch
             {
@@ -97,30 +93,47 @@ namespace TCC.Controls
             {
                 Dispatcher.Invoke(() =>
                 {
-                    // ReSharper disable once IsExpressionAlwaysTrue
-                    if(!(_dc is User)) return;
-                    Debuffs.ItemsSource = Settings.IgnoreGroupDebuffs ? null : _dc.Debuffs;
-                    DebuffGrid.Visibility = Settings.IgnoreGroupDebuffs
+                    if (!(DataContext is User)) return;
+                    Debuffs.ItemsSource = TCC.Settings.IgnoreGroupDebuffs ? null : ((User)DataContext).Debuffs;
+                    DebuffGrid.Visibility = TCC.Settings.IgnoreGroupDebuffs
                         ? Visibility.Collapsed
                         : Visibility.Visible;
                 });
-
             }
             catch
             {
                 // ignored
             }
         }
+        private void SetMp()
+        {
+            Dispatcher.Invoke(() =>
+            {
+                MpBar.Visibility = !TCC.Settings.DisablePartyMP ? Visibility.Visible : Visibility.Collapsed;
+            });
+        }
+        private void SetHp()
+        {
+            Dispatcher.Invoke(() =>
+            {
+                HpBar.Visibility = !TCC.Settings.DisablePartyHP ? Visibility.Visible : Visibility.Collapsed;
+            });
+        }
 
-        public void AnimateIn()
+        private void AnimateIn()
         {
             var an = new DoubleAnimation(0, 1, TimeSpan.FromMilliseconds(500));
             BeginAnimation(OpacityProperty, an);
         }
+
         internal void AnimateOut()
         {
             var an = new DoubleAnimation(1, 0, TimeSpan.FromMilliseconds(200));
             BeginAnimation(OpacityProperty, an);
+        }
+
+        private void UserControl_Unloaded(object sender, RoutedEventArgs e)
+        {
         }
 
         private void UserControl_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
@@ -128,20 +141,7 @@ namespace TCC.Controls
             var dc = (User)DataContext;
             Proxy.AskInteractive(dc.ServerId, dc.Name);
         }
-        private void SetMP()
-        {
-            Dispatcher.Invoke(() =>
-            {
-                MpBar.Visibility = !Settings.DisablePartyMP ? Visibility.Visible : Visibility.Collapsed;
-            });
-        }
-        private void SetHP()
-        {
-            Dispatcher.Invoke(() =>
-            {
-                HpBar.Visibility = !Settings.DisablePartyHP ? Visibility.Visible : Visibility.Collapsed;
-            });
-        }
+
         private void ToolTip_OnOpened(object sender, RoutedEventArgs e)
         {
             FocusManager.PauseTopmost = true;
@@ -151,7 +151,5 @@ namespace TCC.Controls
         {
             FocusManager.PauseTopmost = false;
         }
-
     }
 }
-
--- TCC.Core/Controls/Group/RaidMember.xaml.cs
+++ TCC.Core/Controls/Group/RaidMember.xaml.cs
@@ -6,52 +6,53 @@ using System.Windows.Media.Animation;
 using TCC.Data;
 using TCC.ViewModels;
 
-namespace TCC.Controls
+namespace TCC.Controls.Group
 {
-    public partial class PartyMember //TODO: make base class for this when???
+    /// <summary>
+    /// Logica di interazione per RaidMember.xaml
+    /// </summary>
+    public partial class RaidMember //TODO: make base class for this when???
     {
-        public PartyMember()
+        public RaidMember()
         {
             InitializeComponent();
             Unloaded += (_, __) => { SettingsWindowViewModel.AbnormalityShapeChanged -= OnAbnormalityShapeChanged; };
         }
 
+        private User _dc;
         private void UserControl_Loaded(object sender, RoutedEventArgs e)
         {
+            _dc = (User)DataContext;
             UpdateSettings();
+
             AnimateIn();
             GroupWindowViewModel.Instance.SettingsUpdated += UpdateSettings;
             SettingsWindowViewModel.AbnormalityShapeChanged += OnAbnormalityShapeChanged;
         }
-
-        private void OnAbnormalityShapeChanged()
-        {
-            Buffs.ItemTemplateSelector = null;
-            Buffs.ItemTemplateSelector = Application.Current.FindResource("PartyAbnormalityTemplateSelector") as DataTemplateSelector;
-            Debuffs.ItemTemplateSelector = null;
-            Debuffs.ItemTemplateSelector = Application.Current.FindResource("PartyAbnormalityTemplateSelector") as DataTemplateSelector;
-            
-        }
-
         private void UpdateSettings()
         {
-            SetMp();
-            SetHp();
+            SetMP();
+            SetHP();
             SetBuffs();
             SetDebuffs();
             SetLaurels();
             SetAwakenIcon();
-        }
 
+        }
+        private void OnAbnormalityShapeChanged()
+        {
+            Buffs.ItemTemplateSelector = null;
+            Buffs.ItemTemplateSelector = Application.Current.FindResource("RaidAbnormalityTemplateSelector") as DataTemplateSelector;
+            Debuffs.ItemTemplateSelector = null;
+            Debuffs.ItemTemplateSelector = Application.Current.FindResource("RaidAbnormalityTemplateSelector") as DataTemplateSelector;
+
+        }
 
         private void SetAwakenIcon()
         {
             Dispatcher.Invoke(() => {
                 if (!(DataContext is User user)) return;
-                AwakenIcon.Visibility = Settings.ShowAwakenIcon ? (user.Awakened ? Visibility.Visible : Visibility.Collapsed) : 
-                    Visibility.Collapsed;
-                AwakenBorder.Visibility = Settings.ShowAwakenIcon ? (user.Awakened ? Visibility.Visible : Visibility.Collapsed) : 
-                    Visibility.Collapsed;
+                AwakenIcon.Visibility = TCC.Settings.ShowAwakenIcon ? (user.Awakened ? Visibility.Visible : Visibility.Collapsed) : Visibility.Collapsed;
             });
         }
 
@@ -61,7 +62,7 @@ namespace TCC.Controls
             {
                 Dispatcher.Invoke(() =>
                 {
-                    LaurelImage.Visibility = Settings.ShowMembersLaurels ? Visibility.Visible : Visibility.Collapsed;
+                    LaurelImage.Visibility = TCC.Settings.ShowMembersLaurels ? Visibility.Visible : Visibility.Hidden;
                 });
             }
             catch
@@ -76,11 +77,14 @@ namespace TCC.Controls
                 Dispatcher.Invoke(() =>
                 {
                     if (!(DataContext is User user)) return;
-                    Buffs.ItemsSource = Settings.IgnoreGroupBuffs ? null : user.Buffs;
-                    BuffGrid.Visibility = Settings.IgnoreGroupBuffs
+
+                    Buffs.ItemsSource = TCC.Settings.IgnoreGroupBuffs ? null : user.Buffs;
+                    BuffGrid.Visibility = TCC.Settings.IgnoreGroupBuffs
                         ? Visibility.Collapsed
                         : Visibility.Visible;
+
                 });
+
             }
             catch
             {
@@ -93,47 +97,30 @@ namespace TCC.Controls
             {
                 Dispatcher.Invoke(() =>
                 {
-                    if (!(DataContext is User)) return;
-                    Debuffs.ItemsSource = Settings.IgnoreGroupDebuffs ? null : ((User)DataContext).Debuffs;
-                    DebuffGrid.Visibility = Settings.IgnoreGroupDebuffs
+                    // ReSharper disable once IsExpressionAlwaysTrue
+                    if(!(_dc is User)) return;
+                    Debuffs.ItemsSource = TCC.Settings.IgnoreGroupDebuffs ? null : _dc.Debuffs;
+                    DebuffGrid.Visibility = TCC.Settings.IgnoreGroupDebuffs
                         ? Visibility.Collapsed
                         : Visibility.Visible;
                 });
+
             }
             catch
             {
                 // ignored
             }
         }
-        private void SetMp()
-        {
-            Dispatcher.Invoke(() =>
-            {
-                MpBar.Visibility = !Settings.DisablePartyMP ? Visibility.Visible : Visibility.Collapsed;
-            });
-        }
-        private void SetHp()
-        {
-            Dispatcher.Invoke(() =>
-            {
-                HpBar.Visibility = !Settings.DisablePartyHP ? Visibility.Visible : Visibility.Collapsed;
-            });
-        }
 
-        private void AnimateIn()
+        public void AnimateIn()
         {
             var an = new DoubleAnimation(0, 1, TimeSpan.FromMilliseconds(500));
             BeginAnimation(OpacityProperty, an);
         }
-
         internal void AnimateOut()
         {
             var an = new DoubleAnimation(1, 0, TimeSpan.FromMilliseconds(200));
             BeginAnimation(OpacityProperty, an);
-        }
-
-        private void UserControl_Unloaded(object sender, RoutedEventArgs e)
-        {
         }
 
         private void UserControl_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
@@ -141,7 +128,20 @@ namespace TCC.Controls
             var dc = (User)DataContext;
             Proxy.AskInteractive(dc.ServerId, dc.Name);
         }
-
+        private void SetMP()
+        {
+            Dispatcher.Invoke(() =>
+            {
+                MpBar.Visibility = !TCC.Settings.DisablePartyMP ? Visibility.Visible : Visibility.Collapsed;
+            });
+        }
+        private void SetHP()
+        {
+            Dispatcher.Invoke(() =>
+            {
+                HpBar.Visibility = !TCC.Settings.DisablePartyHP ? Visibility.Visible : Visibility.Collapsed;
+            });
+        }
         private void ToolTip_OnOpened(object sender, RoutedEventArgs e)
         {
             FocusManager.PauseTopmost = true;
@@ -151,5 +151,7 @@ namespace TCC.Controls
         {
             FocusManager.PauseTopmost = false;
         }
+
     }
 }
+
