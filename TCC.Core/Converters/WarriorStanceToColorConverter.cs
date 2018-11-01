be070a17538a0ccbc2d34d3ff45d1817d34d3489
Foglio1024 <foglio1024@gmail.com>
2018-11-01 11:35:25 +0100

    extend stance color converter

--- TCC.Core/Converters/WarriorStanceToColorConverter.cs
+++ TCC.Core/Converters/WarriorStanceToColorConverter.cs
@@ -11,20 +11,29 @@ namespace TCC.Converters
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
+            var col = parameter != null && parameter.ToString().IndexOf("color", StringComparison.Ordinal) != -1;
+            var light = parameter != null && parameter.ToString().IndexOf("light", StringComparison.Ordinal) != -1;
+            var fallback = parameter != null && parameter.ToString().IndexOf("fallback", StringComparison.Ordinal) != -1;
+            var lightFback = Colors.DarkGray;
+            lightFback.A = 170;
+            var color = fallback ? light ?  Colors.White : lightFback : Colors.Transparent;
+
             if (value != null)
             {
                 var s = (WarriorStance)value;
                 switch (s)
                 {
-                    default:
-                        return Application.Current.FindResource("RevampBorderBrush");
                     case WarriorStance.Assault:
-                        return Application.Current.FindResource("AssaultStanceBrush");
+                        color = (Color)Application.Current.FindResource($"AssaultStanceColor{(light ? "Light" : "")}");
+                        break;
                     case WarriorStance.Defensive:
-                        return Application.Current.FindResource("DefensiveStanceBrush");
+                        color = (Color)Application.Current.FindResource($"DefensiveStanceColor{(light ? "Light" : "")}");
+                        break;
                 }
             }
-            else return Brushes.Transparent;
+
+            if (col) return color;
+            return new SolidColorBrush(color);
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
