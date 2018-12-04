195c09f6335943ba2928082f98d5c4966c91c9e8
Foglio1024 <foglio1024@gmail.com>
2018-12-04 20:25:40 +0100

    add Dashboard window

---
+++ TCC.Core/Windows/Dashboard.xaml.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Interop;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using TCC.Controls.Dashboard;
+using TCC.Data.Pc;
+using TCC.ViewModels;
+
+namespace TCC.Windows
+
+{
+    /// <summary>
+    /// Logica di interazione per Dashboard.xaml
+    /// </summary>
+    public partial class Dashboard : TccWindow
+    {
+        public DashboardViewModel VM => Dispatcher.Invoke(() => DataContext as DashboardViewModel);
+        public IntPtr Handle => Dispatcher.Invoke(() => new WindowInteropHelper(this).Handle);
+
+        public Dashboard()
+        {
+            InitializeComponent();
+            DataContext = new DashboardViewModel();
+        }
+
+        private void OnCloseButtonClick(object sender, RoutedEventArgs e)
+        {
+            HideWindow();
+            VM.SaveCharacters();
+        }
+
+
+        private void ChangeView(object sender, RoutedEventArgs e)
+        {
+            VM.DetailView = !VM.DetailView;
+        }
+
+        private void OnTitleBarMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            DragMove();
+        }
+
+        private Popup _last;
+
+        private void CreatePopup(FrameworkElement sender)
+        {
+            if (_last != null) _last.IsOpen = false;
+            _last = new Popup
+            {
+                Placement = PlacementMode.Right,
+                AllowsTransparency = true,
+                Child = new VanguardInfoPopup()
+                {
+                    DataContext = sender?.DataContext
+                },
+                PlacementTarget = sender,
+                IsOpen = true
+            };
+            _last.VerticalOffset = -((FrameworkElement)_last.Child).ActualHeight + 8;
+        }
+
+        private void ShowVanguardCreditsPopup(object sender, MouseEventArgs e)
+        {
+            CreatePopup((FrameworkElement)sender);
+
+            var gauge = (sender as RectangleBarGauge);
+            var dc = gauge?.DataContext as Character;
+
+            ((VanguardInfoPopup) _last.Child).Icon.Children.Add(new Ellipse() { Fill = gauge?.Color });
+            ((VanguardInfoPopup) _last.Child).Text.Text = $"Vanguard credits: {dc?.VanguardCredits}";
+        }
+
+        private void ShowVanguardDailyPopup(object sender, MouseEventArgs e)
+        {
+            CreatePopup((FrameworkElement)sender);
+            var gauge = (sender as RectangleBarGauge);
+            var dc = gauge?.DataContext as Character;
+            ((VanguardInfoPopup)_last.Child).Icon.Children.Add(new Ellipse() { Fill = gauge?.Color });
+            ((VanguardInfoPopup)_last.Child).Text.Text = $"Vanguard daily quests: {dc?.VanguardDailiesDone}";
+
+        }
+
+        private void ShowVanguardWeeklyPopup(object sender, MouseEventArgs e)
+        {
+            CreatePopup((FrameworkElement)sender);
+            var gauge = (sender as RectangleBarGauge);
+            var dc = gauge?.DataContext as Character;
+            ((VanguardInfoPopup)_last.Child).Icon.Children.Add(new Ellipse() { Fill = gauge?.Color });
+            ((VanguardInfoPopup)_last.Child).Text.Text = $"Vanguard weekly quests: {dc?.VanguardWeekliesDone}";
+
+        }
+
+        private void ShowGuardianCreditsPopup(object sender, MouseEventArgs e)
+        {
+            CreatePopup((FrameworkElement)sender);
+            var gauge = (sender as RectangleBarGauge);
+            var dc = gauge?.DataContext as Character;
+            ((VanguardInfoPopup)_last.Child).Icon.Children.Add(new Ellipse() { Fill = gauge?.Color });
+            ((VanguardInfoPopup)_last.Child).Text.Text = $"Guardian credits: {dc?.GuardianCredits}";
+
+
+        }
+
+        private void ShowGuardianQuestsPopup(object sender, MouseEventArgs e)
+        {
+            CreatePopup((FrameworkElement)sender);
+            var gauge = (sender as RectangleBarGauge);
+            var dc = gauge?.DataContext as Character;
+            ((VanguardInfoPopup)_last.Child).Icon.Children.Add(new Ellipse() { Fill = gauge?.Color });
+            ((VanguardInfoPopup)_last.Child).Text.Text = $"Guardian daily quests: {dc?.ClaimedGuardianQuests}";
+
+
+        }
+
+        private void ShowDragonwingPopup(object sender, MouseEventArgs e)
+        {
+            CreatePopup((FrameworkElement)sender);
+            var gauge = (sender as RectangleBarGauge);
+            var dc = gauge?.DataContext as Character;
+            ((VanguardInfoPopup)_last.Child).Icon.Children.Add(new Ellipse() { Fill = gauge?.Color });
+            ((VanguardInfoPopup)_last.Child).Text.Text = $"Dragonwing scales: {dc?.DragonwingScales}";
+
+
+        }
+
+        private void ShowScrollPiecesPopup(object sender, MouseEventArgs e)
+        {
+            CreatePopup((FrameworkElement)sender);
+            var gauge = (sender as RectangleBarGauge);
+            var dc = gauge?.DataContext as Character;
+            ((VanguardInfoPopup)_last.Child).Icon.Children.Add(new Ellipse() { Fill = gauge?.Color });
+            ((VanguardInfoPopup)_last.Child).Text.Text = $"Pieces of dragon scroll: {dc?.PiecesOfDragonScroll}";
+
+
+        }
+
+        private void ShowElleonMarksPopup(object sender, MouseEventArgs e)
+        {
+            CreatePopup((FrameworkElement)sender);
+            var gauge = (sender as RectangleBarGauge);
+            var dc = gauge?.DataContext as Character;
+            ((VanguardInfoPopup)_last.Child).Icon.Children.Add(new Ellipse() { Fill = gauge?.Color });
+            ((VanguardInfoPopup)_last.Child).Text.Text = $"Elleon Marks: {dc?.ElleonMarks}";
+
+
+        }
+
+        private void CloseInfoPopup(object sender, MouseEventArgs e)
+        {
+            if (_last != null) _last.IsOpen = false;
+        }
+    }
+}
