57ad02c66a3fac5cfbcc5fecafa5c69be1ac64ca
Foglio1024 <foglio1024@gmail.com>
2018-12-10 19:20:01 +0100

    added DungeonEditWindow nad NewDungeonDialog windows for Dashboard

---
+++ TCC.Core/Windows/DungeonEditWindow.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Dragablz;
+using TCC.Controls.Dashboard;
+using TCC.Data;
+using TCC.ViewModels;
+
+namespace TCC.Windows
+{
+    /// <summary>
+    /// Logica di interazione per DungeonEditWindow.xaml
+    /// </summary>
+    public partial class DungeonEditWindow
+    {
+
+        public DungeonEditWindow()
+        {
+            InitializeComponent();
+            DataContext = WindowManager.Dashboard.VM;
+        }
+        public IEnumerable<ItemLevelTier> ItemLevelTiers => Utils.ListFromEnum<ItemLevelTier>();
+
+        private void OnCloseButtonClick(object sender, RoutedEventArgs e)
+        {
+            SessionManager.DungeonDatabase.SaveCustomDefs();
+            Close();
+
+        }
+        private void OnTitleBarMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            DragMove();
+        }
+
+        private void RemoveDungeon(object sender, RoutedEventArgs e)
+        {
+            var dng = (sender as FrameworkElement).DataContext as DungeonColumnViewModel;
+            dng.Dungeon.Show = false;
+        }
+
+        private void OnDungeonsOrderChanged(object sender, OrderChangedEventArgs e)
+        {
+            foreach (DungeonColumnViewModel dcvm in e.NewOrder)
+            {
+                dcvm.Dungeon.Index = e.NewOrder.ToList().IndexOf(dcvm);
+            }
+        }
+
+        private void AddDungeon(object sender, RoutedEventArgs e)
+        {
+            new NewDungeonDialog { Topmost = true, Owner = this}.ShowDialog();
+        }
+    }
+}
---
+++ TCC.Core/Windows/NewDungeonDialog.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using TCC.Data;
+using TCC.ViewModels;
+
+namespace TCC.Windows
+{
+    /// <summary>
+    /// Logica di interazione per NewDungeonDialog.xaml
+    /// </summary>
+    public partial class NewDungeonDialog : Window
+    {
+        public NewDungeonDialog()
+        {
+            InitializeComponent();
+        }
+
+        public ICollectionView Dungeons => Utils.InitView(d => d != null, SessionManager.DungeonDatabase.Dungeons.Values, new[]
+        {
+            new SortDescription(nameof(Dungeon.Name), ListSortDirection.Ascending)
+        });
+        private void AddDungeon(object sender, RoutedEventArgs e)
+        {
+            var dg = (sender as FrameworkElement).DataContext as Dungeon;
+            if (dg.Show) return;
+            dg.Show = true;
+            dg.Index = int.MaxValue;
+            //WindowManager.Dashboard.VM.ExNPC(nameof(DashboardViewModel.SortedColumns));
+        }
+
+        private void OnTitleBarMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            DragMove();
+
+        }
+
+        private void OnCloseButtonClick(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
