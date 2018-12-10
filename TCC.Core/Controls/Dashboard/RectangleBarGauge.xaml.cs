54067115cd90cb24e0fa29aaccec57ecd74011a5
Foglio1024 <foglio1024@gmail.com>
2018-12-10 19:25:23 +0100

    added skew and animations to rectanglebargauge

--- TCC.Core/Controls/Dashboard/RectangleBarGauge.xaml.cs
+++ TCC.Core/Controls/Dashboard/RectangleBarGauge.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System.Windows;
+﻿using System;
+using System.Windows;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 
 namespace TCC.Controls.Dashboard
 {
@@ -11,39 +13,62 @@ namespace TCC.Controls.Dashboard
         public RectangleBarGauge()
         {
             InitializeComponent();
+            Loaded += OnLoaded;
+        }
+
+        private void OnLoaded(object sender, RoutedEventArgs e)
+        {
+            var a = new DoubleAnimation(0, Factor, TimeSpan.FromMilliseconds(App.Random.Next(350, 650))) { EasingFunction = new QuadraticEase() };
+            GaugeValue.RenderTransform.BeginAnimation(ScaleTransform.ScaleXProperty, a);
         }
 
         public new double Width
         {
-            get { return (double)GetValue(WidthProperty); }
-            set { SetValue(WidthProperty, value); }
+            get => (double)GetValue(WidthProperty);
+            set => SetValue(WidthProperty, value);
         }
         public new static readonly DependencyProperty WidthProperty =
-            DependencyProperty.Register("Width", typeof(double), typeof(RectangleBarGauge), new PropertyMetadata(0d));
+            DependencyProperty.Register("Width", typeof(double), typeof(RectangleBarGauge), new PropertyMetadata(double.NaN));
 
         public new double Height
         {
-            get { return (double)GetValue(HeightProperty); }
-            set { SetValue(HeightProperty, value); }
+            get => (double)GetValue(HeightProperty);
+            set => SetValue(HeightProperty, value);
         }
         public new  static readonly DependencyProperty HeightProperty =
-            DependencyProperty.Register("Height", typeof(double), typeof(RectangleBarGauge), new PropertyMetadata(0d));
+            DependencyProperty.Register("Height", typeof(double), typeof(RectangleBarGauge), new PropertyMetadata(double.NaN));
 
         public Brush Color
         {
-            get { return (Brush)GetValue(ColorProperty); }
-            set { SetValue(ColorProperty, value); }
+            get => (Brush)GetValue(ColorProperty);
+            set => SetValue(ColorProperty, value);
         }
         public static readonly DependencyProperty ColorProperty =
             DependencyProperty.Register("Color", typeof(Brush), typeof(RectangleBarGauge));
 
         public double Factor
         {
-            get { return (double)GetValue(FactorProperty); }
-            set { SetValue(FactorProperty, value); }
+            get => (double)GetValue(FactorProperty);
+            set => SetValue(FactorProperty, value);
         }
         public static readonly DependencyProperty FactorProperty =
-            DependencyProperty.Register("Factor", typeof(double), typeof(RectangleBarGauge), new PropertyMetadata(0d));
+            DependencyProperty.Register("Factor", typeof(double), typeof(RectangleBarGauge), new PropertyMetadata(0d, OnFactorChanged));
+
+        private static void OnFactorChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+
+            var a = new DoubleAnimation((double)e.NewValue, TimeSpan.FromMilliseconds(250)) { EasingFunction = new QuadraticEase() };
+            (d as RectangleBarGauge).GaugeValue.RenderTransform.BeginAnimation(ScaleTransform.ScaleXProperty, a);
+        }
+
+        public double Skew
+        {
+            get => (double)GetValue(SkewProperty);
+            set => SetValue(SkewProperty, value);
+        }
+        public static readonly DependencyProperty SkewProperty =
+            DependencyProperty.Register("Skew", typeof(double), typeof(RectangleBarGauge), new PropertyMetadata(0d));
+
 
 
     }
