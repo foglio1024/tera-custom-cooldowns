4a737fe09ba43d248c499ab787e87e3b33a4da62
Foglio1024 <foglio1024@gmail.com>
2018-12-15 22:39:05 +0100

    added backing class for datatemplates dictionary

---
+++ TCC.Core/ResourceDictionaries/DataTemplates.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using TCC.Data.Pc;
+
+namespace TCC.ResourceDictionaries
+{
+    public partial class DataTemplates
+    {
+        private void OnCharacterNameMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            WindowManager.Dashboard.VM.SelectCharacter((sender as FrameworkElement)?.DataContext as Character);
+        }
+    }
+}
