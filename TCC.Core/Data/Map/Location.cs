73d62700a0c6811c037e0e98e87965e0060fa688
Foglio1024 <foglio1024@gmail.com>
2018-12-15 22:40:00 +0100

    refactor and use Location class

--- TCC.Core/Data/Map/Location.cs
+++ TCC.Core/Data/Map/Location.cs
@@ -1,11 +1,24 @@
 ﻿using System.Windows;
+using TCC.Data.Databases;
 
 namespace TCC.Data.Map
 {
     public class Location
     {
-        public uint World, Guard, Section;
-        public Point Position;
+
+        public uint World { get; }
+        public uint Guard { get; }
+        public uint Section { get; }
+
+        public string SectionName => SessionManager.MapDatabase.GetName(Guard, Section);
+
+        public Point Position { get; }
+
+        public Location()
+        {
+            Position = new Point(0,0);
+
+        }
         public Location(uint w, uint g, uint s, double x, double y)
         {
             World = w;
@@ -13,6 +26,23 @@ namespace TCC.Data.Map
             Section = s;
             Position = new Point(x, y);
         }
-        
+        public Location(uint w, uint g, uint s)
+        {
+            World = w;
+            Guard = g;
+            Section = s;
+            Position = new Point(0, 0);
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="value">WorldId_GuardId_SectionId</param>
+        public Location(string value)
+        {
+            var split = value.Split('_');
+            World = uint.Parse(split[0]);
+            Guard = uint.Parse(split[1]);
+            Section = uint.Parse(split[2]);
+        }
     }
 }
