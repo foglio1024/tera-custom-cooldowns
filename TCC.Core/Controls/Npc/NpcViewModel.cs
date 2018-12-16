3105736cda04f0662c35f607576b46b3dc9f0f7f
Foglio1024 <foglio1024@gmail.com>
2018-12-16 12:36:08 +0100

    make base class for boss and mob viewmodels

---
+++ TCC.Core/Controls/Npc/NpcViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Windows.Threading;
+using TCC.Data.NPCs;
+
+namespace TCC.Controls.NPCs
+{
+    public class NpcViewModel : TSPropertyChanged
+    {
+        public const uint Delay = 5000;
+        protected readonly DispatcherTimer _deleteTimer;
+
+        public event Action Disposed;
+        public event Action HpFactorChanged;
+
+        public NPC NPC { get; set; }
+
+        public NpcViewModel(NPC npc) 
+        {
+            NPC = npc;
+
+            _deleteTimer = new DispatcherTimer { Interval = TimeSpan.FromMilliseconds(Delay) };
+            _deleteTimer.Tick += (s, ev) =>
+            {
+                _deleteTimer.Stop();
+                InvokeDisposed();
+            };
+        }
+
+        protected void InvokeDisposed() => Disposed?.Invoke();
+        protected void InvokeHpChanged() => HpFactorChanged?.Invoke();
+    }
+}
