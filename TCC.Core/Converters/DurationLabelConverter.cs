0ba6cc9b053033567cd5e56eb999cabcf202e0fe
Foglio1024 <foglio1024@gmail.com>
2018-12-16 12:49:55 +0100

    fix converter

--- TCC.Core/Converters/DurationLabelConverter.cs
+++ TCC.Core/Converters/DurationLabelConverter.cs
@@ -8,8 +8,8 @@ namespace TCC.Converters
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            var val = 0;
-            if (value != null) val = System.Convert.ToInt32(value);
+            var val = 0U;
+            if (value != null) val = System.Convert.ToUInt32(value);
             var seconds = val / 1000;
             var minutes = seconds / 60;
             var hours = minutes / 60;
