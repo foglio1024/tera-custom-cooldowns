de63ccf79f8c584354044b9b3f1c032daa230a20
Foglio1024 <foglio1024@gmail.com>
2018-12-19 18:56:29 +0100

    fix SE detection

--- TCC.Core/TimeManager.cs
+++ TCC.Core/TimeManager.cs
@@ -22,6 +22,7 @@ namespace TCC
             {"RU", new TeraServerTimeInfo("Russian Standard Time", 6, DayOfWeek.Wednesday) },
             {"TW", new TeraServerTimeInfo("China Standard Time", 6, DayOfWeek.Wednesday) },
             {"JP", new TeraServerTimeInfo("Tokyo Standard Time", 6, DayOfWeek.Wednesday) },
+            {"SE", new TeraServerTimeInfo("Indochina Time", 6, DayOfWeek.Wednesday) },
             {"THA", new TeraServerTimeInfo("Indochina Time", 6, DayOfWeek.Wednesday) },
             {"KR", new TeraServerTimeInfo("Korea Standard Time", 6, DayOfWeek.Wednesday) },
             {"KR-PTS", new TeraServerTimeInfo("Korea Standard Time", 6, DayOfWeek.Wednesday) }
