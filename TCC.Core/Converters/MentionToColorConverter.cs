969531c6255edebc21083730c7cae8a80ed6ba88
Foglio1024 <foglio1024@gmail.com>
2018-12-27 13:57:47 +0100

    small layout fixes for chat messages, made multi-line messages more compact

--- TCC.Core/Converters/MentionToColorConverter.cs
+++ TCC.Core/Converters/MentionToColorConverter.cs
@@ -12,7 +12,7 @@ namespace TCC.Converters
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            return (bool?) value ?? false ? Brushes.Orange : Brushes.Black; //TODO: use trigger
+            return (bool?) value ?? false ? R.Brushes.GoldBrush : Brushes.Black; //TODO: use trigger
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
