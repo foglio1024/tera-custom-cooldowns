cbe6b2772975fdfc6adbed4bbee120dfb4c4599f
Foglio1024 <foglio1024@gmail.com>
2019-01-12 23:00:00 +0100

    added method to force experimental build download

--- TCC.Core/UpdateManager.cs
+++ TCC.Core/UpdateManager.cs
@@ -7,6 +7,8 @@ using System.Diagnostics;
 using System.Threading;
 using TCC.Data;
 using TCC.ViewModels;
+using System.Threading.Tasks;
+using TCC.Windows;
 
 namespace TCC
 {
@@ -15,7 +17,9 @@ namespace TCC
         private static System.Timers.Timer _checkTimer;
         private const string DatabasePath = "https://github.com/Foglio1024/tera-used-icons/archive/master.zip";
         private const string DatabaseVersion = "https://raw.githubusercontent.com/Foglio1024/tera-used-icons/master/current_version";
+
         private const string AppVersion = "https://raw.githubusercontent.com/Foglio1024/Tera-custom-cooldowns/master/version";
+        private const string AppVersionExperimental = "https://raw.githubusercontent.com/Foglio1024/Tera-custom-cooldowns/experimental/version";
         private const string BaseDatabaseDir = "tera-used-icons-master";
 
         public static void CheckDatabaseVersion()
@@ -184,6 +188,60 @@ namespace TCC
                     File.WriteAllText(Path.Combine(App.BasePath, "update-check-error.txt"), ex.Message + "\r\n" +
                              ex.StackTrace + "\r\n" + ex.Source + "\r\n" + ex + "\r\n" + ex.Data + "\r\n" + ex.InnerException +
                              "\r\n" + ex.TargetSite);
+                }
+            }
+        }
+        public async static void ForceDownloadExperimental()
+        {
+            using (var c = new WebClient())
+            {
+                c.Headers.Add(HttpRequestHeader.UserAgent, "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36");
+
+                try
+                {
+                    var st = c.OpenRead(AppVersionExperimental);
+                    if (st != null)
+                    {
+                        var sr = new StreamReader(st);
+                        var newVersionInfo = sr.ReadLine();
+                        var newVersionUrl = sr.ReadLine();
+
+                        if (newVersionInfo != null)
+                        {
+                            var v = Version.Parse(newVersionInfo);
+                            if (v > Assembly.GetExecutingAssembly().GetName().Version)
+                            {
+                                WindowManager.FloatingButton.NotifyExtended("TCC update manager", "Download started", NotificationType.Success, 3000);
+
+                                c.DownloadFile(new Uri(newVersionUrl), "update.zip");
+
+                                WindowManager.FloatingButton.NotifyExtended("TCC update manager", "Extracting zip", NotificationType.Success, 3000);
+                                if (Directory.Exists(Path.Combine(App.BasePath, "tmp"))) Directory.Delete(Path.Combine(App.BasePath, "tmp"), true);
+                                ZipFile.ExtractToDirectory("update.zip", Path.Combine(App.BasePath, "tmp"));
+
+                                WindowManager.FloatingButton.NotifyExtended("TCC update manager", "Moving files", NotificationType.Success, 2000);
+
+
+                                File.Move(Path.Combine(App.BasePath, "tmp/TCCupdater.exe"), Path.Combine(App.BasePath, "TCCupdater.exe"));
+                                WindowManager.FloatingButton.NotifyExtended("TCC update manager", "Starting updater", NotificationType.Success, 1000);
+
+
+                                 await Task.Delay(1000).ContinueWith(t => Process.Start(Path.GetDirectoryName(typeof(App).Assembly.Location) + "/TCCupdater.exe", "update"));
+                                Environment.Exit(0);
+                            }
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    File.WriteAllText(Path.Combine(App.BasePath, "update-check-error.txt"), ex.Message + "\r\n" +
+                             ex.StackTrace + "\r\n" + ex.Source + "\r\n" + ex + "\r\n" + ex.Data + "\r\n" + ex.InnerException +
+                             "\r\n" + ex.TargetSite);
+                    //MessageBox.Show("Error while checking updates. More info in update-check-error.txt", "TCC", MessageBoxButton.OK, MessageBoxImage.Error);
+                    if (TccMessageBox.Show("Error while checking updates. Try again?", MessageBoxType.ConfirmationWithYesNo) == System.Windows.MessageBoxResult.Yes)
+                    {
+                        ForceDownloadExperimental();
+                    }
                 }
             }
         }
