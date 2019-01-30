d30b57c9408ef4c14199ead0a6281b08ba09a4f1
Foglio1024 <foglio1024@gmail.com>
2019-01-30 19:38:56 +0100

    don't crash on invalid S_INVEN

--- TCC.Core/Parsing/Messages/S_INVEN.cs
+++ TCC.Core/Parsing/Messages/S_INVEN.cs
@@ -32,6 +32,7 @@ namespace TCC.Parsing.Messages
     }
     public class S_INVEN : ParsedMessage
     {
+        public bool Failed { get; }
         public bool IsOpen { get; }
         public bool First { get; }
         public bool More { get; }
@@ -45,58 +46,66 @@ namespace TCC.Parsing.Messages
             GearItems = new Dictionary<uint, ParsedGearItem>();
             // don't parse if standard noctenium is on
             if (SessionManager.CurrentPlayer.InfBuffs.Any(x => AbnormalityDatabase.NoctIds.Contains(x.Abnormality.Id))) return;
-            var count = reader.ReadUInt16();
-            var offset = reader.ReadUInt16();
-            reader.Skip(16);
-            IsOpen = reader.ReadBoolean();
-            First = reader.ReadBoolean();
-            More = reader.ReadBoolean();
-            if (offset == 0) return;
-            reader.BaseStream.Position = offset - 4;
-            for (int i = 0; i < count; i++)
+            try
             {
-                var curr = reader.ReadUInt16();
-                var next = reader.ReadUInt16();
-                reader.Skip(2); // passivity array count
-                reader.Skip(2); // passivity array offset
-                reader.Skip(2); // custom string offset
-                var itemId = reader.ReadUInt32();
-                reader.Skip(8); //dbid
-                reader.Skip(8); //ownerId (playerId)
-                var slot = reader.ReadUInt32();
-                reader.Skip(4); //storage type (0)
-                var amount = reader.ReadInt32();
-                var enchant = reader.ReadInt32();
-                reader.Skip(4); //durability
-                reader.Skip(1); //soulbound
-                reader.Skip(4 * 5); //crystals
-                reader.Skip(4); //passivity set
-                reader.Skip(4); //extra passivity set
-                reader.Skip(4); //remodel
-                reader.Skip(4); //dye
-                reader.Skip(4); //dye sec remaining
-                reader.Skip(8); //dye date
-                reader.Skip(8); //dye expire date
-                reader.Skip(1); //masterwork
-                reader.Skip(4); //enigma
-                reader.Skip(4); //times brokered
-                reader.Skip(4); //enchant advantage
-                reader.Skip(4); //enchant bonus
-                reader.Skip(4); //enchant bonus max plus
-                reader.Skip(8); //bound to player (playerId)
-                reader.Skip(1); //awakened
-                reader.Skip(4); //liberation count
-                reader.Skip(4); //feedstock
-                reader.Skip(4); //bound to item
-                reader.Skip(1); //has etching
-                reader.Skip(1); //pc bang
-                var exp = reader.ReadInt64();
+                var count = reader.ReadUInt16();
+                var offset = reader.ReadUInt16();
+                reader.Skip(16);
+                IsOpen = reader.ReadBoolean();
+                First = reader.ReadBoolean();
+                More = reader.ReadBoolean();
+                if (offset == 0) return;
+                reader.BaseStream.Position = offset - 4;
+                for (int i = 0; i < count; i++)
+                {
+                    var curr = reader.ReadUInt16();
+                    var next = reader.ReadUInt16();
+                    reader.Skip(2); // passivity array count
+                    reader.Skip(2); // passivity array offset
+                    reader.Skip(2); // custom string offset
+                    var itemId = reader.ReadUInt32();
+                    reader.Skip(8); //dbid
+                    reader.Skip(8); //ownerId (playerId)
+                    var slot = reader.ReadUInt32();
+                    reader.Skip(4); //storage type (0)
+                    var amount = reader.ReadInt32();
+                    var enchant = reader.ReadInt32();
+                    reader.Skip(4); //durability
+                    reader.Skip(1); //soulbound
+                    reader.Skip(4 * 5); //crystals
+                    reader.Skip(4); //passivity set
+                    reader.Skip(4); //extra passivity set
+                    reader.Skip(4); //remodel
+                    reader.Skip(4); //dye
+                    reader.Skip(4); //dye sec remaining
+                    reader.Skip(8); //dye date
+                    reader.Skip(8); //dye expire date
+                    reader.Skip(1); //masterwork
+                    reader.Skip(4); //enigma
+                    reader.Skip(4); //times brokered
+                    reader.Skip(4); //enchant advantage
+                    reader.Skip(4); //enchant bonus
+                    reader.Skip(4); //enchant bonus max plus
+                    reader.Skip(8); //bound to player (playerId)
+                    reader.Skip(1); //awakened
+                    reader.Skip(4); //liberation count
+                    reader.Skip(4); //feedstock
+                    reader.Skip(4); //bound to item
+                    reader.Skip(1); //has etching
+                    reader.Skip(1); //pc bang
+                    var exp = reader.ReadInt64();
 
-                if (slot > 39) Items[slot] = new ItemAmount(itemId, amount);
-                else    GearItems[itemId] = new ParsedGearItem(itemId, enchant, exp);
-                
-                if (next == 0) break;
-                reader.BaseStream.Position = next - 4;
+                    if (slot > 39) Items[slot] = new ItemAmount(itemId, amount);
+                    else GearItems[itemId] = new ParsedGearItem(itemId, enchant, exp);
+
+                    if (next == 0) break;
+                    reader.BaseStream.Position = next - 4;
+                }
+            }
+            catch
+            {
+                Log.F("Failed to parse S_INVEN");
+                Failed = true;
             }
         }
     }
