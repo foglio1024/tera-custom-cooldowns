e7faab19a2ca05e00c502c789872d6761b5ba3b6
Foglio1024 <foglio1024@gmail.com>
2019-02-12 18:43:37 +0100

    don't crash on wrong DungeonCooltimeList

--- TCC.Core/Parsing/Messages/S_DUNGEON_COOL_TIME_LIST.cs
+++ TCC.Core/Parsing/Messages/S_DUNGEON_COOL_TIME_LIST.cs
@@ -9,17 +9,26 @@ namespace TCC.Parsing.Messages
         public readonly Dictionary<uint, short> DungeonCooldowns;
         public S_DUNGEON_COOL_TIME_LIST(TeraMessageReader reader) : base(reader)
         {
-            var count = reader.ReadUInt16();
-            reader.Skip(2); //var offset = reader.ReadUInt16();
-            reader.Skip(4);
             DungeonCooldowns = new Dictionary<uint, short>();
-            for (var i = 0; i < count; i++)
+
+            try
             {
+                var count = reader.ReadUInt16();
+                reader.Skip(2); //var offset = reader.ReadUInt16();
                 reader.Skip(4);
-                var id = reader.ReadUInt32();
-                reader.Skip(8);
-                var entries = reader.ReadInt16();
-                DungeonCooldowns.Add(id, entries);
+                for (var i = 0; i < count; i++)
+                {
+                    reader.Skip(4);
+                    var id = reader.ReadUInt32();
+                    reader.Skip(8);
+                    var entries = reader.ReadInt16();
+                    DungeonCooldowns.Add(id, entries);
+                }
+
+            }
+            catch (System.Exception)
+            {
+                Log.F($"[S_DUNGEON_COOL_TIME_LIST] Failed to parse packet");
             }
         }
     }
