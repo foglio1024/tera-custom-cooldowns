2d88d9fea5584fec3f93bacbab95a4c6dbd02485
Foglio1024 <foglio1024@gmail.com>
2019-03-12 19:16:26 +0100

    moved S_CREATURE_LIFE to main messages folder

---
+++ TCC.Core/Parsing/Messages/S_CREATURE_LIFE.cs
@@ -0,0 +1,18 @@
+﻿using TCC.TeraCommon.Game.Messages;
+using TCC.TeraCommon.Game.Services;
+
+namespace TCC.Parsing.Messages
+{
+    public class S_CREATURE_LIFE : ParsedMessage
+    {
+        internal S_CREATURE_LIFE(TeraMessageReader reader) : base(reader)
+        {
+            Target = reader.ReadUInt64();
+            var pos = reader.ReadVector3f();
+            Alive = reader.ReadBoolean(); // 0=dead;1=alive
+        }
+
+        public ulong Target { get; }
+        public bool Alive { get; }
+    }
+}
