817f02089fcb826d4fa8994333ed27731dd83df8
Foglio1024 <foglio1024@gmail.com>
2019-03-30 10:53:49 +0100

    fix S_DUNGEON_COOL_TIME_LIST again

--- TCC.Core/Parsing/Messages/S_DUNGEON_COOL_TIME_LIST.cs
+++ TCC.Core/Parsing/Messages/S_DUNGEON_COOL_TIME_LIST.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using TCC.TeraCommon.Game.Messages;
 using TCC.TeraCommon.Game.Services;
 
@@ -13,21 +14,23 @@ namespace TCC.Parsing.Messages
 
             try
             {
+                reader.RepositionAt(4);
                 var count = reader.ReadUInt16();
                 reader.Skip(2); //var offset = reader.ReadUInt16();
                 reader.Skip(4);
                 for (var i = 0; i < count; i++)
                 {
-                    reader.Skip(2);
+                    var curr = reader.ReadUInt16();
                     var next = reader.ReadUInt16();
                     var id = reader.ReadUInt32();
-                    reader.Skip(10);
-                    var entries = reader.ReadInt16();
-                    DungeonCooldowns.Add(id, entries);
+                    reader.Skip(8);
+                    var daily = reader.ReadInt16();
+                    var weekly = reader.ReadInt16();
+                    var entries = (short) (daily == -1 ? weekly == -1 ? -1 : weekly : daily);
+                    if(entries != -1) DungeonCooldowns[id] = entries;
                     if (next == 0) return;
                     reader.RepositionAt(next);
                 }
-
             }
             catch (System.Exception)
             {
