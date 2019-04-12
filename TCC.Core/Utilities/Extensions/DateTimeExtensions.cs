c1cc5fac4949e24a9716b453675d7e60a16afe46
Foglio1024 <foglio1024@gmail.com>
2019-04-12 15:47:20 +0200

    added datetimeextensions

---
+++ TCC.Core/Utilities/Extensions/DateTimeExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace TCC.Utilities.Extensions
+{
+    public static class DateTimeExtensions
+    {
+        public static long ToEpoch(this DateTime dt)
+        {
+            var t = dt - new DateTime(1970, 1, 1);
+            return (long)t.TotalSeconds;
+        }
+    }
+}
