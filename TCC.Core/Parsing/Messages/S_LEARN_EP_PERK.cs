a63142f159002bb184881ce1ad8ee7c166e1310f
Foglio1024 <foglio1024@gmail.com>
2019-04-14 15:48:19 +0200

    move classes to own files

--- TCC.Core/Parsing/Messages/S_LEARN_EP_PERK.cs
+++ TCC.Core/Parsing/Messages/S_LEARN_EP_PERK.cs
@@ -4,22 +4,17 @@ using TCC.TeraCommon.Game.Services;
 
 namespace TCC.Parsing.Messages
 {
-    public class S_LOAD_EP_INFO : ParsedMessage
+    public class S_LEARN_EP_PERK : ParsedMessage
     {
         public Dictionary<uint, uint> Perks;
-        public S_LOAD_EP_INFO(TeraMessageReader r) : base(r)
+
+        public S_LEARN_EP_PERK(TeraMessageReader r) : base(r)
         {
             var perksCount = r.ReadInt16();
             var perksOffset = r.ReadInt16();
 
-            var lvl = r.ReadUInt32();
-            var exp = r.ReadUInt64();
-            var totPoints = r.ReadUInt32();
+            var success = r.ReadBoolean();
             var usedPoints = r.ReadUInt32();
-            var dailyExp = r.ReadUInt32();
-            var dailyExpMax = r.ReadUInt32();
-            var prevLevel = r.ReadUInt32();
-            var prevTotalPoints = r.ReadUInt32();
 
             Perks = new Dictionary<uint, uint>();
             if (perksCount == 0) return;
@@ -34,6 +29,7 @@ namespace TCC.Parsing.Messages
                 if (next == 0) break;
                 r.RepositionAt(next);
             }
+
         }
     }
 }
