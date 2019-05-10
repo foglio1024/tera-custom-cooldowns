866f85076e0901cff13399f49838c26d27e1e80a
Foglio1024 <foglio1024@gmail.com>
2019-05-10 12:55:07 +0200

    Size extension method

---
+++ TCC.Core/Utilities/Extensions/SizeExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace TCC.Utilities.Extensions
+{
+    public static class SizeExtensions
+    {
+        public static bool IsEqual(this Size s, Size other)
+        {
+            return s.Width == other.Width &&
+                   s.Height == other.Height;
+        }
+    }
+}
