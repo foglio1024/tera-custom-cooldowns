45f5986c3b98e839a8426827c3393fc7c054fb8c
Foglio1024 <foglio1024@gmail.com>
2019-05-31 12:09:25 +0200

    init proxyinterface only once

--- TCC.Core/Interop/Proxy/ProxyInterface.cs
+++ TCC.Core/Interop/Proxy/ProxyInterface.cs
@@ -5,6 +5,7 @@ namespace TCC.Interop.Proxy
 {
     public class ProxyInterface
     {
+        private bool _initialized = false;
         private static ProxyInterface _instance;
         public static ProxyInterface Instance => (_instance ?? new ProxyInterface());
         private Server Server { get; }
@@ -26,7 +27,9 @@ namespace TCC.Interop.Proxy
 
         public async Task Init()
         {
+            if (_initialized) return;
             if (!SettingsHolder.EnableProxy) return;
+            _initialized = true;
             IsStubAvailable = await Stub.PingStub();
             if (!IsStubAvailable) return;
             Stub.Initialize();
