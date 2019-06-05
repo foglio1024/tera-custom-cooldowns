8af6300ee47d668c1bf21b5eb89aba61953950e1
Foglio1024 <foglio1024@gmail.com>
2019-06-05 18:39:35 +0200

    unversion release folder again

--- release/index.js
+++
@@ -1,6 +0,0 @@
-const { spawn } = require('child_process');
-module.exports = function TCC(m)
-{
-    const tcc = spawn('TCC.exe', ["--toolbox"]);
-    tcc.on('exit', (code, signal) => { "TCC exited." });
-}
