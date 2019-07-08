e79182635967251e1946f0d9e2106a70abc23278
Foglio1024 <foglio1024@gmail.com>
2019-07-08 15:06:31 +0200

    ignore stdio in js module

--- TCC.Core/Module/index.js
+++ TCC.Core/Module/index.js
@@ -3,6 +3,6 @@ const path = require('path');
 
 module.exports = function TCC(m)
 {
-    const tcc = spawn(path.join(__dirname, 'TCC.exe'), ["--toolbox"]);
+    const tcc = spawn(path.join(__dirname, 'TCC.exe'), ["--toolbox"], {stdio : "ignore"});
     tcc.on('exit', (code, signal) => { "TCC exited." });
 }
