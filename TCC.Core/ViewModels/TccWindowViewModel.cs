d5cd6ceaafd9ee9c1c41ea54a6445028194077bc
Foglio1024 <foglio1024@gmail.com>
2019-07-10 17:15:25 +0200

    vm base class improvements

--- TCC.Core/ViewModels/TccWindowViewModel.cs
+++ TCC.Core/ViewModels/TccWindowViewModel.cs
@@ -1,16 +1,34 @@
 ﻿using System.Windows.Threading;
 using TCC.Parsing;
+using TCC.Settings;
 
 namespace TCC.ViewModels
 {
     public class TccWindowViewModel : TSPropertyChanged
     {
         protected virtual void InstallHooks() { }
+        protected virtual void RemoveHooks() { }
 
-        protected TccWindowViewModel()
+        protected virtual void OnEnabledChanged(bool enabled)
+        {
+            if (enabled) InstallHooks();
+            else RemoveHooks();
+        }
+
+        private TccWindowViewModel()
+        {
+            Dispatcher = Dispatcher.CurrentDispatcher;
+        }
+
+        protected TccWindowViewModel(WindowSettings settings) : this()
         {
             App.BaseDispatcher.Invoke(() =>
             {
+                if (settings != null)
+                {
+                    settings.EnabledChanged += OnEnabledChanged;
+                    if (!settings.Enabled) return;
+                }
                 PacketAnalyzer.ProcessorReady += InstallHooks;
             });
         }
