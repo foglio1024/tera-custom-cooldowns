9788f00a6bf11cc055f7e8255d4c39dd1a076c26
Foglio1024 <foglio1024@gmail.com>
2019-08-04 14:04:52 +0200

    don't prompt the user about db update

--- TCC.Core/Session.cs
+++ TCC.Core/Session.cs
@@ -134,11 +134,14 @@ namespace TCC
             DB.CheckVersion();
             if (!DB.IsUpToDate)
             {
-                var res = TccMessageBox.Show($"Some database files may be missing or out of date.\nDo you want to update them?", MessageBoxType.ConfirmationWithYesNo);
-                if (res == MessageBoxResult.Yes)
-                {
-                    DB.DownloadOutdatedDatabases();
-                }
+                WindowManager.FloatingButton.NotifyExtended("TCC", $"Some database files are out of date, updating... Contact the deveolper if you see this message at every login.", NotificationType.Warning, 5000);
+                ChatWindowManager.Instance.AddTccMessage($"Some database files are out of date, updating...");
+
+                //var res = TccMessageBox.Show($"Some database files may be missing or out of date.\nDo you want to update them?", MessageBoxType.ConfirmationWithYesNo);
+                //if (res == MessageBoxResult.Yes)
+                //{
+                DB.DownloadOutdatedDatabases();
+                //}
             }
             if (!DB.Exists)
             {
