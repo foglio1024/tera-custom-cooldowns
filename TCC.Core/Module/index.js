004988333a454d999a7402c2f1d7a1329577c49a
Foglio1024 <foglio1024@gmail.com>
2019-09-08 17:56:35 +0200

    don't start TCC in TTB if not admin

--- TCC.Core/Module/index.js
+++ TCC.Core/Module/index.js
@@ -3,6 +3,11 @@ const path = require('path');
 
 module.exports = function TCC(m)
 {
-    const tcc = spawn(path.join(__dirname, 'TCC.exe'), ["--toolbox"], {stdio : "ignore"});
+    if (!global.TeraProxy.IsAdmin)
+    {
+        m.error("TCC requires Toolbox to be executed as admin.");
+        return;
+    }
+    const tcc = spawn(path.join(__dirname, 'TCC.exe'), ["--toolbox"], { stdio: "ignore" });
     tcc.on('exit', (code, signal) => { "TCC exited." });
 }
