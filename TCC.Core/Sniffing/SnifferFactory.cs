b116f3fd0c49edbe40c7986494194a067785f776
Foglio1024 <foglio1024@gmail.com>
2019-09-19 20:04:29 +0200

    init sniffer via factory

---
+++ TCC.Core/Sniffing/SnifferFactory.cs
@@ -0,0 +1,21 @@
+﻿using TCC.Data;
+using TCC.Parsing;
+using TCC.TeraCommon.Sniffing;
+
+namespace TCC.Sniffing
+{
+    public static class SnifferFactory
+    {
+        public static ITeraSniffer Create()
+        {
+            switch (App.Settings.CaptureMode)
+            {
+                case CaptureMode.Npcap when !App.ToolboxMode:
+                case CaptureMode.RawSockets when !App.ToolboxMode:
+                    return new TeraSniffer();
+                default:
+                    return new ToolboxSniffer();
+            }
+        }
+    }
+}
