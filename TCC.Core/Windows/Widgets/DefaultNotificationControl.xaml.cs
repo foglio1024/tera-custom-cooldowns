152c463f0224dee15c15f4c3a1b32df05a86d2de
Foglio1024 <foglio1024@gmail.com>
2019-09-28 21:43:45 +0200

    add new controls for NotificationArea

---
+++ TCC.Core/Windows/Widgets/DefaultNotificationControl.xaml.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using FoglioUtils;
+using TCC.Controls;
+
+namespace TCC.Windows.Widgets
+{
+    public partial class DefaultNotificationControl
+    {
+        private readonly DoubleAnimation _arcAnimation;
+        private readonly DoubleAnimation _slideInAnimation;
+        private readonly DoubleAnimation _slideOutAnimation;
+        private readonly DoubleAnimation _fadeInAnimation;
+        private readonly DoubleAnimation _fadeOutAnimation;
+        private readonly DoubleAnimation _shrinkAnimation;
+        private NotificationInfo _dc;
+        public DefaultNotificationControl()
+        {
+            Loaded += OnLoaded;
+            _arcAnimation = AnimationFactory.CreateDoubleAnimation(4000, 0, 359.9, completed: OnTimeExpired);
+
+            _slideInAnimation = AnimationFactory.CreateDoubleAnimation(150, 0, -100, easing: true, completed: OnSlideInCompleted);
+            _fadeInAnimation = AnimationFactory.CreateDoubleAnimation(150, 1,0);
+
+            _slideOutAnimation = AnimationFactory.CreateDoubleAnimation(150, -100, 0, easing: true);
+            _fadeOutAnimation = AnimationFactory.CreateDoubleAnimation(150, 0, completed: OnFadeFinished);
+            _shrinkAnimation = AnimationFactory.CreateDoubleAnimation(150, 0, 1, easing: true, completed: OnShrinkFinished);
+
+            Opacity = 0;
+            InitializeComponent();
+        }
+
+        private void OnSlideInCompleted(object sender, EventArgs e)
+        {
+            _arcAnimation.Duration = TimeSpan.FromMilliseconds(_dc.Duration);
+            TimeArc.BeginAnimation(Arc.EndAngleProperty, _arcAnimation);
+        }
+
+        private void OnShrinkFinished(object sender, EventArgs e)
+        {
+            WindowManager.ViewModels.NotificationArea.DeleteNotification(_dc);
+        }
+
+        private void OnFadeFinished(object sender, EventArgs e)
+        {
+            LayoutTransform.BeginAnimation(ScaleTransform.ScaleYProperty, _shrinkAnimation);
+        }
+
+        private void OnTimeExpired(object sender, EventArgs e)
+        {
+            BeginAnimation(OpacityProperty, _fadeOutAnimation);
+            RenderTransform.BeginAnimation(TranslateTransform.XProperty, _slideOutAnimation);
+        }
+
+        private void OnLoaded(object sender, RoutedEventArgs e)
+        {
+            _dc = (NotificationInfo)DataContext;
+            BeginAnimation(OpacityProperty, _fadeInAnimation);
+            RenderTransform.BeginAnimation(TranslateTransform.XProperty, _slideInAnimation);
+        }
+    }
+}
---
+++ TCC.Core/Windows/Widgets/NotificationAreaWindow.xaml.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using TCC.Data;
+using TCC.Settings;
+using TCC.ViewModels;
+
+namespace TCC.Windows.Widgets
+{
+    public class NotificationInfo
+    {
+        public string Title { get; }
+        public string Message { get; }
+        public NotificationType NotificationType { get; }
+        public uint Duration { get; }
+        public string TimeStamp { get; }
+        public NotificationInfo(string title, string message, NotificationType type, uint duration)
+        {
+            Title = title;
+            Message = message;
+            NotificationType = type;
+            Duration = duration;
+            TimeStamp = DateTime.Now.ToString("HH:mm:ss");
+        }
+    }
+    public class NotificationAreaViewModel : TccWindowViewModel
+    {
+        private readonly ConcurrentQueue<NotificationInfo> _queue;
+        public SynchronizedObservableCollection<NotificationInfo /*TODO: use VM if needed*/> Notifications { get; }
+        public NotificationAreaViewModel(WindowSettings settings) : base(settings)
+        {
+            _queue = new ConcurrentQueue<NotificationInfo>();
+            Notifications = new SynchronizedObservableCollection<NotificationInfo>(Dispatcher);
+        }
+
+        private void CheckShow()
+        {
+            while (Notifications.Count < ((NotificationAreaSettings)Settings).MaxNotifications)
+            {
+                if (_queue.IsEmpty) break;
+                if (!_queue.TryDequeue(out var next)) continue;
+                if (!Pass(next)) continue;
+                Console.WriteLine($"Adding {next.Message}");
+                Notifications.Add(next);
+            }
+        }
+        private bool Pass(NotificationInfo info)
+        {
+            return Notifications.ToSyncList().All(n => n.Message != info.Message);
+        }
+        public void Enqueue(string title, string message, NotificationType type, uint duration = 4000U)
+        {
+            _queue.Enqueue(new NotificationInfo(title, message, type, duration));
+            CheckShow();
+        }
+
+        public void DeleteNotification(NotificationInfo dc)
+        {
+            Notifications.Remove(dc);
+            CheckShow();
+        }
+    }
+    public partial class NotificationAreaWindow
+    {
+        public NotificationAreaWindow(NotificationAreaViewModel vm)
+        {
+            DataContext = vm;
+            InitializeComponent();
+            ButtonsRef = Buttons;
+            MainContent = WindowContent;
+            Init(vm.Settings);
+            MainContent.Opacity = 1;
+        }
+    }
+}
