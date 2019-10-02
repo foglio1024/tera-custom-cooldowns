5c0f9c7fa92ed55a80d71bbb3a7644af9a34e385
Foglio1024 <foglio1024@gmail.com>
2019-10-02 18:28:20 +0200

    move types to own files

---
+++ TCC.Core/Windows/Widgets/NotificationAreaViewModel.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Linq;
+using System.Windows.Threading;
+using TCC.Data;
+using TCC.Settings;
+using TCC.ViewModels;
+
+namespace TCC.Windows.Widgets
+{
+    public class NotificationAreaViewModel : TccWindowViewModel
+    {
+        public event Action NotificationAddedEvent;
+        private readonly ConcurrentQueue<NotificationInfo> _queue;
+        public SynchronizedObservableCollection<NotificationInfo /*TODO: use VM if needed*/> Notifications { get; }
+        public NotificationInfo Current { get; set; }
+        public NotificationAreaViewModel(WindowSettings settings) : base(settings)
+        {
+            _queue = new ConcurrentQueue<NotificationInfo>();
+            Notifications = new SynchronizedObservableCollection<NotificationInfo>(Dispatcher);
+        }
+
+        private void CheckShow()
+        {
+            while (Notifications.Count < ((NotificationAreaSettings)Settings).MaxNotifications)
+            {
+                if (_queue.IsEmpty) break;
+                if (!_queue.TryDequeue(out var next)) continue;
+                if (!Pass(next)) continue;
+                Notifications.Add(next);
+                Current = Notifications[0];
+                N(nameof(Current));
+                NotificationAddedEvent?.Invoke();
+            }
+        }
+        private bool Pass(NotificationInfo info)
+        {
+            return Notifications.ToSyncList().All(n => n.Message != info.Message);
+        }
+        public void Enqueue(string title, string message, NotificationType type, uint duration = 4000U)
+        {
+            _queue.Enqueue(new NotificationInfo(title, message, type, duration));
+            CheckShow();
+        }
+
+        public void DeleteNotification(NotificationInfo dc)
+        {
+            Dispatcher.BeginInvoke(new Action(() =>
+            {
+                Notifications.Remove(dc);
+                CheckShow();
+            }), DispatcherPriority.Background);
+        }
+    }
+}
---
+++ TCC.Core/Windows/Widgets/NotificationInfo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using TCC.Data;
+
+namespace TCC.Windows.Widgets
+{
+    public class NotificationInfo
+    {
+        public string Title { get; }
+        public string Message { get; }
+        public NotificationType NotificationType { get; }
+        public uint Duration { get; }
+        public string TimeStamp { get; }
+        public NotificationInfo(string title, string message, NotificationType type, uint duration)
+        {
+            Title = title;
+            Message = message;
+            NotificationType = type;
+            Duration = duration;
+            TimeStamp = DateTime.Now.ToString("HH:mm:ss");
+        }
+    }
+}
