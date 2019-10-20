3e9eb864d30f3c4b568fc466405ffdfc6552ad71
Foglio1024 <foglio1024@gmail.com>
2019-10-20 16:56:28 +0200

    v1.4.43-e

--- TCC.Core/Properties/AssemblyInfo.cs
+++ TCC.Core/Properties/AssemblyInfo.cs
@@ -13,5 +13,5 @@ using System.Windows;
 [assembly: ComVisible(false)]
 [assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
 
-[assembly: AssemblyVersion("1.4.42.0")]
-[assembly: AssemblyFileVersion("1.4.42.0")]
+[assembly: AssemblyVersion("1.4.43.0")]
+[assembly: AssemblyFileVersion("1.4.43.0")]
