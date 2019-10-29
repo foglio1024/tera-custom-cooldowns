9c02d0dfc821986c28427323abea0bf00fde6544
Foglio1024 <foglio1024@gmail.com>
2019-10-29 15:45:22 +0100

    refactor app.xaml.cs

--- TCC.Core/App.xaml.cs
+++ TCC.Core/App.xaml.cs
@@ -6,7 +6,6 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
-using System.Reflection;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
@@ -14,14 +13,14 @@ using System.Windows.Interop;
 using System.Windows.Media;
 using System.Windows.Threading;
 using TCC.Data;
+using TCC.Exceptions;
 using TCC.Interop.Proxy;
-using TCC.Loader;
 using TCC.Parsing;
 using TCC.Settings;
+using TCC.Utilities;
 using TCC.Utils;
 using TCC.ViewModels;
 using TCC.Windows;
-using MessageBoxImage = TCC.Data.MessageBoxImage;
 
 namespace TCC
 {
@@ -31,55 +30,62 @@ namespace TCC
         private static bool _running;
         private static Mutex _mutex;
 
-        public static bool Experimental { get; } = true;
+        public static bool Beta { get; } = true;
 
-        public static Dispatcher BaseDispatcher { get; private set; }
         /// <summary>
-        /// Version in the "TCC vX.Y.Z" format.
+        ///     Version in the "TCC vX.Y.Z-e" format.
         /// </summary>
-        public static string AppVersion { get; private set; } //"TCC vX.Y.Z"
+        public static string AppVersion { get; private set; }
+
         /// <summary>
-        /// 'TCC.exe' folder
+        ///     'TCC.exe' folder
         /// </summary>
         public static string BasePath { get; } = Path.GetDirectoryName(typeof(App).Assembly.Location);
+
         /// <summary>
-        /// 'TCC/resources' folder
+        ///     'TCC/resources' folder
         /// </summary>
         public static string ResourcesPath { get; } = Path.Combine(BasePath, "resources");
+
         /// <summary>
-        /// 'TCC/resources/data' folder
+        ///     'TCC/resources/data' folder
         /// </summary>
         public static string DataPath { get; } = Path.Combine(ResourcesPath, "data");
+
         public static bool Loading { get; private set; }
         public static bool ToolboxMode { get; private set; }
-        public static Random Random { get; } = new Random(DateTime.Now.DayOfYear + DateTime.Now.Year + DateTime.Now.Minute + DateTime.Now.Second + DateTime.Now.Millisecond);
+        public static Random Random { get; } = new Random(DateTime.Now.DayOfYear + DateTime.Now.Year +
+                                                          DateTime.Now.Minute + DateTime.Now.Second +
+                                                          DateTime.Now.Millisecond);
         public static TccSplashScreen SplashScreen { get; set; }
         public static SettingsContainer Settings { get; set; }
-
 
         private async void OnStartup(object sender, StartupEventArgs e)
         {
             _running = true;
+            AppVersion = TccUtils.GetTccVersion();
             Log.Config(Path.Combine(BasePath, "logs"), AppVersion); // NLog when?
             ParseStartupArgs(e.Args.ToList());
             BaseDispatcher = Dispatcher.CurrentDispatcher;
             BaseDispatcher.Thread.Name = "Main";
-            TccMessageBox.CreateAsync(); 
-
+            TccMessageBox.CreateAsync();
             if (IsAlreadyRunning())
             {
                 if (!ToolboxMode) TccMessageBox.Show("Another instance of TCC is already running. Shutting down.", MessageBoxType.Information);
                 Current.Shutdown();
                 return;
             }
+            
+            if (!Debugger.IsAttached) AppDomain.CurrentDomain.UnhandledException += GlobalExceptionHandler.HandleGlobalException;
 
             Loading = true;
-            var v = Assembly.GetExecutingAssembly().GetName().Version;
-            AppVersion = $"TCC v{v.Major}.{v.Minor}.{v.Build}{(Experimental ? "-e" : "")}";
+            await Setup();
+            Loading = false;
+        }
+
+        private static async Task Setup()
+        {
             TccSplashScreen.InitOnNewThread();
-#if RELEASE
-            AppDomain.CurrentDomain.UnhandledException += GlobalExceptionHandler.HandleGlobalException;
-#endif
 
             if (!ToolboxMode)
             {
@@ -89,99 +95,71 @@ namespace TCC
                 await UpdateManager.CheckAppVersion();
             }
 
+            // ----------------------------
             SplashScreen.VM.Progress = 10;
-            SplashScreen.VM.BottomText = "Checking for icon database updates...";
-            await UpdateManager.CheckIconsVersion();
-
-            SplashScreen.VM.Progress = 20;
             SplashScreen.VM.BottomText = "Loading settings...";
-
-            WindowManager.VisibilityManager = new VisibilityManager();
-
             SettingsContainer.Load();
 
-            SplashScreen.VM.Progress = 30;
-
+            SplashScreen.VM.Progress = 20;
             Process.GetCurrentProcess().PriorityClass = Settings.HighPriority
                 ? ProcessPriorityClass.High
                 : ProcessPriorityClass.Normal;
             if (Settings.ForceSoftwareRendering) RenderOptions.ProcessRenderMode = RenderMode.SoftwareOnly;
 
+            // ----------------------------
+            SplashScreen.VM.Progress = 30;
             SplashScreen.VM.BottomText = "Pre-loading databases...";
             UpdateManager.CheckDatabaseHash();
-
-            SplashScreen.VM.Progress = 50;
-
+            SplashScreen.VM.Progress = 40;
             await Game.InitAsync();
 
-            SplashScreen.VM.Progress = 70;
+            // ----------------------------
+            SplashScreen.VM.Progress = 50;
             SplashScreen.VM.BottomText = "Initializing widgets...";
-
-
             RunningDispatchers = new ConcurrentDictionary<int, Dispatcher>();
             await WindowManager.Init();
+            SplashScreen.VM.Progress = 60;
             StartDispatcherWatcher();
 
+            // ----------------------------
+            SplashScreen.VM.Progress = 70;
+            SplashScreen.VM.BottomText = "Checking for icon database updates...";
+            await new IconsUpdater().CheckForUpdates();
+
+            // ----------------------------
             SplashScreen.VM.BottomText = "Initializing packet processor...";
             SplashScreen.VM.Progress = 80;
-
-            PacketAnalyzer.ProcessorReady += () => BaseDispatcher.Invoke(() =>
-            {
-                try
-                {
-                    ModuleLoader.LoadModules(BasePath);
-                }
-                catch (FileLoadException fle)
-                {
-                    TccMessageBox.Show("TCC module loader",
-                        $"An error occured while loading {fle.FileName}. TCC will now close. You can find more info about this error in TERA Dps discord #known-issues channel.",
-                        MessageBoxButton.OK, MessageBoxImage.Error);
-                    Close();
-                }
-                catch (FileNotFoundException fnfe)
-                {
-                    TccMessageBox.Show("TCC module loader",
-                        $"An error occured while loading {Path.GetFileName(fnfe.FileName)}. TCC will now close. You can find more info about this error in TERA Dps discord #known-issues channel.",
-                        MessageBoxButton.OK, MessageBoxImage.Error);
-                    Close();
-                }
-            });
             await PacketAnalyzer.InitAsync();
-            SplashScreen.VM.Progress = 100;
+
+            // ----------------------------
+            SplashScreen.VM.Progress = 90;
             SplashScreen.VM.BottomText = "Starting";
-
             TimeManager.Instance.SetServerTimeZone(Settings.LastLanguage);
-            ChatWindowManager.Instance.AddTccMessage(AppVersion);
-
             UpdateManager.StartPeriodicCheck();
 
-            if (!Experimental && Settings.ExperimentalNotification && UpdateManager.IsExperimentalNewer())
-                WindowManager.ViewModels.NotificationAreaVM.Enqueue("TCC experimental",
-                    "An experimental version of TCC is available. Open System settings to download it or disable this notification.",
+            SplashScreen.VM.Progress = 100;
+            SplashScreen.CloseWindowSafe();
+
+            // ----------------------------
+            ChatWindowManager.Instance.AddTccMessage($"{AppVersion} ready.");
+
+            if (!Beta && Settings.BetaNotification && UpdateManager.IsExperimentalNewer())
+                Log.N("TCC beta", "A beta version of TCC is available. Open System settings to download it or to disable this notification.",
                     NotificationType.Success,
                     10000);
-
-            SplashScreen.CloseWindowSafe();
-            Loading = false;
         }
-
         private static void ParseStartupArgs(IList<string> args)
         {
             // --toolbox
             ToolboxMode = args.IndexOf("--toolbox") != -1;
-            
+
             // --restart
             _restarted = args.IndexOf("--restart") != -1;
 
             // --settings_override 'path'
             var settingsOverrideIdx = args.IndexOf("--settings_override");
-            if (settingsOverrideIdx != -1)
-            {
-                SettingsContainer.SettingsOverride = args[settingsOverrideIdx + 1];
-            }
-
+            if (settingsOverrideIdx != -1) SettingsContainer.SettingsOverride = args[settingsOverrideIdx + 1];
         }
-
         public static void Restart()
         {
             Settings.Save();
@@ -199,7 +177,6 @@ namespace TCC
             UpdateManager.StopTimer();
             Environment.Exit(0);
         }
-
         private static bool IsAlreadyRunning()
         {
             _mutex = new Mutex(true, "TCC", out var createdNew);
@@ -213,15 +190,21 @@ namespace TCC
             _mutex.ReleaseMutex();
         }
 
+        #region Dispatchers
+        public static Dispatcher BaseDispatcher { get; private set; }
+
         public static ConcurrentDictionary<int, Dispatcher> RunningDispatchers { get; private set; }
+
         public static void AddDispatcher(int threadId, Dispatcher d)
         {
-            App.RunningDispatchers[threadId] = d;
+            RunningDispatchers[threadId] = d;
         }
+
         public static void RemoveDispatcher(int threadId)
         {
-            App.RunningDispatchers.TryRemove(threadId, out _);
+            RunningDispatchers.TryRemove(threadId, out _);
         }
+
         public static void WaitDispatchersShutdown()
         {
             if (RunningDispatchers == null) return;
@@ -233,40 +216,47 @@ namespace TCC
                 Thread.Sleep(100);
                 tries--;
             }
+
             Log.CW("All dispatchers shut down.");
         }
+
         private static void StartDispatcherWatcher()
         {
-            var t = new Thread(() =>
-            {
-                while (_running)
+            new Thread(() =>
                 {
-                    var deadlockedDispatchers = new List<Dispatcher>();
-                    try
+                    while (_running)
                     {
-                        Parallel.ForEach(Enumerable.Append(RunningDispatchers.Values, App.BaseDispatcher), (v) =>
+                        var deadlockedDispatchers = new List<Dispatcher>();
+                        try
                         {
-                            if (v.IsAlive(60000).Result) return;
-                            Log.CW($"{v.Thread.Name} didn't respond in time!");
-                            deadlockedDispatchers.Add(v);
-                        });
-                        Thread.Sleep(1000);
-                    }
-                    catch { }
-                    if (deadlockedDispatchers.Count > 1)
-                    {
-                        throw new DeadlockException($"The following threads didn't report in time: {deadlockedDispatchers.Select(d => d.Thread.Name).ToList().ToCSV()}");
+                            Parallel.ForEach(RunningDispatchers.Values.Append(BaseDispatcher), v =>
+                            {
+                                if (v.IsAlive(60000).Result) return;
+                                Log.CW($"{v.Thread.Name} didn't respond in time!");
+                                deadlockedDispatchers.Add(v);
+                            });
+                            Thread.Sleep(1000);
+                        }
+                        catch
+                        {
+                        }
+
+                        if (deadlockedDispatchers.Count > 1)
+                            throw new DeadlockException($"The following threads didn't report in time: {deadlockedDispatchers.Select(d => d.Thread.Name).ToList().ToCSV()}");
                         //Log.F($"The following threads didn't report in time: {deadlockedDispatchers.Select(d => d.Thread.Name).ToList().ToCSV()}");
                     }
-                }
-            })
-            {Name = "Watcher"};
-            t.Start();
+                })
+            { Name = "Watcher" }.Start();
         }
+        #endregion
 
         #region Misc
+
         private static FUBH _fubh;
-        public static bool FI { get; } = DateTime.Now >= TimeUtils.FromUnixTime(1567123200) && DateTime.Now < TimeUtils.FromUnixTime(1567209600);
+
+        public static bool FI { get; } = DateTime.Now >= TimeUtils.FromUnixTime(1567123200) &&
+                                         DateTime.Now < TimeUtils.FromUnixTime(1567209600);
+
         public static void FUBH()
         {
             BaseDispatcher.InvokeAsync(() =>
@@ -277,13 +267,5 @@ namespace TCC
         }
 
         #endregion
-    }
-
-    public class DeadlockException : Exception
-    {
-        public DeadlockException(string msg) : base(msg)
-        {
-
-        }
     }
 }
