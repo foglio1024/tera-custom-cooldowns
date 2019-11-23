3b9712a226cb81d0d41ff92298a9f512d02e30b3
Foglio1024 <foglio1024@gmail.com>
2019-11-23 18:38:37 +0100

    add MessageProcessException

---
+++ TCC.Core/Exceptions/MessageProcessException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace TCC.Exceptions
+{
+    public class MessageProcessException : Exception
+    {
+        public MessageProcessException(string msg) : base(msg)
+        {
+        }
+
+        public MessageProcessException(string msg, Exception inner) : base(msg, inner)
+        {
+            
+        }
+    }
+}
