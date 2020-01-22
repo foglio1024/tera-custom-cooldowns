1842dc1ef1b73d00cdb30c7153d2ac2c3bef3fc0
Foglio1024 <foglio1024@gmail.com>
2020-01-22 10:14:57 +0100

    rename S_WHISPER.PlayerId -> GameId

--- TeraPacketParser/Messages/S_WHISPER.cs
+++ TeraPacketParser/Messages/S_WHISPER.cs
@@ -5,14 +5,14 @@ namespace TeraPacketParser.Messages
 {
     public class S_WHISPER : ParsedMessage
     {
-        public ulong PlayerId { get; private set; }
+        public ulong GameId { get; private set; }
         public string Author { get; private set; }
         public string Recipient { get; private set; }
         public string Message { get; private set; }
         public S_WHISPER(TeraMessageReader reader) : base(reader)
         {
             reader.Skip(6);
-            PlayerId = reader.ReadUInt64();
+            GameId = reader.ReadUInt64();
             reader.Skip(3);
             Author = reader.ReadTeraString();
             Recipient = reader.ReadTeraString();
