9f002d9a8bf3a228b1ab9cc000b6a1a5dc3bde1b
Foglio1024 <foglio1024@gmail.com>
2020-01-27 18:34:38 +0100

    add new template selector for LFG

--- TCC.Core/TemplateSelectors/GroupWindowTemplateSelector.cs
+++ TCC.Core/TemplateSelectors/GroupWindowTemplateSelector.cs
@@ -4,8 +4,6 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using TCC.Data;
-using TCC.Utils;
-using TCC.Windows.Widgets;
 
 namespace TCC.TemplateSelectors
 {
@@ -27,23 +25,6 @@ namespace TCC.TemplateSelectors
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
         {
             throw new NotImplementedException();
-        }
-    }
-
-    public class NotificationTemplateSelector : DataTemplateSelector
-    {
-        public DataTemplate Default { get; set; }
-        public DataTemplate Progress { get; set; }
-
-        public override DataTemplate SelectTemplate(object item, DependencyObject container)
-        {
-            var n = (NotificationInfoBase) item;
-
-            switch (n.NotificationTemplate)
-            {
-                case NotificationTemplate.Progress: return Progress;
-                default: return Default; 
-            }
         }
     }
 }
---
+++ TCC.Core/TemplateSelectors/NotificationTemplateSelector.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using TCC.Utils;
+using TCC.Windows.Widgets;
+
+namespace TCC.TemplateSelectors
+{
+    public class NotificationTemplateSelector : DataTemplateSelector
+    {
+        public DataTemplate Default { get; set; }
+        public DataTemplate Progress { get; set; }
+
+        public override DataTemplate SelectTemplate(object item, DependencyObject container)
+        {
+            var n = (NotificationInfoBase) item;
+
+            switch (n.NotificationTemplate)
+            {
+                case NotificationTemplate.Progress: return Progress;
+                default: return Default; 
+            }
+        }
+    }
+}
