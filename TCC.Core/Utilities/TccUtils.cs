510d51fc2848b4aa8bd520203c41ae79468962c3
Foglio1024 <foglio1024@gmail.com>
2020-02-11 16:34:40 +0100

    use switch expressions in tcc utils

--- TCC.Core/Utilities/TccUtils.cs
+++ TCC.Core/Utilities/TccUtils.cs
@@ -16,44 +16,44 @@ namespace TCC.Utilities
     {
         public static Geometry SvgClass(Class c)
         {
-            switch (c)
+            return c switch
             {
-                case Class.Warrior: return SVG.SvgClassWarrior;
-                case Class.Lancer: return SVG.SvgClassLancer;
-                case Class.Slayer: return SVG.SvgClassSlayer;
-                case Class.Berserker: return SVG.SvgClassBerserker;
-                case Class.Sorcerer: return SVG.SvgClassSorcerer;
-                case Class.Archer: return SVG.SvgClassArcher;
-                case Class.Priest: return SVG.SvgClassPriest;
-                case Class.Mystic: return SVG.SvgClassMystic;
-                case Class.Reaper: return SVG.SvgClassReaper;
-                case Class.Gunner: return SVG.SvgClassGunner;
-                case Class.Brawler: return SVG.SvgClassBrawler;
-                case Class.Ninja: return SVG.SvgClassNinja;
-                case Class.Valkyrie: return SVG.SvgClassValkyrie;
-                default: return SVG.SvgClassCommon;
-            }
+                Class.Warrior => SVG.SvgClassWarrior,
+                Class.Lancer => SVG.SvgClassLancer,
+                Class.Slayer => SVG.SvgClassSlayer,
+                Class.Berserker => SVG.SvgClassBerserker,
+                Class.Sorcerer => SVG.SvgClassSorcerer,
+                Class.Archer => SVG.SvgClassArcher,
+                Class.Priest => SVG.SvgClassPriest,
+                Class.Mystic => SVG.SvgClassMystic,
+                Class.Reaper => SVG.SvgClassReaper,
+                Class.Gunner => SVG.SvgClassGunner,
+                Class.Brawler => SVG.SvgClassBrawler,
+                Class.Ninja => SVG.SvgClassNinja,
+                Class.Valkyrie => SVG.SvgClassValkyrie,
+                _ => SVG.SvgClassCommon
+            };
         }
         public static string ClassEnumToString(Class c)
         {
-            switch (c)
+            return c switch
             {
-                case Class.Warrior: return "Warrior";
-                case Class.Lancer: return "Lancer";
-                case Class.Slayer: return "Slayer";
-                case Class.Berserker: return "Berserker";
-                case Class.Sorcerer: return "Sorcerer";
-                case Class.Archer: return "Archer";
-                case Class.Priest: return "Priest";
-                case Class.Mystic: return "Mystic";
-                case Class.Reaper: return "Reaper";
-                case Class.Gunner: return "Gunner";
-                case Class.Brawler: return "Brawler";
-                case Class.Ninja: return "Ninja";
-                case Class.Valkyrie: return "Valkyrie";
-                case Class.Common: return "All classes";
-                default: return "";
-            }
+                Class.Warrior => "Warrior",
+                Class.Lancer => "Lancer",
+                Class.Slayer => "Slayer",
+                Class.Berserker => "Berserker",
+                Class.Sorcerer => "Sorcerer",
+                Class.Archer => "Archer",
+                Class.Priest => "Priest",
+                Class.Mystic => "Mystic",
+                Class.Reaper => "Reaper",
+                Class.Gunner => "Gunner",
+                Class.Brawler => "Brawler",
+                Class.Ninja => "Ninja",
+                Class.Valkyrie => "Valkyrie",
+                Class.Common => "All classes",
+                _ => ""
+            };
         }
         public static List<ChatChannelOnOff> GetEnabledChannelsList()
         {
