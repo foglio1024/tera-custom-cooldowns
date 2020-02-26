8ceac6a6d9f906fd6890f72a0a3fd205de9a3a0e
Foglio1024 <foglio1024@gmail.com>
2020-02-26 21:36:36 +0100

    S_PLAYER_CHANGE_STAMINA.unk1 is BonusST

--- TeraPacketParser/Messages/S_PLAYER_CHANGE_STAMINA.cs
+++ TeraPacketParser/Messages/S_PLAYER_CHANGE_STAMINA.cs
@@ -6,13 +6,14 @@
         public int CurrentST { get; }
 
         public int MaxST {  get; }
+        public int BonusST {  get; }
 
         public S_PLAYER_CHANGE_STAMINA(TeraMessageReader reader) : base(reader)
         {
             CurrentST = reader.ReadInt32();
             MaxST = reader.ReadInt32();
             reader.Skip(12);
-            //unk1 = reader.ReadInt32();
+            BonusST = reader.ReadInt32();
             //unk2 = reader.ReadInt32();
             //unk3 = reader.ReadInt32();
         }
