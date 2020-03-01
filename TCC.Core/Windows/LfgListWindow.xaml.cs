ff5416e408cfd2f1aeacdbf1c6748023033bf619
Foglio1024 <foglio1024@gmail.com>
2020-03-01 22:05:19 +0100

    reorganized some buttons in lfg window

--- TCC.Core/Windows/LfgListWindow.xaml.cs
+++ TCC.Core/Windows/LfgListWindow.xaml.cs
@@ -1,13 +1,19 @@
 ﻿using Nostrum;
 using System.Windows;
 using System.Windows.Input;
+using System.Windows.Interop;
+using Nostrum.WinAPI;
 using TCC.Interop.Proxy;
+using TCC.UI;
 using TCC.ViewModels;
+using FocusManager = TCC.UI.FocusManager;
 
 namespace TCC.Windows
 {
     public partial class LfgListWindow
     {
+        private bool _keepPopupOpen = false;
+
         private LfgListViewModel VM { get; }
 
         public ICommand HideWindowCommand { get; }
@@ -37,7 +43,7 @@ namespace TCC.Windows
 
         public override void ShowWindow()
         {
-            ProxyInterface.Instance.Stub.RequestListings();
+            StubInterface.Instance.StubClient.RequestListings();
 
             base.ShowWindow();
 
@@ -57,23 +63,45 @@ namespace TCC.Windows
 
         private void OnTbMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
-
+            _keepPopupOpen = true;
             FocusManager.UndoUnfocusable(Handle);
+            var src = (HwndSource) PresentationSource.FromVisual(ActionsPopup.Child);
+            User32.SetForegroundWindow(src.Handle);
+            FocusManager.UndoUnfocusable(src.Handle);
+
+            ((FrameworkElement) sender).Focus();
+            Keyboard.Focus((FrameworkElement)sender);
         }
 
         private void OnTbLostKeyboardFocus(object sender, KeyboardFocusChangedEventArgs e)
         {
             FocusManager.MakeUnfocusable(Handle);
+            _keepPopupOpen = false;
         }
 
         private void OnBgMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
             Keyboard.ClearFocus();
+            _keepPopupOpen = false;
         }
 
         private void LfgPopup_OnMouseLeave(object sender, MouseEventArgs e)
         {
+            if (_keepPopupOpen) return;
             VM.IsPopupOpen = false;
+        }
+
+        private void OnTbKeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                OnBgMouseLeftButtonDown(sender, null);
+            }
+        }
+
+        private void OnTbMouseLeave(object sender, MouseEventArgs e)
+        {
+            _keepPopupOpen = false;
         }
     }
 }
