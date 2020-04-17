562cd9d16acc2d06eece934fc5c78b2874c94fbd
Foglio1024 <foglio1024@gmail.com>
2020-04-17 23:59:20 +0200

    fix crash on S_IMAGE_DATA

--- TeraPacketParser/Messages/S_IMAGE_DATA.cs
+++ TeraPacketParser/Messages/S_IMAGE_DATA.cs
@@ -26,51 +26,60 @@ namespace TeraPacketParser.Messages
 
         public S_IMAGE_DATA(TeraMessageReader reader) : base(reader)
         {
-            var nameOffset = reader.ReadUInt16();
-            var imageOffset = reader.ReadUInt16();
-            var imageLength = reader.ReadUInt16();
-
-            reader.BaseStream.Position = nameOffset - 4;
-            var imageName = reader.ReadTeraString();
-            if (!imageName.StartsWith("guildlogo")) return;
-            reader.BaseStream.Position = imageOffset - 4;
-            var imageBytes = reader.ReadBytes(imageLength);
-            var width = BitConverter.ToInt32(imageBytes, 8);
-            var type = BitConverter.ToInt32(imageBytes, 12);
-
-            var image = new Bitmap(width, width, PixelFormat.Format8bppIndexed);
-            var palette = image.Palette;
-            int length;
-            switch (type)
+            var imageName = "";
+            try
             {
-                case 0:
-                case 1:
-                    var paletteSize = BitConverter.ToInt32(imageBytes, 16);
-                    if (paletteSize >= imageLength - 24) { Debug.WriteLine("palette size too big"); return; }
-                    for (var i = 0; i < paletteSize; i++)
-                    {
-                        palette.Entries[i] = Color.FromArgb(imageBytes[0x14 + i * 3], imageBytes[0x15 + i * 3], imageBytes[0x16 + i * 3]);
-                    }
-                    length = BitConverter.ToInt32(imageBytes, paletteSize * 3 + 20);
-                    break;
-                case 2:
-                case 3:
-                    for (var i = 0; i < 255; i++)
-                    {
-                        palette.Entries[i] = Color.FromArgb(i, i, i);
-                    }
-                    length = BitConverter.ToInt32(imageBytes, 16);
-                    break;
-                default:
-                    Debug.WriteLine("Unknown guild logo format");
-                    return;
+                var nameOffset = reader.ReadUInt16();
+                var imageOffset = reader.ReadUInt16();
+                var imageLength = reader.ReadUInt16();
+
+                reader.BaseStream.Position = nameOffset - 4;
+                imageName = reader.ReadTeraString();
+                if (!imageName.StartsWith("guildlogo")) return;
+                reader.BaseStream.Position = imageOffset - 4;
+                var imageBytes = reader.ReadBytes(imageLength);
+                var width = BitConverter.ToInt32(imageBytes, 8);
+                var type = BitConverter.ToInt32(imageBytes, 12);
+
+                var image = new Bitmap(width, width, PixelFormat.Format8bppIndexed);
+                var palette = image.Palette;
+                int length;
+                switch (type)
+                {
+                    case 0:
+                    case 1:
+                        var paletteSize = BitConverter.ToInt32(imageBytes, 16);
+                        if (paletteSize >= imageLength - 24) { Debug.WriteLine("palette size too big"); return; }
+                        for (var i = 0; i < paletteSize; i++)
+                        {
+                            palette.Entries[i] = Color.FromArgb(imageBytes[0x14 + i * 3], imageBytes[0x15 + i * 3], imageBytes[0x16 + i * 3]);
+                        }
+                        length = BitConverter.ToInt32(imageBytes, paletteSize * 3 + 20);
+                        break;
+                    case 2:
+                    case 3:
+                        for (var i = 0; i < 255; i++)
+                        {
+                            palette.Entries[i] = Color.FromArgb(i, i, i);
+                        }
+                        length = BitConverter.ToInt32(imageBytes, 16);
+                        break;
+                    default:
+                        Debug.WriteLine("Unknown guild logo format");
+                        return;
+                }
+                var pixels = image.LockBits(new Rectangle(0, 0, width, width), ImageLockMode.WriteOnly, image.PixelFormat);
+                Marshal.Copy(imageBytes, imageLength - length, pixels.Scan0, length);
+                image.UnlockBits(pixels);
+                image.Palette = palette;
+                var id = Convert.ToUInt32(imageName.Split('_')[2]);
+                Database[id] = image;
+
             }
-            var pixels = image.LockBits(new Rectangle(0, 0, width, width), ImageLockMode.WriteOnly, image.PixelFormat);
-            Marshal.Copy(imageBytes, imageLength - length, pixels.Scan0, length);
-            image.UnlockBits(pixels);
-            image.Palette = palette;
-            var id = Convert.ToUInt32(imageName.Split('_')[2]);
-            Database[id] = image;
+            catch
+            {
+                Console.WriteLine($"Failed to parse S_IMAGE_DATA for {imageName}");
+            }           
             //if (!Directory.Exists("resources/images/guilds")) Directory.CreateDirectory("resources/images/guilds");
             //image.Save("resources/images/guilds/" + imageName + ".bmp", ImageFormat.Bmp);
 
