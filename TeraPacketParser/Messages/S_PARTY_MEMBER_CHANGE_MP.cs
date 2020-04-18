09ed066a9f678e198ba5568c254f28e7ad143142
Foglio1024 <foglio1024@gmail.com>
2020-04-18 15:46:34 +0200

    small packet refactoring

--- TeraPacketParser/Messages/S_PARTY_MEMBER_CHANGE_MP.cs
+++ TeraPacketParser/Messages/S_PARTY_MEMBER_CHANGE_MP.cs
@@ -5,20 +5,21 @@ namespace TeraPacketParser.Messages
 {
     public class S_PARTY_MEMBER_CHANGE_MP : ParsedMessage
     {
-        private uint _serverId, _playerId;
-        private int _currentMP, _maxMP;
-        public uint ServerId => _serverId;
-        public uint PlayerId => _playerId;
-        public int CurrentMP => _currentMP;
-        public int MaxMP => _maxMP;
+        public uint ServerId { get; }
+
+        public uint PlayerId { get; }
+
+        public int CurrentMP { get; }
+
+        public int MaxMP { get; }
 
         public S_PARTY_MEMBER_CHANGE_MP(TeraMessageReader reader) : base(reader)
         {
-            _serverId = reader.ReadUInt32();
-            _playerId = reader.ReadUInt32();
+            ServerId = reader.ReadUInt32();
+            PlayerId = reader.ReadUInt32();
 
-            _currentMP = reader.ReadInt32();
-            _maxMP = reader.ReadInt32();
+            CurrentMP = reader.ReadInt32();
+            MaxMP = reader.ReadInt32();
 
         }
     }
