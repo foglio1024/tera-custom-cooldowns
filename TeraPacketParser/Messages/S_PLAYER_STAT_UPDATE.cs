f6f4e4955717e37bfea26a8d1c735c84f2ce4a94
Foglio1024 <foglio1024@gmail.com>
2020-04-28 14:14:19 +0200

    update S_PLAYER_STAT_UPDATE to p93

--- TeraPacketParser/Messages/S_PLAYER_STAT_UPDATE.cs
+++ TeraPacketParser/Messages/S_PLAYER_STAT_UPDATE.cs
@@ -5,10 +5,10 @@ namespace TeraPacketParser.Messages
 {
     public class S_PLAYER_STAT_UPDATE : ParsedMessage
     {
-        public int CurrentHP { get; }
+        public long CurrentHP { get; }
         public int CurrentMP { get; }
         public int CurrentST { get; }
-        public int MaxHP { get; }
+        public long MaxHP { get; }
         public int MaxMP { get; }
         public int BonusHP { get; }
         public int BonusMP { get; }
@@ -17,8 +17,9 @@ namespace TeraPacketParser.Messages
         public int Level { get; }
         public float Ilvl { get; }
         public int Edge { get; }
-        public float BonusCritFactor { get; }
+        public float TotalCritFactor { get; }
         public bool Status { get; set; }
+        public int TotalMagicalResistance { get; set; }
         public bool Fire { get; set; }
         public bool Ice { get; set; }
         public bool Arcane { get; set; }
@@ -27,7 +28,62 @@ namespace TeraPacketParser.Messages
 
         public S_PLAYER_STAT_UPDATE(TeraMessageReader reader) : base(reader)
         {
-            if (reader.Factory.ReleaseVersion >= 8600 && reader.Factory.ReleaseVersion < 9901)
+            if (reader.Factory.ReleaseVersion >= 9300 && reader.Factory.ReleaseVersion < 9901)
+            {
+                reader.BaseStream.Position = 0;
+                CurrentHP = reader.ReadInt64();
+                CurrentMP = reader.ReadInt32();
+                reader.Skip(8);
+                MaxHP = reader.ReadInt64();
+                MaxMP = reader.ReadInt32();
+
+                reader.Skip(2 * 4);
+
+                var baseCrit = reader.ReadSingle();
+
+                reader.Skip((14 * 4) + (3 * 2));
+
+                var magRes = reader.ReadInt32();
+
+                reader.Skip((12 * 4) + (3 * 2));
+
+                TotalCritFactor = reader.ReadSingle() + baseCrit;
+
+                reader.Skip(12 * 4);
+
+                TotalMagicalResistance = magRes + reader.ReadInt32();
+
+                reader.Skip(7 * 4);
+
+                Level = reader.ReadInt16();
+
+                reader.Skip(4);
+
+                Status = reader.ReadBoolean();
+                BonusHP = reader.ReadInt32();
+                BonusMP = reader.ReadInt32();
+
+                reader.Skip(8);
+
+                CurrentST = reader.ReadInt32();
+                MaxST = reader.ReadInt32();
+                BonusST = reader.ReadInt32();
+
+                reader.Skip(8);
+
+                Ilvl = reader.ReadSingle();
+                Edge = reader.ReadInt32();
+
+                reader.Skip(28);
+
+                Fire = reader.ReadUInt32() == 4;
+                Ice = reader.ReadUInt32() == 4;
+                Arcane = reader.ReadUInt32() == 4;
+                Coins = reader.ReadUInt32();
+                MaxCoins = reader.ReadUInt32();
+
+            }
+            else if (reader.Factory.ReleaseVersion >= 8600 && reader.Factory.ReleaseVersion < 9901)
             {
                 CurrentHP = reader.ReadInt32();
                 reader.Skip(4);
@@ -40,7 +96,7 @@ namespace TeraPacketParser.Messages
                 var baseCrit = reader.ReadSingle();
                 reader.Skip(108);
 
-                BonusCritFactor = reader.ReadSingle() + baseCrit;
+                TotalCritFactor = reader.ReadSingle() + baseCrit;
                 reader.Skip(72);
                 Level = reader.ReadInt16();
                 reader.Skip(4);
@@ -78,8 +134,8 @@ namespace TeraPacketParser.Messages
                 reader.Skip(4);
                 MaxMP = reader.ReadInt32();
                 reader.Skip(88);
-                BonusCritFactor = reader.ReadSingle();
-                                                      
+                TotalCritFactor = reader.ReadSingle();
+
                 reader.Skip(40);
 
                 Level = reader.ReadInt16();
