fe49f55298fe15728f689c28ee04e0501d3db695
Foglio1024 <foglio1024@gmail.com>
2020-08-07 20:11:07 +0200

    new mod API

--- TCC.Core/Module/index.js
+++ TCC.Core/Module/index.js
@@ -1,16 +1,19 @@
 const { spawn } = require('child_process');
 const path = require('path');
 
-module.exports = function TCC(m)
+exports.ClientMod = class
 {
-    const tccPath = path.join(__dirname, 'TCC.exe');
-
-    if(!m.manager.isInstalled("tcc-chat-link"))
+    constructor(m)
     {
-        m.log("TCC chat link is not installed. Advanced functionality won't be available.");
-    }
+        const tccPath = path.join(__dirname, 'TCC.exe');
 
-    m.log('Starting TCC...');
-    const tcc = spawn(tccPath, ['--toolbox'], { stdio: 'ignore' });
-    tcc.on('exit', () => m.log('TCC exited because it closed or it is already running.'));
+        if (!m.manager.isInstalled("tcc-chat-link"))
+        {
+            m.log("TCC chat link is not installed. Advanced functionality won't be available.");
+        }
+
+        m.log('Starting TCC...');
+        const tcc = spawn(tccPath, ['--toolbox'], { stdio: 'ignore' });
+        tcc.on('exit', () => m.log('TCC exited because it closed or it is already running.'));
+    }
 }
