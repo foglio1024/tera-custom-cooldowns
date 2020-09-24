fd43a0f5e6e3e20cac16e4173c03fce013b78e7b
Foglio1024 <foglio1024@gmail.com>
2020-09-24 12:37:54 +0200

    merge UI Removers in js module

--- TCC.Core/Module/index.js
+++ TCC.Core/Module/index.js
@@ -1,22 +1,91 @@
 const { spawn } = require('child_process');
-const path = require('path');
+const Path = require('path');
+const FS = require('fs');
 
 exports.ClientMod = class
 {
     constructor(m)
     {
-        const tccPath = path.join(__dirname, 'TCC.exe');
-
-        if (!m.manager.isInstalled("tcc-chat-link"))
-        {
-            m.log("TCC chat link is not installed. Advanced functionality won't be available.");
-        }
+        const tccPath = Path.join(__dirname, 'TCC.exe');
 
         m.clientInterface.once('ready', () => 
         {
-		    m.log('Starting TCC...');
-		    const tcc = spawn(tccPath, ['--toolbox'], { stdio: 'ignore' });
-		    tcc.on('exit', () => m.log('TCC exited because it closed or it is already running.'));
-	    });
+            m.log('Starting TCC...');
+            const tcc = spawn(tccPath, ['--toolbox'], { stdio: 'ignore' });
+            tcc.on('exit', () => m.log('TCC exited because it closed or it is already running.'));
+        });
+
+        this.install = function (installer)
+        {
+            const tccSettingsPath = Path.join(__dirname, 'tcc-settings.json');
+            let noSettings = false;
+            if (!FS.existsSync(tccSettingsPath))
+                noSettings = true;;
+
+            if (noSettings)
+            {
+                // no settings, remove everything by default
+                m.log("No settings found, installing all removers by default.")
+                installer.gpk('gpk/S1UI_CharacterWindow.gpk');
+                installer.gpk('gpk/S1UI_TargetInfo.gpk');
+                installer.gpk('gpk/S1UI_GageBoss.gpk');
+                installer.gpk('gpk/S1UI_Abnormality.gpk');
+                installer.gpk("gpk/S1UI_PartyWindow.gpk");
+                installer.gpk("gpk/S1UI_PartyWindowRaidInfo.gpk");
+                // installer.gpk('gpk/S1UI_ProgressBar.gpk');
+                installer.gpk("gpk/S1UI_PartyBoard.gpk");
+                installer.gpk("gpk/S1UI_PartyBoardMemberInfo.gpk");
+                installer.gpk("gpk/S1UI_Chat2.gpk");
+                return;
+            }
+            
+            m.log("Reading TCC settings.")
+            let settings = JSON.parse(FS.readFileSync(tccSettingsPath));
+            
+            if (settings.CharacterWindowSettings.Enabled === true)
+            {
+                installer.gpk('gpk/S1UI_CharacterWindow.gpk');
+                m.log("Installed S1UI_CharacterWindow remover.");
+            }
+            
+            if (settings.NpcWindowSettings.Enabled === true)
+            {
+                installer.gpk('gpk/S1UI_TargetInfo.gpk');
+                installer.gpk('gpk/S1UI_GageBoss.gpk');
+                m.log("Installed S1UI_TargetInfo and S1UI_GageBoss remover.");
+            }
+            
+            if (settings.BuffWindowSettings.Enabled === true)
+            {
+                installer.gpk('gpk/S1UI_Abnormality.gpk');
+                m.log("Installed S1UI_Abnormality remover.");
+            }
+            
+            if (settings.GroupWindowSettings.Enabled === true)
+            {
+                installer.gpk("gpk/S1UI_PartyWindow.gpk");
+                installer.gpk("gpk/S1UI_PartyWindowRaidInfo.gpk");
+                m.log("Installed S1UI_PartyWindow and S1UI_PartyWindowRaidInfo remover.");
+            }
+            
+            // if (settings.FlightGaugeWindowSettings.Enabled === true)
+            // {
+            //     installer.gpk('gpk/S1UI_ProgressBar.gpk');
+            //     m.log("Installed S1UI_ProgressBar remover.");
+            // }
+            
+            if (settings.LfgWindowSettings.Enabled === true)
+            {
+                installer.gpk("gpk/S1UI_PartyBoard.gpk");
+                installer.gpk("gpk/S1UI_PartyBoardMemberInfo.gpk");
+                m.log("Installed S1UI_PartyBoard and S1UI_PartyBoardMemberInfo remover.");
+            }
+
+            if (settings.ChatEnabled === true)
+            {
+                installer.gpk("gpk/S1UI_Chat2.gpk");
+                m.log("Installed S1UI_Chat2.");
+            }
+        };
     }
 }
