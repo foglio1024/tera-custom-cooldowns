51989dc9c4e86f70c55123827701cc71ff31cdfe
Foglio1024 <foglio1024@gmail.com>
2020-09-26 22:34:03 +0200

    add untracked debug stuff

---
+++ TCC.Core/Debugging/ObjectTracker.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Threading;
+using TCC.Utils;
+
+namespace TCC.Debug
+{
+    public static class ObjectTracker
+    {
+        private static ConcurrentDictionary<Type, long> Instances { get; } = new ConcurrentDictionary<Type, long>();
+        private static readonly object _lock = new object();
+        public static void Register(Type t)
+        {
+            lock (_lock)
+            {
+                if (!Instances.ContainsKey(t)) Instances.TryAdd(t, 0);
+                Instances[t]++;
+            }
+        }
+
+        public static void Unregister(Type t)
+        {
+            lock (_lock)
+            {
+                if (!Instances.ContainsKey(t)) return;
+                Instances[t]--;
+            }
+        }
+
+        public static void DumpToFile()
+        {
+            var sb = "Instanced objects\n";
+            foreach (var (type, count) in Instances)
+            {
+                var name = $"- {type.Name} ";
+                while (name.Length < 35)
+                {
+                    name += "-";
+                }
+
+                name += $" {count}";
+                sb += name;
+                sb += "\n";
+            }
+
+            Log.F(sb, "objects.log");
+        }
+    }
+}
