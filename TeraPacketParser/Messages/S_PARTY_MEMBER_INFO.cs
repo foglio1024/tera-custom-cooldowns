82c761b819ce2396180875d17c6f2e1e7be55c98
Foglio1024 <foglio1024@gmail.com>
2020-12-16 19:10:19 +0100

    update S_PARTY_MEMBER_INFO

--- TeraPacketParser/Messages/S_PARTY_MEMBER_INFO.cs
+++ TeraPacketParser/Messages/S_PARTY_MEMBER_INFO.cs
@@ -3,11 +3,8 @@ using System.Collections.Generic;
 using TeraDataLite;
 namespace TeraPacketParser.Messages
 {
-
-
     public class S_PARTY_MEMBER_INFO : ParsedMessage
     {
-
         public uint Id { get; }
         public List<GroupMemberData> Members { get; }
 
@@ -15,40 +12,33 @@ namespace TeraPacketParser.Messages
         {
             var count = reader.ReadUInt16();
             var offset = reader.ReadUInt16();
-            reader.Skip(8);
-            Id = reader.ReadUInt32();
+            reader.Skip(2);
 
             reader.BaseStream.Position = offset - 4;
             Members = new List<GroupMemberData>();
             for (var i = 0; i < count; i++)
             {
                 var u = new GroupMemberData();
-                reader.Skip(2); // var current = reader.ReadUInt16();
+                reader.Skip(2);
                 var next = reader.ReadUInt16();
                 var nameOffset = reader.ReadUInt16();
                 u.PlayerId = reader.ReadUInt32();
                 u.Class = (Class)reader.ReadUInt16();
-                reader.Skip(2 + 2);
+                reader.Skip(4);
                 u.Level = Convert.ToUInt32(reader.ReadUInt16());
-                reader.ReadBoolean();
-                reader.Skip(1);
-                u.Order = Convert.ToInt32(reader.ReadInt16());
+                reader.Skip(4);
                 u.GuardId = reader.ReadUInt32();
                 u.SectionId = reader.ReadUInt32();
-                //u.Location = SessionManager.DB.GetSectionName(gId, aId);
+                u.Order = Convert.ToInt32(reader.ReadInt16());
+                reader.Skip(2);
                 u.IsLeader = reader.ReadBoolean();
-                //u.Laurel = (Laurel)reader.ReadUInt32();
-                //Console.WriteLine("---");
+                if (u.IsLeader) Id = u.PlayerId;
                 u.Online = reader.ReadBoolean();
-                //Console.WriteLine(reader.ReadByte());
-                //Console.WriteLine(reader.ReadByte());
                 reader.BaseStream.Position = nameOffset - 4;
                 u.Name = reader.ReadTeraString();
-                //u.IsLeader = u.PlayerId == Id;
                 Members.Add(u);
                 if (next != 0) reader.BaseStream.Position = next - 4;
             }
-
         }
     }
 }
