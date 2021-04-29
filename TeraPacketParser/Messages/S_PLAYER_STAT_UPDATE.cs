60821431853a6093c1c640b6554d4a0fe8b388ed
Foglio <foglio1024@gmail.com>
2021-04-29 14:12:16 +0200

    update S_PLAYER_STAT_UPDATE for p105

--- TeraPacketParser/Messages/S_PLAYER_STAT_UPDATE.cs
+++ TeraPacketParser/Messages/S_PLAYER_STAT_UPDATE.cs
@@ -28,139 +28,57 @@ namespace TeraPacketParser.Messages
 
         public S_PLAYER_STAT_UPDATE(TeraMessageReader reader) : base(reader)
         {
-            if (reader.Factory.ReleaseVersion >= 9300)
-            {
-                reader.BaseStream.Position = 0;
-                CurrentHP = reader.ReadInt64();
-                CurrentMP = reader.ReadInt32();
-                reader.Skip(8);
-                MaxHP = reader.ReadInt64();
-                MaxMP = reader.ReadInt32();
+            reader.BaseStream.Position = 0;
+            CurrentHP = reader.ReadInt64();
+            CurrentMP = reader.ReadInt32();
+            reader.Skip(8);
+            MaxHP = reader.ReadInt64();
+            MaxMP = reader.ReadInt32();
 
-                reader.Skip(2 * 4);
+            reader.Skip(2 * 4);
 
-                var baseCrit = reader.ReadSingle();
+            var baseCrit = reader.ReadSingle();
 
-                reader.Skip((14 * 4) + (3 * 2));
+            reader.Skip((14 * 4) + (3 * 2));
 
-                var magRes = reader.ReadInt32();
+            var magRes = reader.ReadInt32();
 
-                reader.Skip((12 * 4) + (3 * 2));
+            reader.Skip((12 * 4) + (3 * 2));
 
-                TotalCritFactor = reader.ReadSingle() + baseCrit;
+            TotalCritFactor = reader.ReadSingle() + baseCrit;
 
-                reader.Skip(12 * 4);
+            reader.Skip(12 * 4);
 
-                TotalMagicalResistance = magRes + reader.ReadInt32();
+            TotalMagicalResistance = magRes + reader.ReadInt32();
 
-                reader.Skip(7 * 4);
+            reader.Skip(7 * 4);
 
-                Level = reader.ReadInt16();
+            Level = reader.ReadInt16();
 
-                reader.Skip(4);
+            reader.Skip(4);
 
-                Status = reader.ReadBoolean();
-                BonusHP = reader.ReadInt32();
-                BonusMP = reader.ReadInt32();
+            Status = reader.ReadBoolean();
+            BonusHP = reader.ReadInt32();
+            BonusMP = reader.ReadInt32();
 
-                reader.Skip(8);
+            reader.Skip(8);
 
-                CurrentST = reader.ReadInt32();
-                MaxST = reader.ReadInt32();
-                BonusST = reader.ReadInt32();
+            CurrentST = reader.ReadInt32();
+            MaxST = reader.ReadInt32();
+            BonusST = reader.ReadInt32();
 
-                reader.Skip(8);
+            reader.Skip(8);
 
-                Ilvl = reader.ReadSingle();
-                Edge = reader.ReadInt32();
+            Ilvl = reader.ReadSingle();
+            Edge = reader.ReadInt32();
 
-                reader.Skip(28);
+            reader.Skip(reader.Factory.ReleaseVersion/100 >= 105 ? 32 : 28);
 
-                Fire = reader.ReadUInt32() == 4;
-                Ice = reader.ReadUInt32() == 4;
-                Arcane = reader.ReadUInt32() == 4;
-                Coins = reader.ReadUInt32();
-                MaxCoins = reader.ReadUInt32();
-
-            }
-            else if (reader.Factory.ReleaseVersion >= 8600)
-            {
-                CurrentHP = reader.ReadInt32();
-                reader.Skip(4);
-                CurrentMP = reader.ReadInt32();
-                reader.Skip(8);
-                MaxHP = reader.ReadInt32();
-                reader.Skip(4);
-                MaxMP = reader.ReadInt32();
-                reader.Skip(8);
-                var baseCrit = reader.ReadSingle();
-                reader.Skip(108);
-
-                TotalCritFactor = reader.ReadSingle() + baseCrit;
-                reader.Skip(72);
-                Level = reader.ReadInt16();
-                reader.Skip(4);
-
-                Status = reader.ReadBoolean();
-                BonusHP = reader.ReadInt32();
-                BonusMP = reader.ReadInt32();
-                reader.Skip(8);
-
-                CurrentST = reader.ReadInt32();
-                MaxST = reader.ReadInt32();
-                BonusST = reader.ReadInt32();
-
-                reader.Skip(8);
-
-                Ilvl = reader.ReadSingle();
-                Edge = reader.ReadInt32();
-
-                reader.Skip(28);
-                Fire = reader.ReadUInt32() == 4;
-                Ice = reader.ReadUInt32() == 4;
-                Arcane = reader.ReadUInt32() == 4;
-                Coins = reader.ReadUInt32();
-                MaxCoins = reader.ReadUInt32();
-
-            }
-            else
-            {
-                CurrentHP = reader.ReadInt32();
-                reader.Skip(4);
-                CurrentMP = reader.ReadInt32();
-                reader.Skip(8);
-
-                MaxHP = reader.ReadInt32();
-                reader.Skip(4);
-                MaxMP = reader.ReadInt32();
-                reader.Skip(88);
-                TotalCritFactor = reader.ReadSingle();
-
-                reader.Skip(40);
-
-                Level = reader.ReadInt16();
-                reader.Skip(2);
-                reader.Skip(2);
-
-                Status = reader.ReadBoolean();
-                BonusHP = reader.ReadInt32();
-                BonusMP = reader.ReadInt32();
-                reader.Skip(8);
-
-                CurrentST = reader.ReadInt32();
-                MaxST = reader.ReadInt32();
-                BonusST = reader.ReadInt32();
-
-                reader.Skip(8);
-                Ilvl = reader.ReadInt32();
-                Edge = reader.ReadInt32();
-                reader.Skip(28);
-                Fire = reader.ReadUInt32() == 4;
-                Ice = reader.ReadUInt32() == 4;
-                Arcane = reader.ReadUInt32() == 4;
-                Coins = reader.ReadUInt32();
-                MaxCoins = reader.ReadUInt32();
-            }
+            Fire = reader.ReadUInt32() == 4;
+            Ice = reader.ReadUInt32() == 4;
+            Arcane = reader.ReadUInt32() == 4;
+            Coins = reader.ReadUInt32();
+            MaxCoins = reader.ReadUInt32();
         }
 
     }
