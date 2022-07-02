8cfee82b5538d4dda47fe16b4b0ce20d1be4d058
Foglio <foglio1024@gmail.com>
2022-07-02 16:37:45 +0200

    fix lfg data parsing

--- TeraPacketParser/Messages/S_PARTY_MEMBER_INFO.cs
+++ TeraPacketParser/Messages/S_PARTY_MEMBER_INFO.cs
@@ -15,29 +15,36 @@ namespace TeraPacketParser.Messages
 
             reader.Skip(2); // bool isRaid + bool adminInspectionUI
 
-            reader.BaseStream.Position = offset - 4;
+            reader.RepositionAt(offset);
             Members = new List<GroupMemberData>();
             for (var i = 0; i < count; i++)
             {
                 var u = new GroupMemberData();
-                reader.Skip(2);
+                reader.Skip(2); // curr
                 var next = reader.ReadUInt16();
                 var nameOffset = reader.ReadUInt16();
                 u.PlayerId = reader.ReadUInt32();
-                if (reader.Factory.ReleaseVersion / 100 >= 108) u.ServerId = reader.ReadUInt32();
+
+                if (reader.Factory.ReleaseVersion / 100 >= 108)
+                    u.ServerId = reader.ReadUInt32();
+
                 u.Class = (Class)reader.ReadUInt16();
-                reader.Skip(4);
+                reader.Skip(4); // race + gender
                 u.Level = Convert.ToUInt32(reader.ReadUInt16());
-                reader.Skip(4); // worldId
+                var worldId = reader.ReadUInt32(); // worldId
                 u.GuardId = reader.ReadUInt32();
                 u.SectionId = reader.ReadUInt32();
-                u.Order = Convert.ToInt32(reader.ReadInt16());
-                reader.Skip(2);
+
+                if (reader.Factory.ReleaseVersion / 100 >= 108)
+                    reader.Skip(4); // dungeonGauntletDifficultyId
+
                 u.IsLeader = reader.ReadBoolean();
-                if (u.IsLeader) Id = u.PlayerId;
                 u.Online = reader.ReadBoolean();
-                reader.BaseStream.Position = nameOffset - 4;
+
+                reader.RepositionAt(nameOffset);
                 u.Name = reader.ReadTeraString();
+
+                if (u.IsLeader) Id = u.PlayerId;
                 Members.Add(u);
                 if (next != 0) reader.BaseStream.Position = next - 4;
             }
