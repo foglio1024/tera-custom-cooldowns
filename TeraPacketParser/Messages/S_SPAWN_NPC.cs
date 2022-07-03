1eade2b84acb6776e5a3e61a49c73d364a690496
Foglio <foglio1024@gmail.com>
2022-07-03 13:02:18 +0200

    fix mobs detection

--- TeraPacketParser/Messages/S_SPAWN_NPC.cs
+++ TeraPacketParser/Messages/S_SPAWN_NPC.cs
@@ -17,52 +17,25 @@ namespace TeraPacketParser.Messages
 
         public S_SPAWN_NPC(TeraMessageReader reader) : base(reader)
         {
-            //reader.Skip(10);
-            //id = reader.ReadUInt64();
-            //var target = reader.ReadUInt64();
-            //var pos = reader.ReadVector3f();
-            //var angle = reader.ReadUInt16();
-            //var relation = reader.ReadUInt32();
-            //templateId = reader.ReadUInt32();
-            //huntingZoneId = reader.ReadUInt16();
-            //var unk1 = reader.ReadInt32();
-            //var walkSpeed = reader.ReadInt16();
-            //var runSpeed = reader.ReadInt16();
-            //var unk5 = reader.ReadInt16();
-            //var unk6 = reader.ReadBoolean();
-            //var unk7 = reader.ReadInt32();
-            //var visible = reader.ReadBoolean();
-            //var villager = reader.ReadBoolean();
-            //var spawnType = reader.ReadUInt32();
-            //var unk11 = reader.ReadUInt64();
-            //var unk12 = reader.ReadUInt64();
-            //var unk13 = reader.ReadUInt16();
-            //var unk14 = reader.ReadUInt32();
-            //var unk15 = reader.ReadBoolean();
-            //var owner = reader.ReadUInt64();
-            //var unk16 = reader.ReadUInt32();
-            //var unk17 = reader.ReadUInt32();
-            //var unk18 = reader.ReadUInt64();
-            //var unk19 = reader.ReadByte();
-            //var unk20 = reader.ReadUInt32();
-            //var unk25 = reader.ReadUInt32();
-
             reader.Skip(10); // seatsRef [4] + partsRef[4] + npcNameRef [2]
             EntityId = reader.ReadUInt64();
             reader.Skip(8); // target
-            Level = reader.ReadInt32();
-            MaxHP = reader.ReadInt64();
-            EnrageThreshold = reader.ReadInt64();
+            if (reader.Factory.ReleaseVersion/100 >= 101)
+            {
+                Level = reader.ReadInt32();
+                MaxHP = reader.ReadInt64();
+                EnrageThreshold = reader.ReadInt64();
+            }
             reader.Skip(12); //var loc = reader.ReadVector3f();
             reader.Skip(2); //var angle = reader.ReadInt16();
             reader.Skip(4); //var relation = reader.ReadInt32();
             TemplateId = reader.ReadUInt32();
             HuntingZoneId = reader.ReadUInt16();
             reader.Skip(4  // shapeID
-                      + 2 +2  // walkSpeed + runSpeed
+                      + 2 + 2  // walkSpeed + runSpeed
                       );
             reader.Skip(2); // status
-            IsEnraged =  reader.ReadInt16() == 1; //var enrage = reader.ReadUInt16(); // 0/1
+            IsEnraged = reader.ReadInt16() == 1; //var enrage = reader.ReadUInt16(); // 0/1
             RemainingEnrageTime = reader.ReadInt32();
             reader.Skip(2  // hpLevel  
                       + 2  // questInfo
