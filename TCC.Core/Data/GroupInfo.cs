c6c89e147f9396b52e6852db266a54acb1df5156
Foglio <foglio1024@gmail.com>
2023-08-15 15:19:22 +0200

    add leader indicator to character window (close #280)

--- TCC.Core/Data/GroupInfo.cs
+++ TCC.Core/Data/GroupInfo.cs
@@ -1,69 +1,80 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using TeraDataLite;
 
-namespace TCC.Data
+namespace TCC.Data;
+
+public class GroupInfo
 {
-    public class GroupInfo
+    GroupMemberData leader = new();
+
+    public event Action? LeaderChanged;
+    public bool InGroup { get; private set; }
+    public bool IsRaid { get; private set; }
+    public bool AmILeader => Game.Me.Name == Leader.Name && InGroup;
+    public int Size { get; private set; }
+
+    public GroupMemberData Leader
     {
-        public bool InGroup { get; private set; }
-        public bool IsRaid { get; private set; }
-        public bool AmILeader => Game.Me.Name == Leader.Name;
-        public int Size { get; private set; }
+        get => leader;
+        private set
+        {
+            if (leader == value) return;
+            leader = value;
+            LeaderChanged?.Invoke();
+        }
+    }
+    List<GroupMemberData> Members { get; set; } = new();
 
-        public GroupMemberData Leader { get; private set; } = new();
-        private List<GroupMemberData> Members { get; set; } = new();
-
-        public void SetGroup(List<GroupMemberData> members, bool raid)
-        {
-            Members = members;
-            Leader = members.FirstOrDefault(m => m.IsLeader)!;
-            IsRaid = raid;
-            InGroup = true;
-            Size = Members.Count;
-        }
-        public void ChangeLeader(string name)
-        {
-            Members.ForEach(x => x.IsLeader = x.Name == name);
-            Leader = Members.FirstOrDefault(m => m.Name == name)!;
-        }
-        public void Remove(uint playerId, uint serverId)
-        {
-            var target = Members.FirstOrDefault(m => m.PlayerId == playerId && m.ServerId == serverId);
-            if (target == null) return;
-            Members.Remove(target);
-            Size = Members.Count;
-        }
-        public void Disband()
-        {
-            Members.Clear();
-            Leader = new GroupMemberData();
-            IsRaid = false;
-            InGroup = false;
-            Size = Members.Count;
-        }
-        public bool Has(string name)
-        {
-            return Members.Any(m => m.Name == name);
-        }
-        public bool Has(uint pId)
-        {
-            return Members.Any(m => m.PlayerId == pId);
-        }
-        public bool HasPowers(string name)
-        {
-            return Has(name) && Members.FirstOrDefault(x => x.Name == name)?.CanInvite == true;
-        }
-
-        public bool TryGetMember(uint playerId, uint serverId, out GroupMemberData? member)
-        {
-            member = Members.FirstOrDefault(m => m.PlayerId == playerId && m.ServerId == serverId);
-            return member != null;
-        }
-        public bool TryGetMember(string name, out GroupMemberData? member)
-        {
-            member = Members.FirstOrDefault(m => m.Name == name);
-            return member != null;
-        }
+    public void SetGroup(List<GroupMemberData> members, bool raid)
+    {
+        Members = members;
+        Leader = members.FirstOrDefault(m => m.IsLeader)!;
+        IsRaid = raid;
+        InGroup = true;
+        Size = Members.Count;
+    }
+    public void ChangeLeader(string name)
+    {
+        Members.ForEach(x => x.IsLeader = x.Name == name);
+        Leader = Members.FirstOrDefault(m => m.Name == name)!;
+    }
+    public void Remove(uint playerId, uint serverId)
+    {
+        var target = Members.FirstOrDefault(m => m.PlayerId == playerId && m.ServerId == serverId);
+        if (target == null) return;
+        Members.Remove(target);
+        Size = Members.Count;
+    }
+    public void Disband()
+    {
+        Members.Clear();
+        Leader = new GroupMemberData();
+        IsRaid = false;
+        InGroup = false;
+        Size = Members.Count;
+    }
+    public bool Has(string name)
+    {
+        return Members.Any(m => m.Name == name);
+    }
+    public bool Has(uint pId)
+    {
+        return Members.Any(m => m.PlayerId == pId);
+    }
+    public bool HasPowers(string name)
+    {
+        return Has(name) && Members.FirstOrDefault(x => x.Name == name)?.CanInvite == true;
+    }
+    public bool TryGetMember(uint playerId, uint serverId, out GroupMemberData? member)
+    {
+        member = Members.FirstOrDefault(m => m.PlayerId == playerId && m.ServerId == serverId);
+        return member != null;
+    }
+    public bool TryGetMember(string name, out GroupMemberData? member)
+    {
+        member = Members.FirstOrDefault(m => m.Name == name);
+        return member != null;
     }
 }
--- TCC.Core/ViewModels/Widgets/CharacterWindowViewModel.cs
+++ TCC.Core/ViewModels/Widgets/CharacterWindowViewModel.cs
@@ -6,109 +6,109 @@ using TeraDataLite;
 using TeraPacketParser.Analysis;
 using TeraPacketParser.Messages;
 
-namespace TCC.ViewModels.Widgets
+namespace TCC.ViewModels.Widgets;
+
+[TccModule]
+public class CharacterWindowViewModel : TccWindowViewModel
 {
-    //TODO: remove references to other modules?
-    [TccModule]
-    public class CharacterWindowViewModel : TccWindowViewModel
+    public Player Player => Game.Me;
+    public bool ShowRe =>
+        (Player.Class == Class.Brawler || Player.Class == Class.Gunner ||
+         Player.Class == Class.Ninja || Player.Class == Class.Valkyrie) &&
+        ((CharacterWindowSettings)Settings!).ShowStamina;
+    public bool ShowElements => Player.Class == Class.Sorcerer &&
+                                ((CharacterWindowSettings)Settings!).SorcererShowElements;
+
+    public bool ShowEdge => Player.Class == Class.Warrior &&
+                            ((CharacterWindowSettings) Settings!).WarriorShowEdge;
+
+    public bool ShowLeaderIcon => Game.Group.AmILeader;
+
+    public CharacterWindowViewModel(CharacterWindowSettings settings) : base(settings)
     {
-        public Player Player => Game.Me;
-        public bool ShowRe =>
-            (Player.Class == Class.Brawler || Player.Class == Class.Gunner ||
-             Player.Class == Class.Ninja || Player.Class == Class.Valkyrie) &&
-            ((CharacterWindowSettings)Settings!).ShowStamina;
-        public bool ShowElements => Player.Class == Class.Sorcerer &&
-                                    ((CharacterWindowSettings)Settings!).SorcererShowElements;
+        Game.Me.PropertyChanged += MePropertyChanged;
+        Game.Group.LeaderChanged += OnLeaderChanged;
+        settings.SorcererShowElementsChanged += () => N(nameof(ShowElements));
+        settings.WarriorShowEdgeChanged += () => N(nameof(ShowEdge));
+        settings.ShowStaminaChanged += () => N(nameof(ShowRe));
+    }
 
-        public bool ShowEdge => Player.Class == Class.Warrior &&
-                                ((CharacterWindowSettings) Settings!).WarriorShowEdge;
+    void OnLeaderChanged()
+    {
+        N(nameof(ShowLeaderIcon));
+    }
 
-        public CharacterWindowViewModel(CharacterWindowSettings settings) : base(settings)
-        {
-            Game.Me.PropertyChanged += MePropertyChanged;
-            settings.SorcererShowElementsChanged += () => N(nameof(ShowElements));
-            settings.WarriorShowEdgeChanged += () => N(nameof(ShowEdge));
-            settings.ShowStaminaChanged += () => N(nameof(ShowRe));
-        }
+    protected override void InstallHooks()
+    {
+        PacketAnalyzer.Processor.Hook<S_PLAYER_STAT_UPDATE>(OnPlayerStatUpdate);
+        PacketAnalyzer.Processor.Hook<S_CREATURE_LIFE>(OnCreatureLife);
+        PacketAnalyzer.Processor.Hook<S_ABNORMALITY_DAMAGE_ABSORB>(OnAbnormalityDamageAbsorb);
+        PacketAnalyzer.Processor.Hook<S_CREATURE_CHANGE_HP>(OnCreatureChangeHp);
+        PacketAnalyzer.Processor.Hook<S_PLAYER_CHANGE_MP>(OnPlayerChangeMp);
+        PacketAnalyzer.Processor.Hook<S_PLAYER_CHANGE_STAMINA>(OnPlayerChangeStamina);
+    }
 
-        protected override void InstallHooks()
-        {
-            PacketAnalyzer.Processor.Hook<S_PLAYER_STAT_UPDATE>(OnPlayerStatUpdate);
-            PacketAnalyzer.Processor.Hook<S_CREATURE_LIFE>(OnCreatureLife);
-            PacketAnalyzer.Processor.Hook<S_ABNORMALITY_DAMAGE_ABSORB>(OnAbnormalityDamageAbsorb);
-            PacketAnalyzer.Processor.Hook<S_CREATURE_CHANGE_HP>(OnCreatureChangeHp);
-            PacketAnalyzer.Processor.Hook<S_PLAYER_CHANGE_MP>(OnPlayerChangeMp);
-            PacketAnalyzer.Processor.Hook<S_PLAYER_CHANGE_STAMINA>(OnPlayerChangeStamina);
-        }
+    protected override void RemoveHooks()
+    {
+        PacketAnalyzer.Processor.Unhook<S_PLAYER_STAT_UPDATE>(OnPlayerStatUpdate);
+        PacketAnalyzer.Processor.Unhook<S_CREATURE_LIFE>(OnCreatureLife);
+        PacketAnalyzer.Processor.Unhook<S_ABNORMALITY_DAMAGE_ABSORB>(OnAbnormalityDamageAbsorb);
+        PacketAnalyzer.Processor.Unhook<S_CREATURE_CHANGE_HP>(OnCreatureChangeHp);
+        PacketAnalyzer.Processor.Unhook<S_PLAYER_CHANGE_MP>(OnPlayerChangeMp);
+        PacketAnalyzer.Processor.Unhook<S_PLAYER_CHANGE_STAMINA>(OnPlayerChangeStamina);
+    }
 
-        protected override void RemoveHooks()
-        {
-            PacketAnalyzer.Processor.Unhook<S_PLAYER_STAT_UPDATE>(OnPlayerStatUpdate);
-            PacketAnalyzer.Processor.Unhook<S_CREATURE_LIFE>(OnCreatureLife);
-            PacketAnalyzer.Processor.Unhook<S_ABNORMALITY_DAMAGE_ABSORB>(OnAbnormalityDamageAbsorb);
-            PacketAnalyzer.Processor.Unhook<S_CREATURE_CHANGE_HP>(OnCreatureChangeHp);
-            PacketAnalyzer.Processor.Unhook<S_PLAYER_CHANGE_MP>(OnPlayerChangeMp);
-            PacketAnalyzer.Processor.Unhook<S_PLAYER_CHANGE_STAMINA>(OnPlayerChangeStamina);
-        }
+    void OnPlayerChangeStamina(S_PLAYER_CHANGE_STAMINA m)
+    {
+        Player.MaxST = m.MaxST + m.BonusST;
+        Player.CurrentST = m.CurrentST;
+    }
+    void OnPlayerChangeMp(S_PLAYER_CHANGE_MP m)
+    {
+        if (!Game.IsMe(m.Target)) return;
+        Player.MaxMP = m.MaxMP;
+        Player.CurrentMP = m.CurrentMP;
+    }
+    void OnCreatureChangeHp(S_CREATURE_CHANGE_HP m)
+    {
+        if (!Game.IsMe(m.Target)) return;
+        Player.MaxHP = m.MaxHP;
+        Player.CurrentHP = m.CurrentHP;
+    }
+    void OnAbnormalityDamageAbsorb(S_ABNORMALITY_DAMAGE_ABSORB p)
+    {
+        // todo: add chat message too
+        if (!Game.IsMe(p.Target) || Player.CurrentShield < 0) return;
+        Player.DamageShield(p.Damage);
+    }
 
-        private void OnPlayerChangeStamina(S_PLAYER_CHANGE_STAMINA m)
-        {
-            Player.MaxST = m.MaxST + m.BonusST;
-            Player.CurrentST = m.CurrentST;
-        }
-        private void OnPlayerChangeMp(S_PLAYER_CHANGE_MP m)
-        {
-            if (!Game.IsMe(m.Target)) return;
-            Player.MaxMP = m.MaxMP;
-            Player.CurrentMP = m.CurrentMP;
-        }
-        private void OnCreatureChangeHp(S_CREATURE_CHANGE_HP m)
-        {
-            if (!Game.IsMe(m.Target)) return;
-            Player.MaxHP = m.MaxHP;
-            Player.CurrentHP = m.CurrentHP;
-        }
-        private void OnAbnormalityDamageAbsorb(S_ABNORMALITY_DAMAGE_ABSORB p)
-        {
-            // todo: add chat message too
-            if (!Game.IsMe(p.Target) || Player.CurrentShield < 0) return;
-            Player.DamageShield(p.Damage);
-        }
+    void OnCreatureLife(S_CREATURE_LIFE m)
+    {
+        if (!Game.IsMe(m.Target)) return;
+        Player.IsAlive = m.Alive;
+    }
 
-        private void OnCreatureLife(S_CREATURE_LIFE m)
-        {
-            if (!Game.IsMe(m.Target)) return;
-            Player.IsAlive = m.Alive;
-        }
+    // TODO: move these to Game?
+    void OnPlayerStatUpdate(S_PLAYER_STAT_UPDATE m)
+    {
+        Player.ItemLevel = m.Ilvl;
+        Player.Level = m.Level;
+        Player.CritFactor = m.TotalCritFactor;
+        Player.MaxHP = m.MaxHP;
+        Player.MaxMP = m.MaxMP;
+        Player.MaxST = m.MaxST + m.BonusST;
+        Player.CurrentHP = m.CurrentHP;
+        Player.CurrentMP = m.CurrentMP;
+        Player.CurrentST = m.CurrentST;
+        Player.MagicalResistance = m.TotalMagicalResistance;
+    }
 
-        // TODO: move these to Game?
-        private void OnPlayerStatUpdate(S_PLAYER_STAT_UPDATE m)
-        {
-            Player.ItemLevel = m.Ilvl;
-            Player.Level = m.Level;
-            Player.CritFactor = m.TotalCritFactor;
-            Player.MaxHP = m.MaxHP;
-            Player.MaxMP = m.MaxMP;
-            Player.MaxST = m.MaxST + m.BonusST;
-            Player.CurrentHP = m.CurrentHP;
-            Player.CurrentMP = m.CurrentMP;
-            Player.CurrentST = m.CurrentST;
-            Player.MagicalResistance = m.TotalMagicalResistance;
-
-            // moved to Game
-            //if (Player.Class != Class.Sorcerer) return;
-            //Player.Fire = m.Fire;
-            //Player.Ice = m.Ice;
-            //Player.Arcane = m.Arcane;
-        }
-
-        private void MePropertyChanged(object? sender, PropertyChangedEventArgs e)
-        {
-            N(e.PropertyName);
-            if (e.PropertyName != nameof(Data.Pc.Player.Class)) return;
-            N(nameof(ShowRe));
-            N(nameof(ShowEdge));
-            N(nameof(ShowElements));
-        }
+    void MePropertyChanged(object? sender, PropertyChangedEventArgs e)
+    {
+        N(e.PropertyName);
+        if (e.PropertyName != nameof(Data.Pc.Player.Class)) return;
+        N(nameof(ShowRe));
+        N(nameof(ShowEdge));
+        N(nameof(ShowElements));
     }
 }
