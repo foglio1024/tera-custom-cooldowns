3169c04e5cb3ca2d43ab02a7d4de4f86d5fdded4
Foglio <foglio1024@gmail.com>
2023-08-29 21:28:14 +0200

    parse more fields of S_REQUEST_SPAWN_SERVANT

--- TeraPacketParser/Messages/S_REQUEST_SPAWN_SERVANT.cs
+++ TeraPacketParser/Messages/S_REQUEST_SPAWN_SERVANT.cs
@@ -3,10 +3,13 @@
     public class S_REQUEST_SPAWN_SERVANT : ParsedMessage
     {
         public ulong Owner { get; }
-        public ulong GameId { get; set; }
+        public ulong EntityId { get; set; }
+        public GameId GameId { get; set; }
 
         public uint FellowShip { get; set; }
+        public uint ServantType { get; }
         public uint Id { get; set; }
+        public uint SpawnType { get; }
 
         public S_REQUEST_SPAWN_SERVANT(TeraMessageReader reader) : base(reader)
         {
@@ -16,11 +19,12 @@
             var abilitiesCount = reader.ReadUInt16();
             var abilitiesOffset = reader.ReadUInt16();
             var nameOffset = reader.ReadUInt16();
-            GameId = reader.ReadUInt64();
+            EntityId = reader.ReadUInt64();
+            GameId = GameId.Parse(EntityId);
             var dbid = reader.ReadUInt64();
             var loc = reader.ReadVector3f();
             var h = reader.ReadUInt16();
-            var type = reader.ReadUInt32();
+            ServantType = reader.ReadUInt32();
             Id = reader.ReadUInt32();
             var linkedNpcTemplateId = reader.ReadUInt32();
             var linkedNpcZoneId = reader.ReadUInt16();
@@ -29,7 +33,7 @@
 
             Owner = reader.ReadUInt64();
             var energy = reader.ReadUInt32();
-            var spawnType = reader.ReadUInt32();
+            SpawnType = reader.ReadUInt32();
             var level = reader.ReadUInt32();
             FellowShip = reader.ReadUInt32();
             // ReSharper restore UnusedVariable
