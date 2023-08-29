dd748ca043391577bc557f32def1c6836e18a5df
Foglio <foglio1024@gmail.com>
2023-08-29 21:28:20 +0200

    add GameId struct

---
+++ TeraPacketParser/Messages/GameId.cs
@@ -0,0 +1,17 @@
+﻿
+
+
+namespace TeraPacketParser.Messages
+{
+    public readonly record struct GameId(int Id, short Unk1, byte Type, sbyte Unk2)
+    {
+        public static GameId Parse(ulong id)
+        {
+            return new GameId(
+                (int)id,
+                (short)(id >> 32),
+                (byte)(id >> 48),
+                (sbyte)(id >> 56));
+        }
+    }
+}
