d0a9c2bb191e7f33bdbdc7f0d58a9631dfdd5622
Foglio <foglio1024@gmail.com>
2023-09-30 17:39:44 +0200

    update parsing of S_SPAWN_DROPITEM

--- TeraPacketParser/Messages/S_SPAWN_DROPITEM.cs
+++ TeraPacketParser/Messages/S_SPAWN_DROPITEM.cs
@@ -1,13 +1,17 @@
-﻿namespace TeraPacketParser.Messages
-{
-    public class S_SPAWN_DROPITEM : ParsedMessage
-    {
-        public GameId GameId { get; }
-        public S_SPAWN_DROPITEM(TeraMessageReader reader) : base(reader)
-        {
-            reader.Skip(8);
-            GameId = GameId.Parse(reader.ReadUInt64());
-        }
-    }
+﻿namespace TeraPacketParser.Messages;
 
+public class S_SPAWN_DROPITEM : ParsedMessage
+{
+    public GameId GameId { get; }
+    public uint ItemId { get; }
+    public uint Amount { get; }
+
+    public S_SPAWN_DROPITEM(TeraMessageReader reader) : base(reader)
+    {
+        reader.Skip(6);
+        GameId = GameId.Parse(reader.ReadUInt64());
+        reader.Skip(3 * 4); // loc
+        ItemId = reader.ReadUInt32();
+        Amount = reader.ReadUInt32();
+    }
 }
