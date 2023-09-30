4bf75bbeec7dde5a6ddf6fb0a0b02b01719feee6
Foglio <foglio1024@gmail.com>
2023-09-30 17:34:19 +0200

    refactor S_PARTY_MEMBER_INFO

--- TeraPacketParser/Messages/S_PARTY_MEMBER_INFO.cs
+++ TeraPacketParser/Messages/S_PARTY_MEMBER_INFO.cs
@@ -1,53 +1,66 @@
 ﻿using System;
 using System.Collections.Generic;
 using TeraDataLite;
-namespace TeraPacketParser.Messages
+
+namespace TeraPacketParser.Messages;
+
+public class S_PARTY_MEMBER_INFO : ParsedMessage
 {
-    public class S_PARTY_MEMBER_INFO : ParsedMessage
+    public uint Id { get; }
+    public List<GroupMemberData> Members { get; }
+
+    public S_PARTY_MEMBER_INFO(TeraMessageReader reader) : base(reader)
     {
-        public uint Id { get; }
-        public List<GroupMemberData> Members { get; }
+        var count = reader.ReadUInt16();
+        var offset = reader.ReadUInt16();
 
-        public S_PARTY_MEMBER_INFO(TeraMessageReader reader) : base(reader)
+        reader.Skip(2); // bool isRaid + bool adminInspectionUI
+
+        reader.RepositionAt(offset);
+        Members = new List<GroupMemberData>();
+        for (var i = 0; i < count; i++)
         {
-            var count = reader.ReadUInt16();
-            var offset = reader.ReadUInt16();
+            reader.Skip(2); // curr
+            var next = reader.ReadUInt16();
+            var nameOffset = reader.ReadUInt16();
+            var playerId = reader.ReadUInt32();
 
-            reader.Skip(2); // bool isRaid + bool adminInspectionUI
+            var serverId = 0U;
+            if (reader.Factory.ReleaseVersion / 100 >= 108)
+                serverId = reader.ReadUInt32();
 
-            reader.RepositionAt(offset);
-            Members = new List<GroupMemberData>();
-            for (var i = 0; i < count; i++)
+            var cl = (Class)reader.ReadUInt16();
+            reader.Skip(4); // race + gender
+            var level = Convert.ToUInt32(reader.ReadUInt16());
+            var worldId = reader.ReadUInt32(); // worldId
+            var guardId = reader.ReadUInt32();
+            var sectionId = reader.ReadUInt32();
+
+            if (reader.Factory.ReleaseVersion / 100 >= 108)
+                reader.Skip(4); // dungeonGauntletDifficultyId
+
+            var isLeader = reader.ReadBoolean();
+            var online = reader.ReadBoolean();
+
+            reader.RepositionAt(nameOffset);
+            var name = reader.ReadTeraString();
+
+            var u = new GroupMemberData
             {
-                var u = new GroupMemberData();
-                reader.Skip(2); // curr
-                var next = reader.ReadUInt16();
-                var nameOffset = reader.ReadUInt16();
-                u.PlayerId = reader.ReadUInt32();
+                PlayerId = playerId,
+                Name = name,
+                ServerId = serverId,
+                Class = cl,
+                Level = level,
+                GuardId = guardId,
+                SectionId = sectionId,
+                IsLeader = isLeader,
+                Online = online,
+            };
 
-                if (reader.Factory.ReleaseVersion / 100 >= 108)
-                    u.ServerId = reader.ReadUInt32();
-
-                u.Class = (Class)reader.ReadUInt16();
-                reader.Skip(4); // race + gender
-                u.Level = Convert.ToUInt32(reader.ReadUInt16());
-                var worldId = reader.ReadUInt32(); // worldId
-                u.GuardId = reader.ReadUInt32();
-                u.SectionId = reader.ReadUInt32();
-
-                if (reader.Factory.ReleaseVersion / 100 >= 108)
-                    reader.Skip(4); // dungeonGauntletDifficultyId
-
-                u.IsLeader = reader.ReadBoolean();
-                u.Online = reader.ReadBoolean();
-
-                reader.RepositionAt(nameOffset);
-                u.Name = reader.ReadTeraString();
-
-                if (u.IsLeader) Id = u.PlayerId;
-                Members.Add(u);
-                if (next != 0) reader.BaseStream.Position = next - 4;
-            }
+            if (u.IsLeader) Id = u.PlayerId;
+            Members.Add(u);
+            if (next != 0) reader.BaseStream.Position = next - 4;
         }
     }
 }
