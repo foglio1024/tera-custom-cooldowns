bf53e9ad9bcce29d2417a15f7b35f5c47eae379a
Foglio <foglio1024@gmail.com>
2023-09-30 17:33:34 +0200

    actually parse S_ASK_BIDDING_RARE_ITEM

--- TeraPacketParser/Messages/S_ASK_BIDDING_RARE_ITEM.cs
+++ TeraPacketParser/Messages/S_ASK_BIDDING_RARE_ITEM.cs
@@ -1,14 +1,17 @@
-﻿
+﻿namespace TeraPacketParser.Messages;
 
-
-namespace TeraPacketParser.Messages
+public class S_ASK_BIDDING_RARE_ITEM : ParsedMessage
 {
-    public class S_ASK_BIDDING_RARE_ITEM : ParsedMessage
-    {
+    public int Index { get; }
+    public uint Amount { get; }
+    public uint ItemId { get; }
 
-        public S_ASK_BIDDING_RARE_ITEM(TeraMessageReader reader) : base(reader)
-        {
-            //needed only to setup the rolling phase, so not actually parsed
-        }
+    public S_ASK_BIDDING_RARE_ITEM(TeraMessageReader reader) : base(reader)
+    {
+        reader.Skip(4);
+        Index = reader.ReadInt32();
+        reader.Skip(4);
+        ItemId = reader.ReadUInt32();
+        Amount = reader.ReadUInt32();
     }
 }
