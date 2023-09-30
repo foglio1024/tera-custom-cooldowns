6c42887d38e8274030dbff403145058826c2043c
Foglio <foglio1024@gmail.com>
2023-09-30 17:32:44 +0200

    add static Zero GameId

--- TeraPacketParser/Messages/GameId.cs
+++ TeraPacketParser/Messages/GameId.cs
@@ -1,17 +1,18 @@
 ﻿
 
 
-namespace TeraPacketParser.Messages
+namespace TeraPacketParser.Messages;
+
+public readonly record struct GameId(int Id, short Unk1, byte Type, sbyte Unk2)
 {
-    public readonly record struct GameId(int Id, short Unk1, byte Type, sbyte Unk2)
+    public static GameId Parse(ulong id)
     {
-        public static GameId Parse(ulong id)
-        {
-            return new GameId(
-                (int)id,
-                (short)(id >> 32),
-                (byte)(id >> 48),
-                (sbyte)(id >> 56));
-        }
+        return new GameId(
+            (int)id,
+            (short)(id >> 32),
+            (byte)(id >> 48),
+            (sbyte)(id >> 56));
     }
+
+    public static GameId Zero => new GameId(0, 0, 0, 0);
 }
