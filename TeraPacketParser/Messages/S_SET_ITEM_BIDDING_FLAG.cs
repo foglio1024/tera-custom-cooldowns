7e234c512ae629ed9b10b424dd5e22dd1b1af28f
Foglio <foglio1024@gmail.com>
2023-09-30 17:38:38 +0200

    add S_UPDATE_BIDDING_COUNT and S_SET_ITEM_BIDDING_FLAG

---
+++ TeraPacketParser/Messages/S_SET_ITEM_BIDDING_FLAG.cs
@@ -0,0 +1,13 @@
+﻿namespace TeraPacketParser.Messages;
+
+public class S_SET_ITEM_BIDDING_FLAG : ParsedMessage
+{
+    public GameId GameId { get; }
+    public bool Flag { get; }
+
+    internal S_SET_ITEM_BIDDING_FLAG(TeraMessageReader reader) : base(reader)
+    {
+        GameId = GameId.Parse(reader.ReadUInt64());
+        Flag = reader.ReadBoolean();
+    }
+}
---
+++ TeraPacketParser/Messages/S_UPDATE_BIDDING_COUNT.cs
@@ -0,0 +1,11 @@
+﻿namespace TeraPacketParser.Messages;
+
+public class S_UPDATE_BIDDING_COUNT : ParsedMessage
+{
+    public int Count { get; }
+
+    public S_UPDATE_BIDDING_COUNT(TeraMessageReader reader) : base(reader)
+    {
+        Count = reader.ReadInt32();
+    }
+}
