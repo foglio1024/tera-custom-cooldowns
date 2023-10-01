9d0c7d1ffaa5919acf28524e525a8853bd09396f
Foglio <foglio1024@gmail.com>
2023-10-01 12:00:30 +0200

    wip loot config

---
+++ TCC.Core/ViewModels/LootFilterConfigurationViewModel.cs
@@ -0,0 +1,40 @@
+﻿using Nostrum.WPF.Factories;
+using Nostrum.WPF.ThreadSafe;
+using System;
+using System.ComponentModel;
+using TCC.Data;
+using TCC.Settings.WindowSettings;
+
+namespace TCC.ViewModels;
+
+public class LootFilterConfigurationViewModel : ThreadSafeObservableObject
+{
+    public ICollectionViewLiveShaping? ItemsView { get; }
+    public LootDistributionWindowSettings Settings { get; }
+
+    string _searchFilter;
+    public string SearchFilter
+    {
+        get => _searchFilter;
+        set
+        {
+            if (_searchFilter == value) return;
+            _searchFilter = value;
+            N();
+
+            var view = (ICollectionView)ItemsView;
+            view.Filter = o => ((Item)o).Name.IndexOf(_searchFilter, StringComparison.InvariantCultureIgnoreCase) != -1;
+            view.Refresh();
+        }
+    }
+
+
+    // todo: make 3 lists: items list, pass items list, drop items list, use drag&drop
+
+    protected LootFilterConfigurationViewModel(LootDistributionWindowSettings settings)
+    {
+        ItemsView = CollectionViewFactory.CreateLiveCollectionView(Game.DB!.ItemsDatabase.Items);
+
+        Settings = settings;
+    }
+}
