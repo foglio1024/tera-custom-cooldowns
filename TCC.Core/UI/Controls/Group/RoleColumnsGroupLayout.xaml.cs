e4f90b7f5a159ca5f95f2ea19f40eafc35cae4fc
Foglio <foglio1024@gmail.com>
2023-11-08 11:47:52 +0100

    remove unnecessary comments

--- TCC.Core/UI/Controls/Group/RoleColumnsGroupLayout.xaml.cs
+++ TCC.Core/UI/Controls/Group/RoleColumnsGroupLayout.xaml.cs
@@ -1,8 +1,5 @@
 ﻿namespace TCC.UI.Controls.Group;
 
-/// <summary>
-/// Logica di interazione per DefaultGroupLayout.xaml
-/// </summary>
 public partial class RoleColumnsGroupLayout
 {
     public RoleColumnsGroupLayout()
--- TCC.Core/UI/Controls/Group/SingleColumnGroupLayout.xaml.cs
+++ TCC.Core/UI/Controls/Group/SingleColumnGroupLayout.xaml.cs
@@ -1,8 +1,5 @@
 ﻿namespace TCC.UI.Controls.Group;
 
-/// <summary>
-/// Logica di interazione per SingleColumnGroupLayout.xaml
-/// </summary>
 public partial class SingleColumnGroupLayout
 {
     public SingleColumnGroupLayout()
