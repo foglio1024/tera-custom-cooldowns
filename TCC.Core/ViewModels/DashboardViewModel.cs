e02aa19c5f085b11e5a37f7d80f4dbe80d37570a
Foglio <foglio1024@gmail.com>
2024-01-01 13:39:04 +0100

    use ToSyncList() in DashboardViewModel

--- TCC.Core/ViewModels/DashboardViewModel.cs
+++ TCC.Core/ViewModels/DashboardViewModel.cs
@@ -813,11 +813,11 @@ public class DashboardViewModel : TccWindowViewModel
         Task.Run(() =>
         {
             CurrentCharacter.Buffs.Clear();
-            Game.Me.Buffs.ToList().ForEach(b =>
+            Game.Me.Buffs.ToSyncList().ForEach(b =>
             {
                 CurrentCharacter?.Buffs.Add(new AbnormalityData { Id = b.Abnormality.Id, Duration = b.DurationLeft, Stacks = b.Stacks });
             });
-            Game.Me.Debuffs.ToList().ForEach(b =>
+            Game.Me.Debuffs.ToSyncList().ForEach(b =>
             {
                 CurrentCharacter?.Buffs.Add(new AbnormalityData { Id = b.Abnormality.Id, Duration = b.DurationLeft, Stacks = b.Stacks });
             });
