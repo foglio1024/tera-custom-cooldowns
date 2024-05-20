81fe4e1d7ab95f2ddad5cb8a32906580816319a4
Foglio <foglio1024@gmail.com>
2024-05-20 19:31:02 +0200

    move tooltip parsing to a converter

---
+++ TCC.Core/UI/Converters/TooltipTextConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Markup;
+using TCC.UI.Controls.Abnormalities;
+
+namespace TCC.UI.Converters;
+
+public class TooltipTextConverter : MarkupExtension, IValueConverter
+{
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if (value is string tt) return new TooltipParser(tt).Parse();
+        return null;
+    }
+
+    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        return null;
+    }
+
+    public override object ProvideValue(IServiceProvider serviceProvider)
+    {
+        return this;
+    }
+}
